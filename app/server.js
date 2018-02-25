b138f05306d5146a577c04d8d35bc0f63819a821
Wes Jourdan <wes.jourdan@icloud.com>
2018-02-25 13:20:03 -0500

    Implement all logic and error handling. Still need to update README

--- app/server.js
+++ app/server.js
@@ -22,22 +22,27 @@ var myRouter = Router();
 myRouter.use(bodyParser.json());
 
 http.createServer(function (request, response) {
-
+  // Verify that a valid API Key exists before we let anyone access our API
+  if (!VALID_API_KEYS.includes(request.headers["x-authentication"])) {
+    response.writeHead(401, "You need to have a valid API key to use this API");
+    response.end();
+  }
+  myRouter(request, response, finalHandler(request, response))
 }).listen(PORT, (error) => {
   if (error) {
     return console.log('Error on Server Startup: ', error)
   }
-  fs.readFile('brands.json', 'utf8', function (error, data) {
+  fs.readFile('./initial-data/brands.json', 'utf8', function (error, data) {
     if (error) throw error;
     brands = JSON.parse(data);
     console.log(`Server setup: ${brands.length} brands loaded`);
   });
-  fs.readFile('products.json', 'utf8', function (error, data) {
+  fs.readFile('./initial-data/products.json', 'utf8', function (error, data) {
     if (error) throw error;
     products = JSON.parse(data);
-    console.log(`Server setup: ${products.length} users loaded`);
+    console.log(`Server setup: ${products.length} products loaded`);
   });
-  fs.readFile('users.json', 'utf8', function (error, data) {
+  fs.readFile('./initial-data/users.json', 'utf8', function (error, data) {
     if (error) throw error;
     users = JSON.parse(data);
     console.log(`Server setup: ${users.length} users loaded`);
@@ -45,34 +50,197 @@ http.createServer(function (request, response) {
   console.log(`Server is listening on ${PORT}`);
 });
 
-myRouter.get('/api/brands', (request, response) => {
+// Helper method to process access token
+var getValidTokenFromRequest = function (request) {
+  const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+  let passedToken = request._parsedUrl.query;
+  if (passedToken) {
+    // Verify the access token to make sure its valid and not expired
+    let currentAccessToken = accessTokens.find((accessToken) => {
+      return accessToken.token == passedToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
+    });
 
+    if (currentAccessToken) {
+      return currentAccessToken;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+myRouter.get('/api/brands', (request, response) => {
+  if (brands) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(brands));
+  }
+  response.writeHead(404, "Brands not found.")
+  return response.end();
 });
 
 myRouter.get('/api/brands/:id/products', (request, response) => {
-
+  let matchingProducts = products.filter((product) => {
+    return product.categoryId == request.params.id;
+  });
+  if (matchingProducts.length > 0) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(matchingProducts));
+  }
+  response.writeHead(404, "No matching products found")
+  return response.end();
 });
 
 myRouter.get('/api/products', (request, response) => {
+  // the includes method is case sensitive so we convert the search term and the product name to lower case.
+  let query = request._parsedUrl.query.toLowerCase()
+  let matchingProducts = []
 
+  products.filter((product) => {
+    if (product.name.toLowerCase().includes(query)) {
+      matchingProducts.push(product)
+    }
+  });
+  if (matchingProducts.length > 0) {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(matchingProducts));
+  }
+  response.writeHead(404, "No matching products found")
+  return response.end()
 });
 
 myRouter.post('/api/login', (request, response) => {
-
+  // Make sure there is a username and password in the request
+  if (request.body.username && request.body.password) {
+    // See if there is a user that has that username and password 
+    let user = users.find((user) => {
+      return user.login.username == request.body.username && user.login.password == request.body.password;
+    });
+    if (user) {
+      // Write the header because we know we will be returning successful at this point and that the response will be json
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      // We have a successful login, if we already have an existing access token, use that
+      let currentAccessToken = accessTokens.find((tokenObject) => {
+        return tokenObject.username == user.login.username;
+      });
+      // Update the last updated value so we get another time period
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        // Create a new token with the user value and a "random" token
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        }
+        accessTokens.push(newAccessToken);
+        response.end(JSON.stringify(newAccessToken.token));
+      }
+    } else {
+      // When a login fails, tell the client in a generic way that either the username or password was wrong
+      response.writeHead(401, "Invalid username or password");
+      response.end();
+    }
+  } else {
+    // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
+    response.writeHead(400, "Incorrectly formatted response");
+    response.end();
+  }
 });
 
 myRouter.get('/api/me/cart', (request, response) => {
-
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (currentAccessToken) {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username | null;
+    });
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(user.cart));
+  }
+  response.writeHead(401, "You must be logged in to access your cart")
+  return response.end();
 });
 
 myRouter.post('/api/me/cart', (request, response) => {
-
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (currentAccessToken) {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    let productId = request.body.productId;
+    let newQuantity = request.body.quantity;
+    let currentQuantity = user.cart.filter(product => product.id == productId).length;
+    
+    // We are either INCREASING the current quantity...
+    if (currentQuantity < newQuantity) {
+      let productToAdd = products.filter(product => product.id == productId);
+      //verify that the product exists.
+      if (productToAdd.length > 0) {
+        //set the number by which to modify the quantity.
+        let diff = newQuantity - currentQuantity
+        //add the product to the cart until diff is 0.
+        while (diff > 0) {
+          user.cart.push(productToAdd[0]);
+          diff--;
+        }
+      }
+    //... or we are DECREASING the current quantity.
+      } else if (currentQuantity > newQuantity) {
+        //set the number by which to modify the quantity.
+        let diff = currentQuantity - newQuantity;
+        //Iterate through cart. When a match occurs, delete it until diff = 0.
+        for (let i = 0; i < user.cart.length; i++) {
+          let currentItem = user.cart[i];
+          if (currentItem.id == productId) {
+            while (diff > 0) {
+              user.cart.splice(i, 1);
+              diff--;
+            }
+          }
+        }
+      }
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(user.cart));
+    }
+  response.writeHead(401, "You must be logged in to modify your cart")
+  return response.end();
 });
 
 myRouter.delete('/api/me/cart/:productId', (request, response) => {
-
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (currentAccessToken) {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    let idToDelete = request.params.productId;
+    //filter the array to return everything but the deleted product.
+    let newCart = user.cart.filter(product => product.id !== idToDelete )
+    user.cart = newCart
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(user.cart));    
+  }
+  response.writeHead(401, "You must be logged in to modify your cart")
+  return response.end();
 });
 
 myRouter.post('/api/me/cart/:productId', (request, response) => {
-
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (currentAccessToken) {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    let idToAdd = request.params.productId;
+    let productToAdd = products.filter(product => product.id == idToAdd)
+    if (productToAdd.length > 0) {
+      user.cart.push(productToAdd[0])
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      return response.end(JSON.stringify(user.cart));
+    } else {
+      response.writeHead(404, "The product was not found, no changes have been made to the cart")
+      return response.end();
+    }
+  }
+  response.writeHead(401, "You must be logged in to modify your cart")
+  return response.end();
 });
