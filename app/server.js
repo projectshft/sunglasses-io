4e2749095789161248503cd50fa4f19746a58426
Logan Peterson <lapeterson01@gmail.com>
2018-02-26 13:01:19 -0500

    Finish API

--- app/server.js
+++ app/server.js
@@ -14,9 +14,30 @@ const PORT = 3001;
 var brands = [];
 var products = [];
 var users = [];
+var accessTokens = [];
+var failedLoginAttempts = {};
 
 const myRouter = Router();
 myRouter.use(bodyParser.json());
+
+// Helper method to process access token
+var getValidTokenFromRequest = (request) => {
+    var parsedUrl = require('url').parse(request.url, true);
+    const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+    if (parsedUrl.query.accessToken) {
+        // Verify the access token to make sure it's valid and not expired
+        let currentAccessToken = accessTokens.find((accessToken) => {
+            return accessToken.token == parsedUrl.query.accessToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
+        });
+        if (currentAccessToken) {
+            return currentAccessToken;
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+}
 
 http.createServer(function (request, response) {
     if (request.method === 'OPTIONS') {
@@ -90,21 +111,151 @@ myRouter.get('/api/products', (request, response) => {
 });
 
 // User login
-// Find a way for request.body to include username & password using Postman
 myRouter.post('/api/login', (request, response) => {
-    if (request.body.username && request.body.password) {
+    if (!failedLoginAttempts[request.body.username]) {
+        failedLoginAttempts[request.body.username] = 0;
+    };
+    // Make sure there is a username and password in the request
+    if (request.body.username && request.body.password && failedLoginAttempts[request.body.username] < 3) {
+        // See if there is a user that has that username and password
         let user = users.find((user) => {
             return user.login.username == request.body.username && user.login.password == request.body.password;
         });
         if (user) {
+            // Reset our count of failed logins
+            failedLoginAttempts[request.body.username] = 0;
             response.writeHead(200, Object.assign(CORS_HEADERS, {'Content-Type':'application/json'}));
-            response.end();
+            let currentAccessToken = accessTokens.find((tokenObject) => {
+                return tokenObject.username == user.login.username;
+            });
+            if (currentAccessToken) {
+                currentAccessToken.lastUpdated = new Date();
+                response.end(JSON.stringify(currentAccessToken.token));
+            } else {
+                let newAccessToken = {
+                    username: user.login.username,
+                    lastUpdated: new Date(),
+                    token: uid(16)
+                }
+                accessTokens.push(newAccessToken);
+                response.end(JSON.stringify(newAccessToken.token));
+            }
         } else {
-            response.writeHead(401, "Invalid username or passowrd", CORS_HEADERS);
+            failedLoginAttempts[request.body.username]++;
+            response.writeHead(401, "Invalid username or password", CORS_HEADERS);
             response.end();
         }
     } else {
-        response.writeHead(400, "Incorrectly formatted response", CORS_HEADERS);
+        if (request.body.username && request.body.password) {
+            response.writeHead(401, "Too many failed login attempts", CORS_HEADERS);
+            response.end();
+        } else {
+            response.writeHead(400, "Incorrectly formatted response", CORS_HEADERS);
+            response.end();
+        }
+    }
+});
+
+myRouter.get('/api/me/cart', (request, response) => {
+   let currentAccessToken = getValidTokenFromRequest(request);
+   if (currentAccessToken) {
+       let user = users.find((user) => {
+           return user.login.username == currentAccessToken.username;
+       });
+       if (user.cart.length > 0) {
+           response.writeHead(200, Object.assign(CORS_HEADERS, {'Content-Type': 'application/json'}));
+           response.end(JSON.stringify(user.cart));
+       } else {
+           response.writeHead(404, "Your cart is empty", CORS_HEADERS);
+           response.end();
+       }
+   } else {
+       response.writeHead(401, "You must be logged in to continue", CORS_HEADERS);
+       response.end();
+   }
+});
+
+myRouter.post('/api/me/cart', (request, response) => {
+    let currentAccessToken = getValidTokenFromRequest(request);
+    if (currentAccessToken) {
+        let user = users.find((user) => {
+            return user.login.username == currentAccessToken.username;
+        });
+        if (user.cart.length > 0) {
+            user.cart.forEach((product) => {
+                user.purchasedCart.push(product);
+            });
+            user.cart = [];
+            response.writeHead(200, Object.assign(CORS_HEADERS, {'Content-Type': 'application/json'}));
+            response.end(JSON.stringify(user.purchasedCart));
+        } else {
+            response.writeHead(404, "Your cart is empty", CORS_HEADERS);
+            response.end();
+        }
+    } else {
+        response.writeHead(401, "You must be logged in to continue", CORS_HEADERS);
+        response.end();
+    }
+});
+
+myRouter.delete('/api/me/cart/:id', (request, response) => {
+    let currentAccessToken = getValidTokenFromRequest(request);
+    if (currentAccessToken) {
+        let product = products.find((product) => {
+            return product.id == request.params.id;
+        });
+        if (product) {
+            let user = users.find((user) => {
+                return user.login.username == currentAccessToken.username;
+            });
+            if (user.cart.includes(product)) {
+                const index = user.cart.indexOf(product);
+                if (user.cart[index].amount == 1) {
+                    user.cart.splice(index, 1);
+                } else {
+                    user.cart[index].amount--;
+                };
+                response.writeHead(200, Object.assign(CORS_HEADERS, {'Content-Type': 'application/json'}));
+                response.end(JSON.stringify(user.cart));
+            } else {
+                response.writeHead(404, "That product cannot be found in your cart", CORS_HEADERS);
+                response.end();
+            }
+        } else {
+            response.writeHead(404, "That product cannot be found", CORS_HEADERS);
+            response.end();
+        }
+    } else {
+        response.writeHead(401, "You must be logged in to continue", CORS_HEADERS);
+        response.end();
+    }
+});
+
+myRouter.post('/api/me/cart/:id', (request, response) => {
+    let currentAccessToken = getValidTokenFromRequest(request);
+    if (currentAccessToken) {
+        let product = products.find((product) => {
+            return product.id == request.params.id;
+        });
+        if (product) {
+            let user = users.find((user) => {
+                return user.login.username == currentAccessToken.username;
+            });
+            if (!user.cart.includes(product)) {
+                product.amount = 1;
+                user.cart.push(product);
+            } else {
+                let index = user.cart.indexOf(product);
+                user.cart[index].amount++;
+            }
+            response.writeHead(200, Object.assign(CORS_HEADERS, {'Content-Type': 'application/json'}));
+            response.end(JSON.stringify(product));
+        } else {
+            response.writeHead(404, "That product cannot be found", CORS_HEADERS);
+            response.end();
+        }
+    } else {
+        response.writeHead(401, "You must be logged in to continue", CORS_HEADERS);
         response.end();
     }
 });
