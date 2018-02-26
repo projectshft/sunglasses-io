49791cf4f7c5b3618794373746f750abf3acc234
Jorge Rodriguez <rodri0315@gmail.com>
2018-02-25 23:34:08 -0500

    Inital setup, copy files on root dir

--- app/server.js
+++ app/server.js
@@ -7,7 +7,31 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-http.createServer(function (request, response) {
+var brands;
+fs.readFile('brands.json', (err, data) => {
+  if (err) throw err;
+  brands = JSON.parse(data);
+  // brand = brands[0];
+});
+var products;
+fs.readFile('products.json', (err, data) => {
+  if (err) throw err;
+  products = JSON.parse(data);
+  // brand = brands[0];
+});
+var users;
+fs.readFile('users.json', (err, data) => {
+  if (err) throw err;
+  users = JSON.parse(data);
+  // brand = brands[0];
+});
 
+http.createServer(function (req, res) {
+  myRouter(req, res, finalHandler(req, res));
 }).listen(PORT);
+console.log("Server is listening...");
+
