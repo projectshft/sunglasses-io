8909bcadbe06d30fb7dc370cfc749859d0761f71
Matthew Keller <matthewkeller@Hews-MacBook-Air.local>
2018-06-01 17:57:48 -0400

    created swagger.yaml

--- app/server.js
+++ app/server.js
@@ -1,13 +1,20 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser   = require('body-parser');
+const uid = require('rand-token').uid;
 
 const PORT = 3001;
+const myRouter = Router();
+
+const CORS_HEADERS = {
+    "Access-Control-Allow-Origin":"*"
+  ,"Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept, X-Authentication"
+  };
 
 http.createServer(function (request, response) {
 
 }).listen(PORT);
+
