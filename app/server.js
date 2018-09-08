062370a515441af3df856c65bba23333bf83b38d
AaronStearns <stearnsconcepts@gmail.com>
2018-09-08 19:55:26 -0400

    adding endpoint functions to server.js file

--- app/server.js
+++ app/server.js
@@ -19,41 +19,46 @@ http.createServer(function (request, response) {
     myRouter(request, response, finalHandler(request, response))
 }).listen(PORT);
 
-fs.readFile( __dirname + '/initial-data/brands.json', 'utf8', function (err, data) {
+// read brands json file
+fs.readFile( __dirname + '/../initial-data/brands.json', 'utf8', function (err, data) {
     if (err) {
       throw err; 
     }
-    // goals.push(data)
     brands = JSON.parse(data)
   });
 
-fs.readFile( __dirname + '/initial-data/products.json', 'utf8', function (err, data) {
+  // read products json file
+fs.readFile( __dirname + '/../initial-data/products.json', 'utf8', function (err, data) {
     if (err) {
       throw err; 
     }
-    categories = JSON.parse(data)
+    products = JSON.parse(data)
   });
 
-// GET /api/brands/:id/products
-// GET /api/products
-// POST /api/login
-
-// GET /api/me/cart
-// POST /api/me/cart
-// DELETE /api/me/cart/:productId
-// POST /api/me/cart/:productId
+  // read users json file
+  fs.readFile( __dirname + '/../initial-data/users.json', 'utf8', function (err, data) {
+    if (err) {
+      throw err; 
+    }
+    users = JSON.parse(data)
+  });
 
 // get all brands
 myRouter.get('/api/brands', function(request,response) {
   response.end(JSON.stringify(brands));
 });
 
+
 myRouter.get('/api/products', function(request,response) {
+
   response.end(JSON.stringify(user));
 });
 
 myRouter.get('/api/brands/:id/products', function(request,response) {
-
+    let brandId = brands.find((brand)=> {
+        return brand.id == request.params.id
+      }) 
+    response.end(JSON.stringify(brandId));
 });
 
 myRouter.get('/api/me/cart', function(request,response) {
@@ -74,10 +79,10 @@ myRouter.post('/api/me/cart/:productId', function(request,response) {
 
 // See how i'm not having to build up the raw data in the body... body parser just gives me the whole thing as an object.
 // See how the router automatically handled the path value and extracted the value for me to use?  How nice!
-myRouter.post('/v1/me/goals/:goalId/accept', function(request,response) {
-  let goal = goals.find((goal)=> {
-    return goal.id == request.params.goalId
-  })
-  user.acceptedGoals.push(goal); 
-  response.end();
-});
+// myRouter.post('/v1/me/goals/:goalId/accept', function(request,response) {
+//   let goal = goals.find((goal)=> {
+//     return goal.id == request.params.goalId
+//   })
+//   user.acceptedGoals.push(goal); 
+//   response.end();
+// });
