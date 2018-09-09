f1cec2ce0432ccc0b3970754a7cc91d86afcaf90
Tien Linda Vu <lindavu@Tiens-MacBook-Pro.local>
2018-09-09 16:22:59 -0400

    file systems read and initial routers set up

--- app/server.js
+++ app/server.js
@@ -10,10 +10,49 @@ const PORT = 3001;
 
 // State holding variables
 var products = [];
-var brands = [];
+var user = {};
 var users = [];
+var brands = [];
 
 
+
+// file system
+fs.readFile('products.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    products = JSON.parse(data);
+  });
+  
+fs.readFile('categories.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    brands = JSON.parse(data);
+  });
+  
+fs.readFile('users.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    user = JSON.parse(data);
+    users = user[0];
+  });
+
+
+
+// Router setup
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+// Create server
 http.createServer(function (request, response) {
-
+    myRouter(request, response, finalHandler(request, response))
 }).listen(PORT);
+
+// list main endpoints
+myRouter.get('/api/products', function(request,response) {
+    response.end(JSON.stringify(products));
+  });
+
+myRouter.get('/api/brands', function(request,response) {
+  response.end(JSON.stringify(brands));
+});
+
+myRouter.get('/api/me', function(request,response) {
+  response.end(JSON.stringify(user));
+});
