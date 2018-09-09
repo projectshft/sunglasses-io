038eb6b741ad7f1b522fdca0388494bd2e4e5020
shodan993 <paulnstanley@gmail.com>
2018-09-09 15:51:27 -0400

    adding /products functionality, not complete

--- app/server.js
+++ app/server.js
@@ -7,7 +7,42 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+const BASEURL = 'http://localhost:8080'
+
+let state = {
+    login: {
+        username: '',
+        password: ''
+    },
+    products: [],
+    brands: [],
+    cart: []
+}
 
 http.createServer(function (request, response) {
 
 }).listen(PORT);
+
+fs.readFile("../initial-data/products.json", 'utf8', (err, data)) => {
+    if (err) throw err;
+    state.products.push(data);
+}
+
+function productsGET() {
+	let url = `${BASEURL}/api/products`
+	return fetch(url, {
+		method: 'GET',
+		headers: {
+			'content-type': 'application/json'
+	  }
+	})
+	.then(response => {
+		return response.ok ? response.json() : response.statusText
+	})
+	.then(data => {
+		state.products.push(data)
+		renderIssues(state)
+		// render data
+	})
+	.catch(err => console.log(err))
+}
