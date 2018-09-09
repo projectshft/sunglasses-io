c530155921e826fb9ed09346b684bc2774f8fe10
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-09-09 18:41:44 -0400

    generate api keys, add endpoint for brands and test that brands is returning array.

--- app/server.js
+++ app/server.js
@@ -8,6 +8,12 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
+// // Created with https://www.uuidgenerator.net/
+// const VALID_API_KEYS = ["e633b606 - b480 - 11e8 - 96f8 - 529269fb1459",
+// "e633b89a - b480 - 11e8 - 96f8 - 529269fb1459",
+// "e633b9ee - b480 - 11e8 - 96f8 - 529269fb1459"];
+
+
 //State holding variables
 var brands = [];
 var users = [];
@@ -29,15 +35,22 @@ http.createServer(function (request, response) {
     brands = JSON.parse(data);
     console.log(`Server setup: ${brands.length} brands loaded`);
   });
-  fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
-    if (error) throw error;
-    users = JSON.parse(data);
-    console.log(`Server setup: ${users.length} users loaded`);
-  });
   fs.readFile('initial-data/products.json', 'utf8', function (error, data) {
     if (error) throw error;
     products = JSON.parse(data);
     console.log(`Server setup: ${products.length} products loaded`);
   });
+  fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
   console.log(`Server is listening on ${PORT}`);
 });
+
+//the GET/api/brands does not take parameters and sends back an array of brands
+myRouter.get('/api/brands', (request, response) => {
+  response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }));
+  response.end(JSON.stringify(brands));
+});
+
