944eb4333193f1051f9a413474c4e5c13dca5391
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-09-09 14:30:21 -0400

    adding state holding variables and setting server to recognize fs data

--- app/server.js
+++ app/server.js
@@ -8,16 +8,36 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
+//State holding variables
+var brands = [];
+var users = [];
+var products = [];
+
 // Setup router
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
 //create server and set it to listen to the port variable value
 http.createServer(function (request, response) {
-  if (request.url.includes('api')) {
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    myRouter(request, response, finalHandler(request, response))
-  } else {
-    serve(request, response, finalHandler(request, response))
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error on Server Startup: ', error)
   }
-}).listen(PORT);
+  fs.readFile('initial-data/brands.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+  fs.readFile('initial-data/products.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+  console.log(`Server is listening on ${PORT}`);
+});
