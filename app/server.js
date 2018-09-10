a079f9332908fddd4e92f47fa0744f816cc1accf
Tien Linda Vu <lindavu@Tiens-MacBook-Pro.local>
2018-09-10 09:53:27 -0400

    products and brands routes tested and working

--- app/server.js
+++ app/server.js
@@ -17,22 +17,21 @@ var brands = [];
 
 
 // file system
-fs.readFile('products.json', 'utf8', (err, data) => {
+fs.readFile(__dirname + '/../initial-data/products.json', 'utf8', (err, data) => {
     if (err) throw err;
     products = JSON.parse(data);
   });
   
-fs.readFile('categories.json', 'utf8', (err, data) => {
+fs.readFile(__dirname + '/../initial-data/brands.json', 'utf8', (err, data) => {
     if (err) throw err;
     brands = JSON.parse(data);
   });
   
-fs.readFile('users.json', 'utf8', (err, data) => {
+fs.readFile(__dirname + '/../initial-data/users.json', 'utf8', (err, data) => {
     if (err) throw err;
-    user = JSON.parse(data);
-    users = user[0];
+    users = JSON.parse(data);
+    user = users[0];
   });
-
 
 
 // Router setup
@@ -56,3 +55,14 @@ myRouter.get('/api/brands', function(request,response) {
 myRouter.get('/api/me', function(request,response) {
   response.end(JSON.stringify(user));
 });
+
+// See how i'm not having to build up the raw data in the body... body parser just gives me the whole thing as an object.
+// See how the router automatically handled the path value and extracted the value for me to use?  How nice!
+myRouter.post('/api/me/products/:productId/add', function(request,response) {
+    let product = products.find((product)=> {
+      return product.id == request.params.productId
+    })
+    user.addedProducts.push(product); 
+    response.end();
+  });
+  
