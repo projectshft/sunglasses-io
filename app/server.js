43e5025da907e202dfbcffbca28dc9167773c886
Lindsay Schinasi <lindsay.schinasi@gmail.com>
2018-09-09 21:44:44 -0400

    Create API login call - Still unfinished - failedLoginAttempt variable is not adding 1 each time.

--- app/server.js
+++ app/server.js
@@ -13,11 +13,12 @@ const PORT = 3001;
 // "e633b89a - b480 - 11e8 - 96f8 - 529269fb1459",
 // "e633b9ee - b480 - 11e8 - 96f8 - 529269fb1459"];
 
-
 //State holding variables
 var brands = [];
 var users = [];
 var products = [];
+var accessTokens = [];
+var failedLoginAttempts = {};
 
 // Setup router
 var myRouter = Router();
@@ -70,3 +71,55 @@ myRouter.get('/api/products', (request, response) => {
   response.end(JSON.stringify(products));
 });
 
+// POST/api/login will check to see if username and login sent in request match credentials saved in fs - user will be locked out after 3 failed login attempts
+// User login
+myRouter.post('/api/login', (request, response) => {
+if (!failedLoginAttempts[request.body.username]) {
+  failedLoginAttempts[request.body.username] = 0;
+}
+//first we make sure the client sent back the correct parameters and they haven't exceeded login attempts
+if (request.body.username && request.body.password && failedLoginAttempts[request.body.username] < 3) {
+  
+  //next match the parameters received with credentials saved in fs
+  let user = users.find((user) => {
+    return user.login.username == request.body.username && user.login.password == request.body.password;
+  });
+
+    if (user) {
+      // Reset our counter of failed logins
+      failedLoginAttempts[request.body.username] = 0;
+    
+      // Write the header because we know we will be returning  successful at this point and that the response will be json
+      response.writeHead(200, Object.assign({ 'Content-Type':   'application/json' }));
+    
+    // We have a successful login, if we already have an existing   access token, use that
+      let currentAccessToken = accessTokens.find((tokenObject) => {
+        return tokenObject.username == user.login.username;
+      });
+    
+      // Update the last updated value so we get another time period
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        // Create a new token with the user value and a "random" token
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        }
+        accessTokens.push(newAccessToken);
+        response.end(JSON.stringify(newAccessToken.token));
+      } 
+    } else {
+      let numFailedForUser = failedLoginAttempts[request.body.username];
+      if (numFailedForUser) {
+        failedLoginAttempts[request.body.username]++;
+      } else {
+        failedLoginAttempts[request.body.username] = 0
+      }
+      response.writeHead(401, "Invalid username or password");
+      response.end();
+    }
+  }
+}); 
