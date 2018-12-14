03e82257b4f6deda4f116d7447dbb16741760bd1
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-12-14 13:40:26 -0500

    Add swagger document. Set-up server to populate internal state from json files. Create test file. Ready to begin writing tests.

--- app/server.js
+++ app/server.js
@@ -3,11 +3,44 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser  = require('body-parser');
+const url = require("url");
 var uid = require('rand-token').uid;
+
+// State holding variables
+let brands = [];
+let products = {};
+let users = [];
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// Setup router
+const router = Router();
+router.use(bodyParser.json());
 
-}).listen(PORT);
+const server = http.createServer((request, response) => {
+   res.writeHead(200);
+   router(req, res, finalHandler(req, res));
+});
+
+server.listen(PORT, err => {
+   if (err) throw err;
+   console.log(`server running on port ${PORT}`);
+   //populate brands
+   fs.readFile("initial-data/brands.json", "utf-8", (err, data) => {
+     if (err) throw err;
+     brands = JSON.parse(data);
+   });
+   //populate products
+   fs.readFile("initial-data/products.json", "utf-8", (err, data) => {
+     if (err) throw err;
+     products = JSON.parse(data);
+   });
+   //populate users
+   fs.readFile("initial-data/users.json", "utf-8", (err, data) => {
+     if (err) throw err;
+     users = JSON.parse(data);
+     //hardcoded user
+     user = users[0];
+   });
+})
