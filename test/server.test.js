df79d3ec5607197089e5b4f56d3eff0fde686c06
Matt Fine <mfine@wesleyan.edu>
2018-12-14 10:28:45 -0500

    Set up beginnings for testing suite

--- test/server.test.js
+++ test/server.test.js
@@ -8,9 +8,53 @@ const assert = chai.assert;
 chai.use(chaiHTTP);
 chai.use(require('chai-sorted'));
 
-//GET BRANDS
+//GET /brands test
 describe('/GET brands', () => {
   it.only('should GET all brands', done => {
-    
+
+  })
+})
+//GET brands/:id/products test
+describe('/GET products within a brand', () => {
+  it.only('should GET all products from a specific brand', done => {
+
+  })
+})
+
+//GET api/products test
+describe('/GET products', () => {
+  it.only('should GET all products', done => {
+
+  })
+})
+
+//POST api/login test
+describe('/POST login', () => {
+  it.only('should allow a user to login', done => {
+
+  })
+})
+//GET api/me/cart test
+describe('/GET cart', () => {
+  it.only("should GET the currently logged in user's current cart", done => {
+
+  })
+})
+//POST api/me/cart test
+describe('/POST cart', () => {
+  it.only("should POST the cart to the user's information", done => {
+
+  })
+})
+//DELETE api/me/cart/:productId test
+describe('/DELETE product from cart', () => {
+  it.only("should DELETE an existing item from the user's cart", done => {
+
+  })
+})
+//POST api/me/cart/:productId test
+describe('POST product to cart', () => { 
+  it.only("should POST a new item into the logged-in user's cart", done => {
+
   })
 })
