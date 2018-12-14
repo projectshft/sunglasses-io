2bbcec81231938d699c38caae39cb5a610c35def
Rhys Lutsky <rhyslutsky@gmail.com>
2018-12-14 16:54:10 -0500

    get brands test to fail

--- app/server.js
+++ app/server.js
@@ -1,13 +1,53 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser   = require('body-parser');
+const uid = require('rand-token').uid;
+
+// State holding variables
+let brands = [];
+let users = [];
+let products = [];
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// Setup router
+const router = Router();
+router.use(bodyParser.json());
 
-}).listen(PORT);
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    router(req, res, finalHandler(req, res));
+}).listen(PORT, err => {
+    if (err) throw err;
+    console.log(`server running on port ${PORT}`);
+
+    //populate brands
+    brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf8"));
+});
+
+
+router.get("/api/brands", (req, res) => {
+    const parsedUrl = url.parse(request.originalUrl);
+    const { query, sort } = querystring.parse(parsedUrl.query);
+    let brandsToReturn = [];
+    if (query !== undefined) {
+        brandsToReturn = brands.filter(brand => brand.description.includes(query));
+
+        if (!brandsToReturn) {
+            res.writeHead(404, "There aren't any brands to return");
+            return res.end();
+        }
+    } else {
+        brandsToReturn = brands;
+    }
+    // if (sort !== undefined) {
+    //     brandsToReturn.sort((a, b) => a[sort] - b[sort]);
+    // }
+    res.writeHead(200, { "Content-Type": "application/json" });
+    return res.end(JSON.stringify(brandsToReturn));
+})
+
+module.exports = server;
---
+++ test/server.test.js
@@ -0,0 +1,23 @@
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const server = require("../app/server");
+const expect = chai.expect;
+const assert = chai.assert;
+
+chai.use(chaiHTTP);
+
+//GET Brands
+describe("/GET brands", () => {
+    it("should return a list of all brands", done => {
+        chai
+            .request(server)
+            .get("/api/brands")
+            .end((err, res) => {
+                assert.exists(res, 'res.body is neither `null` nor `undefined`');
+                // assert(res).to.have.status(200);
+                // assert("Content-Type", "application/json");
+                // expect(res.body).to.be.an("array");
+                done();
+            });
+    });
+})
