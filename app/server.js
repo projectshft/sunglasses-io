5a7a1c7e15e8152c5a6736e80f1a668056de21c0
Alicia Hetrick <ahetrick22@gmail.com>
2018-12-14 09:14:06 -0500

    2nd test written, followed by necessary server code to make it pass: /api/brands/:id/products

--- app/server.js
+++ app/server.js
@@ -6,36 +6,38 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 const fs = require('fs');
 const CONTENT_HEADERS = {"Content-Type": "application/json"};
-
 const PORT = 3001;
 
 // Setup router
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
+//
+let brands, products, users;
+
 http.createServer(function (req, res) {
-    myRouter(req, res, finalHandler(req, res))
+  myRouter(req, res, finalHandler(req, res))
 }).listen(PORT, error => {
-    if (error) {
-      return console.log("Error on Server Startup: ", error);
-    }
-    fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
-        if (error) throw error;
-        brands = JSON.parse(data);
-        console.log(`Server setup: ${brands.length} brands loaded`);
-      });
-      fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
-        if (error) throw error;
-        users = JSON.parse(data);
-        console.log(`Server setup: ${users.length} users loaded`);
-      });
-      fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
-        if (error) throw error;
-        products = JSON.parse(data);
-        console.log(`Server setup: ${products.length} products loaded`);
-        });
-    console.log(`Server is listening on ${PORT}`);
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
+  }
+  fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
   });
+  fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+  fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+    });
+  console.log(`Server is listening on ${PORT}`);
+});
 
 myRouter.get('/api/brands', (req,res) => {
     res.writeHead(200, CONTENT_HEADERS);
@@ -43,7 +45,9 @@ myRouter.get('/api/brands', (req,res) => {
 }) 
 
 myRouter.get('/api/brands/:id/products', (req,res) => {
-
+  res.writeHead(200, CONTENT_HEADERS);
+  const productsByBrand = products.filter(product => product.brandId == req.params.id);
+  res.end(JSON.stringify(productsByBrand));
 }) 
 
 myRouter.get('/api/products', (req,res) => {
--- test/test.js
+++ test/test.js
@@ -6,32 +6,96 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-let brands, products, users;
-
-describe('/api/brands', () => {
+describe('GET /api/brands', () => {
     it('should GET all the brands', (done) => {
-        let fileBrands;
+        let testBrands;
         fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
             if (error) throw error;
-            fileBrands = JSON.parse(data);
-            console.log(`Server setup: ${fileBrands.length} brands loaded`);
+            testBrands = JSON.parse(data);
           });
         chai.request("http://localhost:3001")
        .get('/api/brands')
        .end((err, res) => {
         res.should.have.status(200)
-        console.log(res.body.length);
         res.body.should.be.an('array')
-        res.body.length.should.be.eql(fileBrands.length);
+        res.body.length.should.be.eql(testBrands.length);
        done();
      })
     })
   })
 
-// GET /api/brands/:id/products
-// GET /api/products
-// POST /api/login
-// GET /api/me/cart
-// POST /api/me/cart
-// DELETE /api/me/cart/:productId
-// POST /api/me/cart/:productId
+describe('GET /api/brands/:id/products', () => {
+    it('should GET all the products of a given brand', (done) => {  
+        let testProducts;
+        fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+            if (error) throw error;
+            testProducts = JSON.parse(data).filter(product => product.brandId == 1);
+        });
+        chai.request("http://localhost:3001")
+       .get('/api/brands/1/products')
+       .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.an('array')
+        res.body.length.should.be.eql(testProducts.length);
+       done();
+     })
+    })
+})
+
+// describe('GET /api/products', () => {
+//     it('should GET all the products in order by decreasing price', (done) => {  
+
+//     })
+// })
+
+// describe('POST /api/login', () => {
+//     it('should POST a username and password and allow login if valid', (done) => {  
+
+//     })
+//     it('should reject an invalid user name and password', (done) => {  
+
+//     })
+// })
+
+// describe('GET /api/me/cart', () => {
+//     it('should GET all the products in the logged in user\'s cart with their quantities' , (done) => {  
+
+//     })
+// })
+
+
+// describe('POST /api/me/cart', () => {
+//     it('should POST a selected product and add it to the logged in user\'s cart' , (done) => {  
+
+//     })
+
+//     it('should allow multiples of the same product, each with their own quantity' , (done) => {  
+
+//     })
+
+//     it('should adjust the quanitity on that item to 1' , (done) => {  
+
+//     })
+// })
+
+// describe('DELETE /api/me/cart/:productId', () => {
+//     it('should DELETE a selected product from the logged in user\'s cart' , (done) => {  
+
+//     })
+
+//     it('should leave all other items in the cart untouched' , (done) => {  
+
+//     })
+// })
+
+// describe('POST /api/me/cart/:productId', () => {
+//     it('should POST a selected product\'s quantity to update it in the logged in user\'s cart' , (done) => {  
+
+//     })
+
+//     it('should leave all other items in the cart untouched' , (done) => {  
+
+//     })
+// })
+
+
