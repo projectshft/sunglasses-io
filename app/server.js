45f4974d2d4cd4cd3a51a066057a88c5165eb4f6
Edward Barton <edward.barton@icloud.com>
2018-12-15 13:42:29 -0500

    GET brands route with test

--- app/server.js
+++ app/server.js
@@ -1,13 +1,48 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const uid = require('rand-token').uid;
 
+// State
+let brands = [];
+let products = [];
+let users = [];
+let accessTokens = [];
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+fs.readFile('initial-data/brands.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  brands = JSON.parse(data);
+});
 
-}).listen(PORT);
+fs.readFile('initial-data/products.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  products = JSON.parse(data);
+});
+
+fs.readFile('initial-data/users.json', 'utf-8', (err, data) => {
+  if (err) throw err;
+  users = JSON.parse(data);
+});
+
+// Setup router
+const router = Router();
+router.use(bodyParser.json());
+
+// Setup server
+const server = http
+  .createServer((req, res) => {
+    res.writeHead(200);
+    router(req, res, finalHandler(req, res));
+  })
+  .listen(PORT);
+
+router.get('/api/brands', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application:json' });
+  res.end(JSON.stringify(brands));
+});
+
+module.exports = server;
--- test/server-test.js
+++ test/server-test.js
@@ -5,3 +5,32 @@ let server = require('../app/server');
 let should = chai.should();
 
 chai.use(chaiHttp);
+
+describe('Sunglasses', () => {
+  // Brands
+  describe('Brands', () => {
+    describe('/GET brands', () => {
+      it('should GET all brands', done => {
+        chai
+          .request(server)
+          .get('/api/brands')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(5);
+            done();
+          });
+      });
+    });
+  });
+
+  // Products
+  describe('', () => {
+    
+  });
+
+  // Users
+  describe('', () => {
+    
+  });
+});
