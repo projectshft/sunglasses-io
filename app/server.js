ffa2b87ba23d4fd7833454fb3d82454d2dbb7eee
Matt Fine <mfine@wesleyan.edu>
2018-12-15 00:30:33 -0500

    Successfully get all brands test to pass

--- app/server.js
+++ app/server.js
@@ -5,9 +5,78 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
+const url = require('url');
+//make the documentation match the code and make the code match the tests
+//each of the end-points that you represent via the API yaml file should be represented in the final test suite
 
-const PORT = 3001;
+//make 400 404 and 401 Error
 
-http.createServer(function (request, response) {
+const PORT = process.env.PORT || 3005;
 
-}).listen(PORT);
+//State holding variables
+let brands = [];
+let products = [];
+let users = [];
+let user = {};
+
+//router set up
+const router = Router();
+router.use(bodyParser.json());
+
+//server set up
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  router(req, res, finalHandler(req, res));
+});
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`Server running on ${PORT}`);
+  //populate brands
+   brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf8"));
+  // console.log(brands)
+  // console.log(brands.length);
+  // fs.readFile("./initial-data/brands.json", 'utf-8', (err, data) => {
+  //   if (err) throw err;
+  //   brands = JSON.parse(data);
+  // });
+
+  //populate products
+  products = JSON.parse(fs.readFileSync('./initial-data/products.json', 'utf8'));
+  // fs.readFile('./initial-data/products.json', 'utf-8', (err, data) => {
+  //   if (err) throw err;
+  //   products = JSON.parse(data);
+  // });
+
+  //populate users
+  users = JSON.parse(fs.readFileSync('./initial-data/users.json', 'utf8'));
+  // fs.readFile('./initial-data/users.json', 'utf-8', (err, data) => {
+  //   if (err) throw err;
+  //   users = JSON.parse(data);
+  let user = users[0];
+  // });
+});
+
+
+//GET Brands Endpoint
+  router.get("/v1/brands", (req, res) => {
+    const parsedUrl = url.parse(req.originalUrl);
+    const { query } = queryString.parse(parsedUrl.query);
+    let brandsToReturn = [];
+    //if there is an empty query all brands should be returned otherwise
+    //the brand returned should be the query in question
+    if (query !== undefined) {
+      brandsToReturn = brands.filter(brand => brand.name.includes(query))
+      if (!brandsToReturn) {
+        res.writeHead(404, `ERROR: PRODUCT DOES NOT EXIST`);
+        res.end();
+      }
+        brandsToReturn = brands;
+    }
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(brands))
+  });
+//REMEMBER ACCESS FOR SWAGGER DOCUMENTATION
+//SECURITY DEFINITIONS
+
+module.exports = server;
--- test/server.test.js
+++ test/server.test.js
@@ -11,50 +11,86 @@ chai.use(require('chai-sorted'));
 //GET /brands test
 describe('/GET brands', () => {
   it.only('should GET all brands', done => {
-
+    chai
+    .request(server)
+    .get('/v1/brands')
+    .end((err, res) => {
+      assert.isNotNull(res.body);
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect("Content-Type", 'application/json');
+      expect(res.body).to.be.an('array');
+      expect(res.body).to.have.lengthOf(5);
+      done();
+    })
   })
+  it.only('should limit results to those with a query string', done => {
+    chai
+    .request(server)
+    .get('/v1/brands?query=Visit')
+    .end((err, res) => {
+      assert.isNotNull(res.body)
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      expect('Content-Type', 'application/json');
+      expect(res.body).to.be.an('array');
+      expect(res.body).to.have.lengthOf(1)
+    })
+  })
+  // it.only('returns all goals if query is missing', done => {
+  //   chai.request(server)
+  //   .get('v1/goals?query=')
+  //   .end((err, res) => {
+  //     expect(err).to.be.null;
+  //     expect(res).to.have.status(200);
+  //     expect("Content-Type", "application/json");
+  //     expect(res.body).to.be.an("array");
+  //     expect(res.body).to.have.lengthOf(5);
+  //     done();
+  //   })
+  // })
 })
 //GET brands/:id/products test
-describe('/GET products within a brand', () => {
-  it.only('should GET all products from a specific brand', done => {
+// describe('/GET products within a brand', () => {
+//   it.only('should GET all products from a specific brand', done => {
 
-  })
-})
+//   })
+// })
 
-//GET api/products test
-describe('/GET products', () => {
-  it.only('should GET all products', done => {
+// //GET api/products test
+// describe('/GET products', () => {
+//   it.only('should GET all products', done => {
 
-  })
-})
+//   })
+// })
 
-//POST api/login test
-describe('/POST login', () => {
-  it.only('should allow a user to login', done => {
+// //POST api/login test
+// describe('/POST login', () => {
+//   it.only('should allow a user to login', done => {
 
-  })
-})
-//GET api/me/cart test
-describe('/GET cart', () => {
-  it.only("should GET the currently logged in user's current cart", done => {
+//   })
+// })
+// //GET api/me/cart test
+// describe('/GET cart', () => {
+//   it.only("should GET the currently logged in user's current cart", done => {
 
-  })
-})
-//POST api/me/cart test
-describe('/POST cart', () => {
-  it.only("should POST the cart to the user's information", done => {
+//   })
+// })
+// //POST api/me/cart test
+// describe('/POST cart', () => {
+//   it.only("should POST an item to the cart", done => {
 
-  })
-})
-//DELETE api/me/cart/:productId test
-describe('/DELETE product from cart', () => {
-  it.only("should DELETE an existing item from the user's cart", done => {
+//   })
+// })
+// //DELETE api/me/cart/:productId test
+// describe('/DELETE product from cart', () => {
+//   it.only("should DELETE an existing item from the user's cart", done => {
 
-  })
-})
-//POST api/me/cart/:productId test
-describe('POST product to cart', () => { 
-  it.only("should POST a new item into the logged-in user's cart", done => {
+//   })
+// })
+// //POST api/me/cart/:productId test
+// describe('POST product to cart', () => { 
+//   it.only("should update the product amount in the cart", done => {
 
-  })
-})
+//   })
+// })
