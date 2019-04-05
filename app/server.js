9a19760f638cb7fa49324b459b2ec073f3c4022c
Richard Eldridge <eldridge.rich@gmail.com>
2019-04-05 16:08:38 -0400

    initial commit - yaml file first draft complete

--- app/server.js
+++ app/server.js
@@ -1,13 +1,50 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const { uid } = require('rand-token');
 
 const PORT = 3001;
+const myURL = 'https://www.sunglasses.io/products?test=true&another_test=false';
+const parsedURL = url.parse(myURL).query;
+console.log(parsedURL);
 
-http.createServer(function (request, response) {
+http.createServer(function(request, response) {}).listen(PORT);
 
-}).listen(PORT);
+// const shoppingCarts = {
+//   userId: [
+//     {
+//       quantity: 1,
+//       product: {
+//         id: '2',
+//         brandId: '1',
+//         name: 'Black Sunglasses',
+//         description: 'The best glasses in the world',
+//         price: 100,
+//         imageUrls: [
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg',
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg',
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg'
+//         ]
+//       }
+//     },
+//     {
+//       quantity: 1,
+//       product: {
+//         id: '1',
+//         brandId: '1',
+//         name: 'Superglasses',
+//         description: 'The best glasses in the world',
+//         price: 150,
+//         imageUrls: [
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg',
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg',
+//           'https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg'
+//         ]
+//       }
+//     }
+//   ]
+// };
