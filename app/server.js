968713e3fbeda822fcd7396103a84aff3e196ed3
Jason Doss <doss2k@gmail.com>
2019-04-05 08:51:53 -0400

    Create basic server

--- app/server.js
+++ app/server.js
@@ -6,8 +6,38 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
-const PORT = 3001;
+const PORT = 3001; // Port for server to listen on
+
+// State holding variables
+let brands = [];
+let products = [];
+let users = [];
+
+// Setup router
+const myRouter = Router();
+myRouter.use(bodyParser.json());
 
 http.createServer(function (request, response) {
-
-}).listen(PORT);
+  myRouter(request, response, finalHandler(request, response));
+})
+.listen(PORT, error => {
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
+  }
+  fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+  fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+  console.log(`Server is listening on ${PORT}`);
+});
