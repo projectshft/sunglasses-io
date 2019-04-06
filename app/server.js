ecc7e62a0715bb70a7f7ce016911cc9bb425b72b
mcstreilein <madelinestreilein@Madelines-MacBook-Air.local>
2019-04-05 21:56:45 -0400

    attempting to create GET /brands/{brandId}/products route

--- app/server.js
+++ app/server.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser')
 var uid = require('rand-token').uid
 
 const PORT = 3001
+let brands = []
+let products = []
 
 // Setup router
 var myRouter = Router()
@@ -20,16 +22,20 @@ const server = http
     if (error) {
       throw error
     }
-    fs.readFile('initial-data/brands.json', 'utf8', (error, data) => {
-      if (error) throw error
-      brands = JSON.parse(data)
-      console.log(`Server setup: ${brands.length} brands loaded`)
-    })
-    fs.readFile('initial-data/products.json', 'utf8', (error, data) => {
-      if (error) throw error
-      products = JSON.parse(data)
-      console.log(`Server setup: ${products.length} products loaded`)
-    })
+
+    brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf8'))
+
+    products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf8'))
+    // fs.readFile('initial-data/brands.json', 'utf8', (error, data) => {
+    //   if (error) throw error
+    //   brands = JSON.parse(data)
+    //   console.log(`Server setup: ${brands.length} brands loaded`)
+    // })
+    // fs.readFile('initial-data/products.json', 'utf8', (error, data) => {
+    //   if (error) throw error
+    //   products = JSON.parse(data)
+    //   console.log(`Server setup: ${products.length} products loaded`)
+    // })
   })
 
 //get all brands
@@ -37,26 +43,48 @@ myRouter.get('/brands', (request, response) => {
   response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }))
   response.end(JSON.stringify(brands))
 })
+//get all products by brandID
+myRouter.get('/brands/:brandId/products', (request, response) => {
+  //filter for the products with the appropriate brand Id
+  let brandProducts = products.filter(product => {
+    return product.brandId == request.params.brandId
+  })
+  //if there are no products with the brand Id, a 401 error should be thrown
+  if (!brandProducts) {
+    response.writeHead(401, 'No products with that brand Id found.')
+  }
+  //if there are products with the brandId, send them
+  response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }))
+  response.end(JSON.stringify(brandProducts))
+})
 //get products by productName
 myRouter.get('/products', (request, response) => {
   let query = queryString.parse(request.url.substring(10))
 
+  //if the user enters an empty string in the search bar, a 403 error should be thrown
   if (Object.keys(query).length == 0) {
-    response.writeHead(401, 'Please enter a product')
+    response.writeHead(
+      402,
+      'Please enter the name of the product you are searching for.'
+    )
     response.end()
     return
   }
+  //match productName in query to the productNames in products.json
   let product = products.find(product => {
     return product.productName === query.productName
   })
+  //if there is no product with that productName, a 403 error should be thrown
   if (!product) {
     response.writeHead(403, 'The product does not exist')
     response.end()
     return
   }
   response.writeHead(200, Object.assign({ 'Content-Type': 'application/json' }))
+  //if there is a product with the queried productName, return the product
   response.end(JSON.stringify(product))
   return
 })
 
+//export the server so that tests can be written
 module.exports = server
--- test/test.js
+++ test/test.js
@@ -6,20 +6,46 @@ let should = chai.should()
 
 chai.use(chaiHttp)
 
-//let server = require('../app/server')
-describe('/GET brands', () => {
-  it('it should GET all the brands', done => {
-    chai
-      .request(server)
-      .get('/brands')
-      .end((error, response) => {
-        response.should.have.status(200)
-        response.body.should.be.an('array')
-        response.body.length.should.be.eq(5)
-        done()
-      })
+describe('Brands', () => {
+  describe('/GET brands', () => {
+    it('it should GET all the brands', done => {
+      chai
+        .request(server)
+        .get('/brands')
+        .end((error, response) => {
+          response.should.have.status(200)
+          response.body.should.be.an('array')
+          response.body.length.should.be.eq(5)
+          done()
+        })
+    })
+  })
+  describe('/GET brands/:brandId/products', () => {
+    it('it should GET all the products by brandID', done => {
+      chai
+        .request(server)
+        .get('/brands/3/products')
+        .end((error, response) => {
+          response.should.have.status(200)
+          response.body.should.be.an('array')
+          response.body.length.should.be.eq(2)
+          done()
+        })
+    })
+    it('it should return an error if there is no product with that brand Id', done => {
+      chai
+        .request(server)
+        .get('/brands/7/products')
+        .end((error, response) => {
+          response.should.have.status(401)
+          response.body.should.be.an('object')
+          response.body.should.not.have.property('brandId')
+          done()
+        })
+    })
   })
 })
+
 describe('Products', () => {
   describe('/GET products', () => {
     it('it should return an error if the query is an empty string', done => {
@@ -27,7 +53,7 @@ describe('Products', () => {
         .request(server)
         .get('/products')
         .end((error, response) => {
-          response.should.have.status(401)
+          response.should.have.status(402)
           response.body.should.be.an('object')
           response.body.should.not.have.property('productName')
           done()
