f7ef792536dbeffc8d2833fe943a3f82e1a71066
Knox <knox.brand@gmail.com>
2019-04-07 08:39:58 -0400

    Brands and Products paths/tests working

--- app/server.js
+++ app/server.js
@@ -39,19 +39,23 @@ http
     console.log(`Server is listening on ${PORT}`);
   });
 
-myRouter.get("/", (request, response) => {
-  response.end("server working");
-});
-
+//Returning all brands
 myRouter.get("/api/brands", function(request, response) {
 		response.writeHead(200, "Successful operation", header);
-		response.statusMessage
     response.end(JSON.stringify(brands));
 });
 
+//Returning all products based on brandId
 myRouter.get("/api/brands/:brandId/products", function(request, response) {
+	requestedId = parseInt(request.params.brandId); 
+	//All valid ids should be capable of being parsed to an int. If the input is NaN we know it's bad input.
+	if (isNaN(requestedId) || requestedId < 1) { 
+		response.writeHead(400, "Invalid brandId supplied");
+    response.end();
+  }
+  //Checking brands data to see if brandId exists.
   requestedBrandProducts = products.filter(
-    product => product.categoryId === request.params.brandId
+  product => product.categoryId === request.params.brandId
   );
   if (requestedBrandProducts.length != 0) {
     response.writeHead(200, "Successful operation", header);
@@ -60,4 +64,39 @@ myRouter.get("/api/brands/:brandId/products", function(request, response) {
     response.writeHead(404, "Brand id not found");
     response.end();
   }
+});
+
+//Returning all products if no query string. If query exists then we search brands and products for a match.
+myRouter.get("/api/products", function(request, response) {
+	const queryData = url.parse(request.url).query;
+	searchString = queryString.parse(queryData);
+	if (Object.keys(searchString).length === 0) {
+		response.writeHead(200, "Successful operation", header);
+		response.end(JSON.stringify(products));
+	} 
+	//Since call has query data, check to see if length is as specified.
+	if (searchString.query.length >= 1 && searchString.query.length <= 50) {
+		//Search brands to get relevant brandId
+		brandId = brands.find(brand => brand.name.toLowerCase() == searchString.query.toLowerCase() )
+		//Search products for results
+		if (brandId) {
+			results =	products.filter(product => product.categoryId == brandId.id)
+			response.writeHead(200, "successful operation", header);
+			response.end(JSON.stringify(results));
+		} else {
+			results = products.filter(product => product.name.toLowerCase() == searchString.query.toLowerCase())
+			if (results.length > 0) {
+			response.writeHead(200, "successful operation", header);
+			response.end(JSON.stringify(results));
+			} else {
+				response.writeHead(404, "No results found.");
+				response.end();
+			}
+		}
+		//query length was not valid
+	} else {
+		response.writeHead(400, "Invalid search");
+		response.end();
+	}
+	
 });
--- app/test.js
+++ app/test.js
@@ -6,8 +6,8 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-//GET all Brands - should return 200 Successful operation.
 describe("/GET brands", () => {
+  //GET all Brands - should return 200 Successful operation.
   it("it should GET all the brands", done => {
     chai
       .request(server)
@@ -24,8 +24,8 @@ describe("/GET brands", () => {
   });
 });
 
-//GET all products of a specific brandId - successful - should return 200 Successful operation
 describe("/GET /brands/{brandId}/products", () => {
+  //GET all products of a specific brandId - successful - should return 200 Successful operation
   it("it should GET all brandId products", done => {
     chai
       .request(server)
@@ -50,265 +50,242 @@ describe("/GET /brands/{brandId}/products", () => {
         done();
       });
   });
-});
-
-//GET all products of a specific brandId - invalid input - should return 400 Invalid brandId supplied.
-describe("/GET /brands/{brandId}/products", () => {
+  //GET all products of a specific brandId - invalid input - should return 400 Invalid brandId supplied.
   it("it should return 400", done => {
     chai
       .request(server)
-      .get("/api/brands/1/products")
+      .get("/api/brands/potato/products")
       .end((err, res) => {
         res.should.have.status(400);
-        res.should.be.json;
-        res.body.should.be.an("array");
-        res.body[0].should.have.deep.keys(
-          "id",
-          "categoryId",
-          "name",
-          "description",
-          "price",
-          "imageUrls"
-        );
-        res.body[0].should.have.deep.property("name", "Superglasses");
-        res.body[0].should.have.deep.property(
-          "description",
-          "The best glasses in the world"
-        );
         done();
       });
   });
-});
-
-//GET all products of a specific brandId - brandId does not exist - should return 404 brandId not found.
-describe("/GET /brands/{brandId}/products", () => {
+  //GET all products of a specific brandId - brandId does not exist - should return 404 brandId not found.
   it("it should return 404", done => {
     chai
       .request(server)
-      .get("/api/brands/1/products")
+      .get("/api/brands/7/products")
       .end((err, res) => {
-        res.should.have.status(400);
-        res.should.be.json;
-        res.body.should.be.an("array");
-        res.body[0].should.have.deep.keys(
-          "id",
-          "categoryId",
-          "name",
-          "description",
-          "price",
-          "imageUrls"
-        );
-        res.body[0].should.have.deep.property("name", "Superglasses");
-        res.body[0].should.have.deep.property(
-          "description",
-          "The best glasses in the world"
-        );
+        res.should.have.status(404);
         done();
       });
   });
 });
 
-//GET all products - should return 200 - Successful operation.
 describe("/GET /products", () => {
+  //GET all products - should return 200 - Successful operation.
   it("it should GET all products", done => {
     chai
       .request(server)
       .get("/api/products")
       .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an("array");
+        res.body[0].should.have.deep.keys(
+          "id",
+          "categoryId",
+          "name",
+          "description",
+          "price",
+          "imageUrls"
+        );
+        res.body[0].should.have.deep.property("name", "Superglasses");
+        res.body[0].should.have.deep.property(
+          "description",
+          "The best glasses in the world"
+        );
         done();
       });
   });
-});
-
-//GET all products - optional query product results based off string query - should return 200 Successful operation.
-describe("/GET /products", () => {
+  //GET all products - optional query product results based off string query - should return 200 Successful operation.
   it("it should GET all products related to the query", done => {
     chai
       .request(server)
-      .get("api/products?query=asdf")
+      .get("/api/products?query=dkny")
       .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an("array");
+        res.body[0].should.have.deep.keys(
+          "id",
+          "categoryId",
+          "name",
+          "description",
+          "price",
+          "imageUrls"
+        );
+        res.body[0].should.have.deep.property("name", "Coke cans");
+        res.body[0].should.have.deep.property(
+          "description",
+          "The thickest glasses in the world"
+        );
         done();
       });
   });
-});
-
-//GET all products - optional query product results based off invalid string query - should return 400 Invalid search
-describe("/GET /products", () => {
+  //GET all products - optional query product results based off invalid string query - should return 400 Invalid search
   it("it should return 400", done => {
     chai
       .request(server)
-      .get("api/products?query=asdf")
+      .get("/api/products?query=hellothereiamarealllllllllllllllllllllllylongstring")
       .end((err, res) => {
+        res.should.have.status(400);
         done();
       });
   });
-});
-
-//GET all products - optional query product results based off valid but non-existent string query - should return 404 No results found.
-describe("/GET /products", () => {
+  //GET all products - optional query product results based off valid but non-existent string query - should return 404 No results found.
   it("it should return 404", done => {
     chai
       .request(server)
-      .get("api/products?query=asdf")
+      .get("/api/products?query=asdf")
       .end((err, res) => {
+        res.should.have.status(404);
         done();
       });
   });
 });
 
-//GET contents of users cart while logged in - should return 200 Successful operation.
-describe("GET /me/cart", () => {
-  it("should get user cart contents", done => {
-    chai
-      .request(server)
-      .get("/api/me/cart")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
+// describe("GET /me/cart", () => {
+//   //GET contents of users cart while logged in - should return 200 Successful operation.
+//   it("should get user cart contents", done => {
+//     chai
+//       .request(server)
+//       .get("/api/me/cart")
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//   //GET contents of users cart while not logged in - should return 403 Not authorized. User must login to view cart.
+//   it("should return 403", done => {
+//     chai
+//       .request(server)
+//       .get("/api/me/cart")
+//       .end((err, res) => {
+//         res.should.have.status(403);
+//         done();
+//       });
+//   });
+// });
 
-//GET contents of users cart while not logged in - should return 403 Not authorized. User must login to view cart.
-describe("/GET /me/cart", () => {
-  it("should return 403", done => {
-    chai
-      .request(server)
-      .get("/api/me/cart")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
+// describe("/POST /me/cart", () => {
+//   //POST to cart to update an item quantity while logged in - should return 200 Update successful.
+//   it("it should update the quantity of an existing cart item", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart")
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//   //POST to cart to update an item quantity while not logged in - should return 403 Not authorized. User must login to update cart.
+//   it("should return 403", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart")
+//       .end((err, res) => {
+//         res.should.have.status(403);
+//         done();
+//       });
+//   });
+//   //POST to cart to update an item quantity while logged in, but invalid productId or quantity - should return 400 Invalid product id or quantity.
+//   it("should return 400", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart")
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         done();
+//       });
+//   });
+// });
 
-//POST to cart to update an item quantity while logged in - should return 200 Update successful.
-describe("/POST /me/cart", () => {
-  it("it should update the quantity of an existing cart item", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
+// describe("/DELETE /me/cart/{productId}", () => {
+//   //DELETE item from cart successfully - should return 200 Successful operation
+//   it("it should delete the item from the user's cart", done => {
+//     chai
+//       .request(server)
+//       .delete("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//   //DELETE item from cart - user not logged in - should return 401 Not authorized. User must login to update cart.
+//   it("should return 401", done => {
+//     chai
+//       .request(server)
+//       .delete("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         done();
+//       });
+//   });
+//   //DELETE item from cart - invalid productId - should return 400 productId invalid or not found in cart.
+//   it("should return 400", done => {
+//     chai
+//       .request(server)
+//       .delete("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         done();
+//       });
+//   });
+// });
 
-//POST to cart to update an item quantity while not logged in - should return 403 Not authorized. User must login to update cart.
-describe("/POST /me/cart", () => {
-  it("should return 403", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
+// describe("/POST /me/cart/{productId}", () => {
+//   //POST item to cart by productId successfully - should return 200 Successful operation.
+//   it("it should post an item to the users cart by productId", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//   //POST item to cart by productId - user not logged in - should return 401 - Not authorized. User must login to update cart.
+//   it("should return 401", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         done();
+//       });
+//   });
+//   //POST item to cart by productId - invalid productId or doesn't exist in db - should return 400 productId is invalid or doesn't exist.
+//   it("should return 400", done => {
+//     chai
+//       .request(server)
+//       .post("/api/me/cart/{productId}")
+//       .end((err, res) => {
+//         res.should.have.status(400);
+//         done();
+//       });
+//   });
+// });
 
-//POST to cart to update an item quantity while logged in, but invalid productId or quantity - should return 400 Invalid product id or quantity.
-describe("/POST /me/cart", () => {
-  it("should return 400", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//DELETE item from cart successfully - should return 200 Successful operation
-describe("/DELETE /me/cart/{productId}", () => {
-  it("it should delete the item from the user's cart", done => {
-    chai
-      .request(server)
-      .delete("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//DELETE item from cart - user not logged in - should return 401 Not authorized. User must login to update cart.
-describe("/DELETE /me/cart/{productId}", () => {
-  it("should return 401", done => {
-    chai
-      .request(server)
-      .delete("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//DELETE item from cart - invalid productId - should return 400 productId invalid or not found in cart.
-describe("/DELETE /me/cart/{productId}", () => {
-  it("should return 400", done => {
-    chai
-      .request(server)
-      .delete("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//POST item to cart by productId successfully - should return 200 Successful operation.
-describe("/POST /me/cart/{productId}", () => {
-  it("it should post an item to the users cart by productId", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//POST item to cart by productId - user not logged in - should return 401 - Not authorized. User must login to update cart.
-describe("/POST /me/cart/{productId}", () => {
-  it("should return 401", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//POST item to cart by productId - invalid productId or doesn't exist in db - should return 400 productId is invalid or doesn't exist.
-describe("/POST /me/cart/{productId}", () => {
-  it("should return 400", done => {
-    chai
-      .request(server)
-      .post("/api/me/cart/{productId}")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//POST user login with email/password - success - should return 200 Successful operation.
-describe("/POST login", () => {
-  it("it should successfully log the user in", done => {
-    chai
-      .request(server)
-      .post("/api/login")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
-//POST user login with email/password - fail - should return 401 Invalid username or password.
-describe("/POST login", () => {
-  it("should return 401", done => {
-    chai
-      .request(server)
-      .post("/api/login")
-      .end((err, res) => {
-        done();
-      });
-  });
-});
-
+// describe("/POST login", () => {
+//   //POST user login with email/password - success - should return 200 Successful operation.
+//   it("it should successfully log the user in", done => {
+//     chai
+//       .request(server)
+//       .post("/api/login")
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//   //POST user login with email/password - fail - should return 401 Invalid username or password.
+//   it("should return 401", done => {
+//     chai
+//       .request(server)
+//       .post("/api/login")
+//       .end((err, res) => {
+//         res.should.have.status(401);
+//         done();
+//       });
+//   });
+// });
