df338990d37f9501f18758f6f3714317eca17b38
Luc Saunders <lucsaunders@gmail.com>
2019-04-08 03:08:18 -0400

    Add tests and route for user login authentication.

--- app/server.js
+++ app/server.js
@@ -9,103 +9,142 @@ const url = require('url');
 
 const PORT = 3001;
 
-/*******************
- * Global variables
- *******************/
-// Create global variables to hold state (per /initial-data json files)
-let brands = [];
-let products = [];
-let users = [];
-// Create a bank of user access tokens for reference
+// Create a bank of user access tokens
 let accessTokens = [];
-// Create a variable to limit validity of access tokens to 30 minutes
-const TOKEN_VALIDITY_TIMEOUT = 30 * 60 * 1000;
+// Create an object to maintain number of failed login attempts per user
+let failedLoginAttempts = {};
 
-const redirectionMessage =
-  'Nothing to see here, please visit /api/brands or /api/products';
+/****************************************************************
+ *  Populate global state variables with /initial-data json files
+ ****************************************************************/
+// Read in brands.json file
+let brands = JSON.parse(fs.readFileSync('./initial-data/brands.json', 'utf8'));
+console.log(`Server setup: ${brands.length} brands loaded`);
 
-// Set up CORS headers
-const CORS_HEADERS = {
+// Read in products.json file
+let products = JSON.parse(
+  fs.readFileSync('./initial-data/products.json', 'utf8')
+);
+console.log(`Server setup: ${products.length} products loaded`);
+
+// Read in users.json file
+let users = JSON.parse(fs.readFileSync('./initial-data/users.json', 'utf8'));
+console.log(`Server setup: ${users.length} users loaded`);
+
+/*************************************************************************
+ * Helper methods to get/set number of failed requests per username
+ * ***********************************************************************/
+var getNumberOfFailedLoginRequests = username => {
+  let currentNumberOfFailedRequests = failedLoginAttempts[username];
+  if (currentNumberOfFailedRequests) {
+    return currentNumberOfFailedRequests;
+  } else {
+    return 0;
+  }
+};
+
+var setNumberOfFailedLoginRequests = (username, numFails) => {
+  failedLoginAttempts[username] = numFails;
+};
+
+/************************************************
+ * // Helper method to process access token
+ * *********************************************/
+// Create a variable to limit validity of access tokens to 15 minutes
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000;
+var getValidTokenFromRequest = request => {
+  var parsedUrl = require('url').parse(request.url, true);
+  if (parsedUrl.query.accessToken) {
+    // Verify the access token to make sure it's valid and not expired
+    let currentAccessToken = accessTokens.find(accessToken => {
+      return (
+        accessToken.token == parsedUrl.query.accessToken &&
+        new Date() - accessToken.lastUpdated < TOKEN_VALIDITY_TIMEOUT
+      );
+    });
+    // Version of above declaration, WITHOUT TIMEOUT
+    // let currentAccessToken = accessTokens.find(accessToken => {
+    //   return accessToken.token == parsedUrl.query.accessToken;
+    // });
+    if (currentAccessToken) {
+      return currentAccessToken;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
+// Set up headers
+const HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
     'Origin, X-Requested-With, Content-Type, Accept, X-Authentication'
+  // 'Content-Type': 'application/json' ???
 };
 
-// Establish router & body-parser middleware
+// Establish router and body-parser middleware
 const router = Router();
 router.use(bodyParser.json());
 
 // Establish server
 const server = http
   .createServer((request, response) => {
-    // Handle CORS Preflight request
-    if (request.method === 'OPTIONS') {
-      response.writeHead(200, CORS_HEADERS);
-      response.end();
-    }
+    // // Handle Preflight request
+    // if (request.method === 'OPTIONS') {
+    //   response.writeHead(200, HEADERS);
+    //   response.end();
+    // }
     router(request, response, finalHandler(request, response));
   })
+
+  // Begin listening on [local] port
   .listen(PORT, error => {
     if (error) {
       return console.log('Error on Server Startup: ', error);
       // throw error?;
+    } else {
+      console.log(`Server is listening on port ${PORT}`);
     }
-    /****************************************************************
-     *  Populate global state variables with /initial-data json files
-     ****************************************************************/
-    // Read in brands.json file
-    fs.readFile('initial-data/brands.json', 'utf-8', (error, data) => {
-      if (error) throw error;
-      brands = JSON.parse(data);
-      console.log(`Server setup: ${brands.length} brands loaded`);
-    });
-    // Read in products.json file
-    fs.readFile('initial-data/products.json', 'utf-8', (error, data) => {
-      if (error) throw error;
-      products = JSON.parse(data);
-      console.log(`Server setup: ${products.length} products loaded`);
-    });
-    // Read in users.json file
-    fs.readFile('initial-data/users.json', 'utf-8', (error, data) => {
-      if (error) throw error;
-      users = JSON.parse(data);
-      // user = users[0];
-      console.log(`Server setup: ${users.length} users loaded`);
-    });
-    console.log(`Server is listening on port ${PORT}`);
   });
 
 /***********************************
- *   Empty query: GET / or GET /api
+ *   GET / or GET /api
  ***********************************/
 router.get('/', (request, response) => {
-  response.writeHead(200, CORS_HEADERS);
-  // Object.assign(CORS_HEADERS, { 'Content-Type': 'application/json' })
+  response.writeHead(200, HEADERS);
+  // Object.assign(HEADERS, { 'Content-Type': 'application/json' })
   response.end(
     "There's nothing to see here, please visit /api/brands or /api/products"
   );
 });
 router.get('/api', (request, response) => {
-  response.writeHead(200, CORS_HEADERS);
-  // Object.assign(CORS_HEADERS, { 'Content-Type': 'application/json' })
+  response.writeHead(200, HEADERS);
+  // Object.assign(HEADERS, { 'Content-Type': 'application/json' })
   response.end(redirectionMessage);
 });
 
-/****************************
- *   Brands: GET /api/brands
- ****************************/
-
+/*********************************************
+ *   Brands: GET /api/brands <> Public route
+ ********************************************/
 router.get('/api/brands', (request, response) => {
-  response.writeHead(200, {
-    ...CORS_HEADERS,
-    'Content-Type': 'application/json'
-  });
-  response.end(JSON.stringify(brands));
+  if (!brands) {
+    response.writeHead(404, 'No products found');
+    response.end();
+  } else {
+    response.writeHead(200, {
+      ...HEADERS,
+      'Content-Type': 'application/json'
+    });
+    response.end(JSON.stringify(brands));
+  }
 });
 
-/********************************************************
- *   Products by brand: GET api/brands/id:/products
- ********************************************************/
+/********************************************************************
+ *   Products by brand: GET api/brands/id:/products <> Public route
+ ********************************************************************/
+// GET /api/brands/:id/products
 router.get('/api/brands/:id/products', (request, response) => {
   const { id } = request.params;
   const brandSpecificProducts = products.filter(
@@ -114,122 +153,110 @@ router.get('/api/brands/:id/products', (request, response) => {
   if (!brandSpecificProducts) {
     response.writeHead(404, 'No products found');
     response.end();
+  } else {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(brandSpecificProducts));
   }
-  response.writeHead(200, { 'Content-Type': 'application/json' });
-  response.end(JSON.stringify(brandSpecificProducts));
 });
 
-/*********************************
- *   Products: GET /api/products
- *********************************/
+/*************************************************
+ *   Products: GET /api/products <> Public route
+ *************************************************/
 router.get('/api/products', (request, response) => {
-  response.writeHead(200, {
-    ...CORS_HEADERS,
-    'Content-Type': 'application/json'
-  });
-  response.end(JSON.stringify(products));
+  if (!products) {
+    response.writeHead(404, 'No products found');
+    response.end();
+  } else {
+    response.writeHead(200, {
+      ...HEADERS,
+      'Content-Type': 'application/json'
+    });
+    response.end(JSON.stringify(products));
+  }
 });
 
-/**********************************************
- *   Specific product: GET /api/products/:id
- **********************************************/
-/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
- // THIS ONE NEEDS LOTS OF WORK (abandoned in midstream)
-/*xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx*/
-
-// router.get('/api/products/:id', (request, response) => {
-//   fs.readFile('initial-data/products.json', 'utf-8', (error, data) => {
-//     if (error) throw error;
-//     const parsedUrl = url.parse(req.originalUrl);
-//     const { query } = queryString.parse(parsedUrl.query);
-//     const { id } = request.params;
-//     if (!products.id) {
-//       response.writeHead(404, 'No product matches that id');
-//       return response.end();
-//     }
-
-//     response.writeHead(200, { 'Content-Type': 'application/json' });
-//     response.end(JSON.stringify(products.id));
-//   });
-
-//   let productToReturn;
-//   if (query !== undefined) {
-//     productToReturn = products.find(
-//       oneProduct =>
-//         oneProduct.name.includes(query) || oneProduct.id===???
-//     );
-//   res.writeHead(200, { 'Content-Type': 'application/json' });
-//   res.end(JSON.stringify(oneProduct));
-// });
-
-/*********************************
+/*****************************************
  *   User login: POST /api/login
- *********************************/
+ *****************************************/
 router.post('/api/login', (request, response) => {
-  //   const { username, password } = request.body;
-  const { username, password } = user.login;
-  if (!username || !password) {
-    response.writeHead(401, 'Invalid username or password');
+  // Ensure username and password are in the request
+  // if (request.body.username && request.body.password) {
+  if (
+    request.body.username &&
+    request.body.password &&
+    getNumberOfFailedLoginRequests(request.body.username) < 3
+  ) {
+    // Verify username and password
+    let user = users.find(user => {
+      return (
+        user.login.username == request.body.username &&
+        user.login.password == request.body.password
+      );
+    });
+    if (user) {
+      // For validated user, reset counter of failed logins
+      setNumberOfFailedLoginRequests(request.body.username, 0);
+      // Write header for successful login
+      response.writeHead(
+        200,
+        Object.assign(HEADERS, { 'Content-Type': 'application/json' })
+      );
+      // response.end();
+
+      // Check access token
+      let currentAccessToken = accessTokens.find(tokenObject => {
+        return tokenObject.username == user.login.username;
+      });
+
+      // Update value for time period
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        // Create new token with the user value and a "random" token (via rand-token)
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        };
+        accessTokens.push(newAccessToken);
+        response.end(JSON.stringify(newAccessToken.token));
+      }
+    } else {
+      // Update number of failed login attempts
+      let numFailedForUser = getNumberOfFailedLoginRequests(
+        request.body.username
+      );
+      setNumberOfFailedLoginRequests(request.body.username, numFailedForUser++);
+      response.writeHead(401, 'Invalid username or password');
+      response.end();
+    }
+  } else {
+    // Missing one of the parameters, something is wrong with formatting
+    response.writeHead(400, 'Incorrectly formatted response');
     response.end();
   }
-  response.writeHead(200, {
-    ...CORS_HEADERS,
-    'Content-Type': 'application/json'
-  });
-  /*---------------------
-    *   Access tokens
-  ----------------------*/
-  const getToken = request => {
-    const parsedUrl = url.parse(request.url, true);
-    if (parsedUrl.query.accessToken) {
-      const currentToken = accessTokens.find(
-        ({ token, lastUpdated }) =>
-          token === parsedUrl.query.accessToken &&
-          new Date() - lastUpdated < TOKEN_VALIDITY_TIMEOUT
-      );
-      return currentToken ? currentToken : null;
-    }
-    return null;
-  };
-  // Check token
-  const currentToken = accessTokens.find(({ username }) => {
-    username === user.login.username;
-  });
-
-  if (currentToken) {
-    currentToken.lastUpdated = new Date();
-    response.end(JSON.stringify(currentToken.token));
-  } else {
-    const newToken = {
-      username,
-      lastUpdated: new Date(),
-      token: uid(16)
-    };
-    accessTokens.push(newToken);
-    return response.end(JSON.stringify(newToken.token));
-  }
-
-  response.writeHead(400, 'Incorrectly formatted response');
-  response.end();
 });
 
 module.exports = server;
 
-/*******************
+/***********************
  *  All Routes
- ********************/
-/*=======DONE=======*/
+ ***********************/
+/*=========DONE========*/
 // GET /api/brands
 // GET /api/brands/:id/products
 // GET /api/products
 
 /*=======PENDING=======*/
-// GET /api/products/:id (Not original path recommended by assignment; currently not working)
 // POST /api/login
 
-/*=======TODO=======*/
+/*======ABANDONED======*/
+// GET /api/products/:id (Not original path recommended by assignment; abandoned)
+
+/*========TODO=========*/
 // GET /api/me/cart
-// POST /api/me/cart: Edit (delete) items already in the cart
+// POST /api/me/cart: Edit items already in the cart
 // DELETE /api/me/cart/:productId
 // POST /api/me/cart/:productId
 
@@ -241,3 +268,11 @@ module.exports = server;
  *  Edit Cart: POST /api/me/cart
  **********************************/
 // Edit quantity of items already in the cart)
+
+/*********************************************
+ *  Delete Items in Cart: DELETE /api/me/cart
+ ********************************************/
+
+/****************************************************
+ *  Add Items to Cart: POST /api/me/cart/:productId
+ ****************************************************/
--- test/server.test.js
+++ test/server.test.js
@@ -3,39 +3,37 @@ const server = require('../app/server');
 const chai = require('chai'),
   chaiHttp = require('chai-http'),
   expect = chai.expect;
-// should = chai.should();
-// const expect = chai.expect;
-// const assert = chai.assert;
+
 chai.use(chaiHttp);
 
-// // Delay running the tests until after 5s
+// // Delay running the tests until after 10s
 // setTimeout(function() {
 //   run();
-// }, 5000);
+// }, 10000);
 
 /***************************************
  *   TEST Empty query: GET / or GET /api
  ***************************************/
 describe('Empty queries (/)', () => {
-  it('should return a suggestion to redirect to brands or products', done => {
+  it.skip('should return a suggestion to redirect to brands or products', done => {
     chai
       .request(server)
       .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.an('object');
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.an('object');
         done();
       });
   });
 });
 describe('Empty queries (/api)', () => {
-  it('should return a suggestion to redirect to brands or products', done => {
+  it.skip('should return a suggestion to redirect to brands or products', done => {
     chai
       .request(server)
       .get('/api')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.a('string');
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.a('string');
         done();
       });
   });
@@ -49,11 +47,11 @@ describe('/GET brands', () => {
     chai
       .request(server)
       .get('/api/brands')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
+      .end((error, response) => {
+        expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
-        expect(res.body).to.be.an('array');
-        expect(res.body).to.have.lengthOf(5);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.lengthOf(5);
         done();
       });
   });
@@ -67,10 +65,10 @@ describe('/GET products by brand', () => {
     chai
       .request(server)
       .get('/api/brands/:id/products')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
+      .end((error, response) => {
+        expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
-        expect(res.body).to.be.an('array');
+        expect(response.body).to.be.an('array');
         done();
       });
   });
@@ -84,92 +82,38 @@ describe('/GET products', () => {
     chai
       .request(server)
       .get('/api/products')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
+      .end((error, response) => {
+        expect(response).to.have.status(200);
         expect('Content-Type', 'application/json');
-        expect(res.body).to.be.an('array');
-        expect(res.body).to.have.lengthOf(11);
+        expect(response.body).to.be.an('array');
+        expect(response.body).to.have.lengthOf(11);
         done();
       });
   });
 });
-
-// /*=================================================
-//  *   TEST Specific product: GET /api/products/:id
-//  *=================================================*/
-// describe('/GET specific product', () => {
-//   it('should return the specified product', done => {
-//     chai
-//       .request(server)
-//       .get('/api/products/1')
-//       .end((err, res) => {
-//         expect(res).to.have.status(200);
-//         expect('Content-Type', 'application/json');
-//         expect(res.body).to.have.keys([
-//           'id',
-//           'categoryId',
-//           'name',
-//           'description',
-//           'price',
-//           'imageUrls'
-//         ]);
-//         expect(res.body)
-//           .to.have.property('id')
-//           .that.is.a('string');
-//         expect(res.body)
-//           .to.have.property('categoryId')
-//           .that.is.a('string');
-//         expect(res.body)
-//           .to.have.property('name')
-//           .that.is.a('string');
-//         expect(res.body)
-//           .to.have.property('description')
-//           .that.is.a('string');
-//         expect(res.body)
-//           .to.have.property('price')
-//           .that.is.a('number');
-//         expect(res.body)
-//           .to.have.property('imageUrls')
-//           .that.is.an('array')
-//           .with.lengthOf(3);
-//         done();
-//       });
-//   });
-// });
 
 /*====================================
  *   TEST User login: POST /api/login
- *====================================*/
-describe('/POST login user', () => {
-  it('should login the user', done => {
+ *====================================
+    REVISED VERSION
+-------------------------------------*/
+describe('/POST user login', () => {
+  it('should succeed if username and password are both valid', done => {
     chai
       .request(server)
       .post('/api/login')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.body).to.be.lengthOf(16);
-        expect(res.body).to.be.a('string');
+      .set('Accept', 'application/json')
+      .set('Content-Type', 'application/json')
+      .send({ username: 'username', password: 'password' })
+      .end((error, response) => {
+        // expect(200);
+        expect(response).to.have.status(200);
+        expect('Content-Type', /json/);
+        expect(function(response) {
+          expect(response.body).not.to.be.empty;
+          // expect(response.body).to.be.an('object');
+        });
         done();
       });
   });
 });
-
-// // Set up the data to pass to the login method
-// const userCredentials = {
-//   username: 'greenlion235',
-//   password: 'waters'
-// };
-// // Login the user before testing
-// var authenticatedUser = request.agent(app);
-// before(function(done) {
-//   authenticatedUser
-//     // Make a POST to the /login route
-//     .post('/login')
-//     .send(userCredentials)
-//     .end(function(err, response) {
-//       expect(response.statusCode).to.equal(200);
-//       // Make sure the user is directed to /api/me/cart
-//       expect('Location', '/api/me/cart');
-//       done();
-//     });
-// });
