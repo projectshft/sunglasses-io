7d049ae969033583aca73dc25940bf73983f03ca
K. Bender <kristinajbender@gmail.com>
2019-07-18 19:39:35 -0400

    Set up server and test files

--- app/server.js
+++ app/server.js
@@ -5,9 +5,43 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
+const url = require("url");
+
+// State holding variables
+let products = [];
+// let user = {};
+let brands = [];
+let users = [];
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// Set up router
+const router = Router();
+router.use(bodyParser.json());
 
-}).listen(PORT);
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  router(req, res, finalHandler(req, res));
+});
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`server running on port ${PORT}`);
+
+  //populate brands  
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json","utf-8"));
+
+  //populate products
+  products = JSON.parse(fs.readFileSync("initial-data/products.json","utf-8"));
+
+  //populate users
+  users = JSON.parse(fs.readFileSync("initial-data/users.json","utf-8"));
+  
+  // hardcode "logged in" user
+  // user = users[0];
+});
+
+
+// endpoint handlers: router.get, router.post
+
+module.exports = server;
---
+++ test/server-test.js
@@ -0,0 +1,11 @@
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const server = require("../app/server");
+
+const expect = chai.expect;
+const assert = chai.assert;
+// let should = chai.should();
+
+chai.use(chaiHTTP);
+
+// describe... endpoints 
