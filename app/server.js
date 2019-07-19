20650a9ee93fefc4d7bbdf4d3ce118810a83171e
Jake Galligan <galligan1996@gmail.com>
2019-07-19 17:50:37 -0400

    Importing necessary modules and establish basic chai test on the get /products endpoint which passes

--- app/server.js
+++ app/server.js
@@ -6,8 +6,38 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
+//set default variables
+let products = [];
+let brands =[];
+let users = [];
+
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+//Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+const server = module.exports = http.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request,response))
+}).listen(PORT, () => {
+    //extract the products data and save it into a  products variable
+    //array of objects
+    products = JSON.parse(fs.readFileSync('../initial-data/products.json', 'utf-8'));
+    //extract the brands data and save it into a products variable
+    //array of objects
+    brands = JSON.parse(fs.readFileSync('../initial-data/brands.json', 'utf-8'));
+   //array of objects 
+    //extract the user data and savi it into a users variable
+    users = JSON.parse(fs.readFileSync('../initial-data/users.json'), 'utf-8')
+    
+
+});
+
+//creater a route for the /products endpoint
+myRouter.get('/api/products', function(request, response) {
+    return response.end(JSON.stringify(products));
+})
+
+
+
+
---
+++ app/test.js
@@ -0,0 +1,28 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require ('./server');
+let Products = require ('../initial-data/products');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+
+// let assert = require('assert');
+
+
+//set a test for a get request 
+describe('/GET products', () => {
+    it('should GET all the products', done => {
+        chai
+            .request(server)
+            .get('/api/products')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                done();
+            })
+    })
+});
+
+
