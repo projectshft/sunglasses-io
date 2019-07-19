d153f7f785f6fd0504937398a975303d86635ead
Kate Kremer <katrinakremer@gmail.com>
2019-07-19 09:30:47 -0400

    add chai-http and create first failing test to get brands

---
+++ tests/server-test.js
@@ -0,0 +1,22 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app/server');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/Get brands', () => {
+    it('it should GET all the brands', done => {
+      chai
+        .request(server)
+        .get('/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(5);
+          done();
+        });
+        
+    });
+});
