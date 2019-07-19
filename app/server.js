ed399a67e1180c5ec1e75470ba137b4bee9f1658
Byron Glaspy <byronglaspy@Byrons-MacBook-Pro.local>
2019-07-19 14:27:44 -0400

    Initial Commit

--- app/server.js
+++ app/server.js
@@ -7,7 +7,35 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+const myRouter = Router();
+myRouter.use(bodyParser.json());
 
-http.createServer(function (request, response) {
+let brands = [];
+let products = [];
+let users = [];
 
-}).listen(PORT);
+http
+.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response));
+
+}).listen(PORT, error => {
+    if (error) {
+        return console.log("Error on Server Startup: ", error);
+      }
+      fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+        if (error) throw error;
+        brands = JSON.parse(data);
+        console.log(`Server setup: ${brands.length} brands loaded`);
+      });
+      fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+        if (error) throw error;
+        products = JSON.parse(data);
+        console.log(`Server setup: ${products.length} products loaded`);
+      });
+      fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+        if (error) throw error;
+        users = JSON.parse(data);
+        console.log(`Server setup: ${users.length} users loaded`);
+      });
+      console.log(`Server is listening on ${PORT}`);
+});
