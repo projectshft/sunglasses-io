0574e59e87383dd9a6e3d17bc6d428e5d7f38c69
Kate Kremer <katrinakremer@gmail.com>
2019-07-19 09:53:42 -0400

    set up server

--- app/server.js
+++ app/server.js
@@ -6,8 +6,26 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
+const hostname = 'localhost';
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+//State holding variables
+var brands = [];
+var products = [];
+var users = [];
+var user = {};
 
-}).listen(PORT);
+//Router set up
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+
+http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, () => {
+  //load data from files into server memory
+  brands = JSON.parse(fs.readFileSync('brands.json', 'utf-8'));
+  products = JSON.parse(fs.readFileSync('products.json', 'utf-8'));
+  users = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
+  user = users[0];
+});
