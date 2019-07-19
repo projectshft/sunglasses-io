7643d72d15fbf3f1e2badf283639b91b9a6db909
snpdolan <snpdolan@gmail.com>
2019-07-18 21:38:23 -0400

    first test set up, lets get some brands

--- app/test/sunglasses-test.js
+++ app/test/sunglasses-test.js
@@ -6,5 +6,19 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 describe('Brands', () => {
-  
+  describe('/GET brands', () =>{
+    it('it should GET all the brands', done => {
+      //arrange
+      chai
+        .request(server)
+        .get('/brands')
+        .end((err, res) => {
+          //assert
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(5);
+          done();
+        })
+    });
+  });
 });
