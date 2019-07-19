b475e01acece244c819cbb769e6f5156064cd2a6
Ronnie <rscott910@outlook.com>
2019-07-19 13:41:15 -0400

    created a basic server startup

--- app/server.js
+++ app/server.js
@@ -8,6 +8,37 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// declare vars that will contain state
+let brands = [];
+let products = [];
+let users = [];
 
-}).listen(PORT);
+// set up router
+const myRouter = Router();
+myRouter.use(bodyParser.json());
+
+
+
+http.createServer(function (request, response) {
+	myRouter(request, response, finalHandler(request, response));
+
+}).listen(PORT, error => {
+	if (error) {
+		return console.log('Server startup error: ', error);
+	}
+	fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+		if (error) throw error;
+		brands = JSON.parse(data);
+		console.log(`Server startup: ${brands.length} brands loaded`);
+	});
+	fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+		if (error) throw error;
+		products = JSON.parse(data);
+		console.log(`Server startup: ${products.length} products loaded`);
+	});
+	fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+		if (error) throw error;
+		users = JSON.parse(data);
+		console.log(`Server startup: ${users.length} users loaded`);
+	});
+});
