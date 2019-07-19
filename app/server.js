4c3c5b855a2f3c2b40957750363c453ed8e13c64
snpdolan <snpdolan@gmail.com>
2019-07-18 21:28:15 -0400

    initial server setup, pulling from initial-data files

--- app/server.js
+++ app/server.js
@@ -8,6 +8,45 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
+//state holding variables
+let brands = [];
+let products = [];
+let users = [];
+let AUTH_TOKENS = [];
+
+//Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
 http.createServer(function (request, response) {
 
-}).listen(PORT);
+  myRouter(request, response, finalHandler(request, response));
+
+  //add in error on start-up check
+  })
+  .listen(PORT, error => {
+
+    if (error) {
+      return console.log("Error on Server Startup: ", error);
+    }
+
+    fs.readFile("../initial-data/users.json", "utf8", (error, data) => {
+      if (error) throw error;
+      users = JSON.parse(data);
+      console.log(`Server setup: ${users.length} users loaded`);
+    });
+
+    fs.readFile("../initial-data/products.json", "utf8", (error, data) => {
+      if (error) throw error;
+      products = JSON.parse(data);
+      console.log(`Server setup: ${products.length} products loaded`);
+    });
+
+    fs.readFile("../initial-data/brands.json", "utf8", (error, data) => {
+      if (error) throw error;
+      brands = JSON.parse(data);
+      console.log(`Server setup: ${brands.length} brands loaded`);
+    });
+
+    console.log(`Server is listening on ${PORT}`);
+  });
