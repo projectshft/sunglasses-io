cb6a5649e2ec46cb6df646d1398cab0602d41d6e
snpdolan <snpdolan@gmail.com>
2019-07-18 21:40:18 -0400

    fixed import issues

--- app/server.js
+++ app/server.js
@@ -30,19 +30,19 @@ http.createServer(function (request, response) {
       return console.log("Error on Server Startup: ", error);
     }
 
-    fs.readFile("../initial-data/users.json", "utf8", (error, data) => {
+    fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
       if (error) throw error;
       users = JSON.parse(data);
       console.log(`Server setup: ${users.length} users loaded`);
     });
 
-    fs.readFile("../initial-data/products.json", "utf8", (error, data) => {
+    fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
       if (error) throw error;
       products = JSON.parse(data);
       console.log(`Server setup: ${products.length} products loaded`);
     });
 
-    fs.readFile("../initial-data/brands.json", "utf8", (error, data) => {
+    fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
       if (error) throw error;
       brands = JSON.parse(data);
       console.log(`Server setup: ${brands.length} brands loaded`);
