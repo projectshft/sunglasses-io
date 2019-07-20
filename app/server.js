6bb5d42ba111eb59f0646259f647d1b4fb79b534
Jake Galligan <galligan1996@gmail.com>
2019-07-19 21:34:44 -0400

    Allowing for a user to enter a search query and get a specific product that matches that query, (case sensitive

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var finalHandler = require('finalhandler');
+const url = require("url");
 var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
@@ -26,21 +27,42 @@ const server = module.exports = http.createServer(function (request, response) {
 }).listen(PORT, () => {
     //extract the products data and save it into a  products variable
     //array of objects
-    products = JSON.parse(fs.readFileSync('../initial-data/products.json', 'utf-8'));
+    products = JSON.parse(fs.readFileSync('./initial-data/products.json', 'utf-8'));
     //extract the brands data and save it into a products variable
     //array of objects
-    brands = JSON.parse(fs.readFileSync('../initial-data/brands.json', 'utf-8'));
+    brands = JSON.parse(fs.readFileSync('./initial-data/brands.json', 'utf-8'));
    //array of objects 
     //extract the user data and savi it into a users variable
-    users = JSON.parse(fs.readFileSync('../initial-data/users.json'), 'utf-8')
+    users = JSON.parse(fs.readFileSync('./initial-data/users.json'), 'utf-8')
     
 
 });
 
 //creater a route for the /products endpoint
 myRouter.get('/api/products', function(request, response) {
+    //set 
+    let productsToReturn = [];
+    //parse the search query from the url
+    const parsedUrl = url.parse(request.originalUrl);
+    const query = queryString.parse(parsedUrl.query);
+    //if there is a query filter through products and return product that matches query
+    //setting a strict 
+    if (query.q !== undefined) {
+        productsToReturn = products.filter(product => {
+            return product.name == query.q
+        })
+        //if products to return is empty return an error
+        if (productsToReturn.length == 0) {
+            response.writeHead(400, 'There are no products matching search query');
+            return response.end();
+        }
+    } else {
+        productsToReturn = products;
+
+    }
     response.writeHead(200, header);
-    return response.end(JSON.stringify(products));
+    console.log(productsToReturn);
+    return response.end(JSON.stringify(productsToReturn));
 })
 
 
--- app/test.js
+++ app/test.js
@@ -13,19 +13,30 @@ chai.use(chaiHttp);
 
 //set a test for a get request 
 describe('/GET products', () => {
-    describe('No query entered', () => {
-        it('should GET all the products', done => {
+    //if no query is entered all of the products should be returned 
+        it('should GET all the products in no query', done => {
             chai
                 .request(server)
                 .get('/api/products')
                 .end((err, res) => {
                     res.should.have.status(200);
-                    res.body.should.be.an('array');                
+                    res.body.should.be.an('array'); 
+                    res.body.length.should.be.eql(11);               
                     done();
                 })
         })
-
-    })
+        //if a single query is entered, the specific product should be returned
+        it('should return only the products that match that specific query', () => {
+            chai    
+                .request(server)
+                .get('/api/products?query=Brown+Sunglasses')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an('array')
+                    res.body.should.have.lengthOf(1);
+                })
+        })        
+    //if  mulitpe queries are enterd return muliptle queries
 });
 
 
