7060d097e89ba1f6a19bf7f6f132b7069bffae2a
Ronnie <rscott910@outlook.com>
2019-07-20 16:10:27 -0400

    Added failing test for GET all products when no query, fixed path /me/cart/:productId

--- test/server-test.js
+++ test/server-test.js
@@ -6,7 +6,7 @@ chai.use(chaiHTTP);
 
 //test GET /api/brands
 describe("/GET  brands", () => {
-	it.only("should GET all brands", done => {
+	it("should GET all brands", done => {
 		chai
 			.request(server)
 			.get("/api/brands")
@@ -16,6 +16,23 @@ describe("/GET  brands", () => {
 				chai.expect("Content-Type", "application/json");
 				chai.expect(response.body).to.be.an("array");
 				chai.expect(response.body).to.have.lengthOf(5);
+				done();
+			});
+	});
+});
+
+//test GET /api/products
+describe("/GET products", () => {
+	it("should GET all products when no query", done => {
+		chai
+			.request(server)
+			.get("/api/products")
+			.end((error, response) => {
+				chai.assert.exists(response.body);
+				chai.expect(response).to.have.status(200);
+				chai.expect("Content-Type", "application/json");
+				chai.expect(response.body).to.be.an("array");
+				chai.expect(response.body).to.have.lengthOf(11);
 				done();
 			});
 	});
