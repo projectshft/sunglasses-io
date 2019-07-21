95eb6def40738bdec72d460346e5093233d0efcb
Ronnie <rscott910@outlook.com>
2019-07-21 08:19:32 -0400

    More tests for /api/brands/:id/products and /api/products

--- app/server.js
+++ app/server.js
@@ -31,6 +31,10 @@ const server = http.createServer(function (request, response) {
 
 // GET /api/brands (return all brands available)
 myRouter.get("/api/brands", (request, response) => {
+	if(brands.length == 0){
+		response.writeHead(404, "No brands available");
+		response.end();
+	}
 	response.writeHead(200, "Request for brands was successful", {"Content-Type": "application/json"});
 	response.end(JSON.stringify(brands));
 })
@@ -41,8 +45,9 @@ myRouter.get("/api/products", (request,response) => {
 	const { query } = queryString.parse(parsedUrl.query);
 	if (products.length == 0){
 		response.writeHead(400, "Products not available");
+		response.end();
 	}
-	
+
 	let productsReturnedByQuery = [];
 	// make sure that if the query is empty that a 404 error is returned, otherwise send a successfull response
 	if(query !== undefined){
@@ -57,6 +62,19 @@ myRouter.get("/api/products", (request,response) => {
 	}
 	response.writeHead(200, "Request for products was successful", {"Content-Type": "application/json"});
 	response.end(JSON.stringify(productsReturnedByQuery));
+})
+
+// GET /api/brands/:id/products (returns all products of a specific brand by brand ID)
+myRouter.get("/api/brands/:id/products", (request, response) => {
+	const{ id } = request.params;
+	const productsByBrandId = products.filter(product => 
+		product.categoryId === id);
+	if(productsByBrandId.length == 0){
+		response.writeHead(404, "No products available or brand ID incorrect");
+		response.end();
+	}
+	response.writeHead(200, "Request for products by brand was successful", {"Content-Type": "application/json"});
+	response.end(JSON.stringify(productsByBrandId));
 })
 
 // export server to use in server-test file
--- test/server-test.js
+++ test/server-test.js
@@ -47,6 +47,43 @@ describe("/GET products", () => {
 				chai.expect(response.body).to.be.an("array");
 				chai.expect(response.body).to.have.lengthOf(1);
 				done();
+			});
+	});
+	it("should return an error if search query does not match any products", done => {
+		chai
+			.request(server)
+			.get("/api/products?query=qwertyasdf")
+			.end((error, response) => {
+				chai.assert.exists(response.body);
+				chai.expect(response).to.have.status(404);
+				done();
+			});
+	});
+	// it("should return an error if no products can be returned", done => {
+	// 	chai
+	// 		.request(server)
+	// 		.get("/api/products")
+	// 		.end((error, response) => {
+	// 			chai.assert.exists(response.body);
+	// 			chai.expect(response).to.have.status(400);
+	// 			done();
+	// 		});
+	// });
+});
+
+// Test for GET /api/brands/:id/products
+describe("GET products by brand ID", () => {
+	it("should get all products based on a brand id", done => {
+		chai
+			.request(server)
+			.get("/api/brands/2/products")
+			.end((error, response) => {
+				chai.assert.exists(response.body);
+				chai.expect(response).to.have.status(200);
+				chai.expect("Content-Type", "application/json");
+				chai.expect(response.body).to.be.an("array");
+				chai.expect(response.body).to.have.lengthOf(2);
+				done();
 			})
 	});
 });
