9e448c6131e731b7572680f3ee8cec24bd3b64fd
Tyler <laorik1@live.com>
2019-10-31 14:15:25 -0400

    setting up initial test environment and server

--- app/server.js
+++ app/server.js
@@ -1,13 +1,24 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+var http = require("http");
+var fs = require("fs");
+var finalHandler = require("finalhandler");
+var queryString = require("querystring");
+var Router = require("router");
+var bodyParser = require("body-parser");
+var uid = require("rand-token").uid;
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-http.createServer(function (request, response) {
+const router = Router();
+router.use(bodyParser.json());
 
-}).listen(PORT);
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  router(req, res, finalHandler(req, res));
+});
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`server running on port ${PORT}`);
+});
+
+module.exports = server;
