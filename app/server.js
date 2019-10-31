78b7d8234522717eb1a8c96c3026874a29599a79
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-31 14:11:37 -0400

    Write first test to return all brands from API call

--- app/server.js
+++ app/server.js
@@ -1,10 +1,10 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const uid = require('rand-token').uid;
 
 const PORT = 3001;
 // State holding variables
@@ -21,18 +21,18 @@ http.createServer(function (request, response) {
   router(request, response, finalHandler(request, response));
 
 }).listen(PORT, error => {
-    // if (error) {
-    //   return console.log("Error on Server Startup: ", error);
-    // }
-    // fs.readFile("stores.json", "utf8", (error, data) => {
-    //   if (error) throw error;
-    //   stores = JSON.parse(data);
-    //   console.log(`Server setup: ${stores.length} stores loaded`);
-    // });
-    // fs.readFile("users.json", "utf8", (error, data) => {
-    //   if (error) throw error;
-    //   users = JSON.parse(data);
-    //   console.log(`Server setup: ${users.length} users loaded`);
-    // });
-    // console.log(`Server is listening on ${PORT}`);
-  });
+  // if (error) {
+  //   return console.log("Error on Server Startup: ", error);
+  // }
+  // fs.readFile("stores.json", "utf8", (error, data) => {
+  //   if (error) throw error;
+  //   stores = JSON.parse(data);
+  //   console.log(`Server setup: ${stores.length} stores loaded`);
+  // });
+  // fs.readFile("users.json", "utf8", (error, data) => {
+  //   if (error) throw error;
+  //   users = JSON.parse(data);
+  //   console.log(`Server setup: ${users.length} users loaded`);
+  // });
+  // console.log(`Server is listening on ${PORT}`);
+});
