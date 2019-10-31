e049b31c4132b2dd36dd5d2974cc71f4da31c3fd
daveman235 <awad.david@gmail.com>
2019-10-31 18:29:40 -0400

    the beginning

--- app/server.js
+++ app/server.js
@@ -3,11 +3,16 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
 http.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response))
+}).listen(3001, () => {
+    // Load all the brands from thr brands JSON file 
+    brands = JSON.parse(fs.readFileSync("brands.json", "utf-8"));
+});
 
-}).listen(PORT);
+export.
