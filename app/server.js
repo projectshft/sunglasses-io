47a7b4993e0f895b6f7208e4ba819a7ec82c0aca
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-31 13:55:44 -0400

    Add router and state holding variables to server

--- app/server.js
+++ app/server.js
@@ -7,7 +7,32 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+// State holding variables
+let brands = [];
+let products = [];
+let users = [];
+
+// Router setup
+const router = Router();
+router.use(bodyParser.json());
 
 http.createServer(function (request, response) {
 
-}).listen(PORT);
+  router(request, response, finalHandler(request, response));
+
+}).listen(PORT, error => {
+    // if (error) {
+    //   return console.log("Error on Server Startup: ", error);
+    // }
+    // fs.readFile("stores.json", "utf8", (error, data) => {
+    //   if (error) throw error;
+    //   stores = JSON.parse(data);
+    //   console.log(`Server setup: ${stores.length} stores loaded`);
+    // });
+    // fs.readFile("users.json", "utf8", (error, data) => {
+    //   if (error) throw error;
+    //   users = JSON.parse(data);
+    //   console.log(`Server setup: ${users.length} users loaded`);
+    // });
+    // console.log(`Server is listening on ${PORT}`);
+  });
