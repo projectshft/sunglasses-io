0d6d06dd0fa362d0d78520a9d645691d676c70c2
Alexandra Ferdinand <alexxkf@gmail.com>
2019-10-31 14:55:34 -0400

    first test attempt.

--- app/server.js
+++ app/server.js
@@ -6,18 +6,26 @@ var Router = require('router');
 var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
+//state holding variables 
+var brands = [];
+var products = [];
+var users = {};
+
 const PORT = 3001;
 
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
-http.createServer(function (request, response) {
+const server = http.createServer(function (request, response) {
   myRouter(request, response, finalHandler(request, response));
 }).listen(PORT, () => {
   // Load dummy data into server memory for serving
-  brands = JSON.parse(fs.readFileSync("brands.json", "utf-8"));
-  products = JSON.parse(fs.readFileSync("products.json", "utf-8"));
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json", "utf-8"));
+  products = JSON.parse(fs.readFileSync("initial-data/products.json", "utf-8"));
   // Load all users into users array and for now hardcode the first user to be "logged in"
-  users = JSON.parse(fs.readFileSync("users.json", "utf-8"));
+  users = JSON.parse(fs.readFileSync("initial-data/users.json", "utf-8"));
   user = users[0];
 });
+
+//export the server so that tests can be written
+module.exports = server
--- test/server.js
+++ test/server.js
@@ -1,10 +1,24 @@
-let Server = require("../app/server");
-
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../server");
+let server = require("../app/server");
 
 let should = chai.should();
+let expect = chai.expect;
+
 
 chai.use(chaiHttp);
 
+describe("/GET brands", () => {
+  it("it should GET all of the brands", done => {
+    chai
+      .request(server)
+      .get("/api/brands")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an("array");
+        res.body.should.have.property("id");
+        res.body.should.have.property("name");
+        done();
+      })
+  })
+});
