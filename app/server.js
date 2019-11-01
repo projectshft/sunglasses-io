d8e38a78adcdcbd82c3b6ca9797bc834b03e4ff7
MrWhatleyUNC <chris.whatley87@gmail.com>
2019-11-01 09:48:26 -0400

    Initialiizing Server

--- app/server.js
+++ app/server.js
@@ -7,7 +7,21 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+let brands = [];
+let users = [];
+let user = {};
+let products = [];
 
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+//Server Data Setup
 http.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, ()=> {
+    brands = JSON.parse(fs.readFileSync('brands.json','utf-8'));
+    users = JSON.parse(fs.readFileSync('users.json','utf-8'));
+    user = users[0];
+    products = JSON.parse(fs.readFileSync('products.json','utf-8'));
+});
 
-}).listen(PORT);
