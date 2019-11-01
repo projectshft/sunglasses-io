ed1a77ec4f4bdd0b263b7165702e5b6661b8b73d
Tyler <laorik1@live.com>
2019-11-01 10:51:51 -0400

    added tests for products endpoint, added implementation of products endpoint

--- app/server.js
+++ app/server.js
@@ -7,6 +7,7 @@ var bodyParser = require("body-parser");
 var uid = require("rand-token").uid;
 
 let brands = [];
+let products = [];
 
 const PORT = process.env.PORT || 3001;
 
@@ -24,10 +25,18 @@ server.listen(PORT, err => {
 
   //populate brands
   brands = JSON.parse(fs.readFileSync("initial-data/brands.json", "utf-8"));
+  //populate products
+  products = JSON.parse(fs.readFileSync("initial-data/products.json", "utf-8"));
 });
 
+//return brands, no auth necessary
 router.get("/v1/brands", (request, response) => {
   return prepareValidResponse(response, brands);
+});
+
+//return products, no auth necessary
+router.get("/v1/products", (request, response) => {
+  return prepareValidResponse(response, products);
 });
 
 //helper function to send return object back as JSON while setting JSON header
--- test/server-tests.js
+++ test/server-tests.js
@@ -26,9 +26,67 @@ describe("/GET brands", () => {
           expect(item).to.be.an("object");
           expect(item).to.have.property("id");
           expect(item).to.have.property("name");
-          expect(item["id"]).to.be.a("string");
+          //id existence check
           expect(item["id"]).to.exist;
           expect(item["id"]).to.not.be.empty;
+          expect(item["id"]).to.be.a("string");
+          //check for unique keys
+          let exists = usedIds.includes(item["id"]);
+          expect(exists).to.be.false;
+          usedIds.push(item["id"]);
+          expect(item["name"]).to.be.a("string");
+        });
+        done();
+      });
+  });
+});
+
+describe("/GET products", () => {
+  it("should GET all products", done => {
+    chai
+      .request(server)
+      .get("/v1/products")
+      .end((error, response) => {
+        expect(error).to.be.null;
+        expect(response.body).to.not.be.null;
+        expect(response).to.have.status(200);
+        expect(response).to.have.header("Content-Type", "application/json");
+        expect(response.body).to.be.an("array");
+        //the below test would be removed when you
+        //decide to implement this with a variable database.
+        //adding this just for completeness
+        expect(response.body).to.have.lengthOf(11);
+
+        let usedIds = [];
+        response.body.forEach(item => {
+          expect(item).to.be.an("object");
+          //property check
+          expect(item).to.have.property("id");
+          expect(item).to.have.property("categoryId");
+          expect(item).to.have.property("name");
+          expect(item).to.have.property("description");
+          expect(item).to.have.property("price");
+          expect(item).to.have.property("imageUrls");
+          //make sure imageUrls is array of strings
+          item["imageUrls"].forEach(image => {
+            expect(image).to.exist;
+            expect(image).to.not.be.empty;
+            expect(image).to.be.a("string");
+          });
+          //type checking
+          expect(item["id"]).to.be.a("string");
+          expect(item["categoryId"]).to.be.a("string");
+          expect(item["name"]).to.be.a("string");
+          expect(item["description"]).to.be.a("string");
+          expect(item["price"]).to.be.a("number");
+          expect(item["imageUrls"]).to.be.a("array");
+          //id existence check
+          expect(item["id"]).to.exist;
+          expect(item["id"]).to.not.be.empty;
+          //category is required for other end points?
+          expect(item["categoryId"]).to.exist;
+          expect(item["categoryId"]).to.not.be.empty;
+          expect(item["categoryId"]).to.be.a("string");
           //check for unique keys
           let exists = usedIds.includes(item["id"]);
           expect(exists).to.be.false;
