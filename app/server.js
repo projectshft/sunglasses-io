efbb9d5f0a90459ab6fc9831e3732097a81e74be
MrWhatleyUNC <chris.whatley87@gmail.com>
2019-11-01 12:37:56 -0400

    Files loading

--- app/server.js
+++ app/server.js
@@ -15,13 +15,68 @@ let products = [];
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
-//Server Data Setup
-http.createServer(function (request, response) {
+//Server Data Setup and Router
+http.createServer( (request, response) => {
     myRouter(request, response, finalHandler(request, response))
-}).listen(PORT, ()=> {
-    brands = JSON.parse(fs.readFileSync('brands.json','utf-8'));
-    users = JSON.parse(fs.readFileSync('users.json','utf-8'));
-    user = users[0];
-    products = JSON.parse(fs.readFileSync('products.json','utf-8'));
+})
+    .listen(PORT,error => {
+        if (error) {
+            return console.log("Error on Server Startup: ", error);
+        }
+        //brands = JSON.parse(fs.readFile('initial-data/brands.json','utf-8'));
+        
+        fs.readFile('initial-data/brands.json','utf-8', (error, data) => {
+            if (error) throw error;
+            brands = JSON.parse(data);
+            console.log(`Server setup: ${brands.length} brands loaded`);
+        });
+        //users = JSON.parse(fs.readFileSync('initial-data/users.json','utf-8'));
+        
+        fs.readFile('initial-data/users.json','utf-8', (error, data) => {
+            if (error) throw error;
+            users = JSON.parse(data);
+            user = users[0];
+            console.log(`Server setup: ${users.length} users loaded`);
+        });
+
+        
+        // products = JSON.parse(fs.readFileSync('initial-data/products.json','utf-8'));
+        
+        fs.readFile('initial-data/products.json','utf-8', (error, data) => {
+            if (error) throw error;
+            products = JSON.parse(data);
+            console.log(`Server setup: ${products.length} products loaded`);
+        });
+    });
+
+myRouter.get('/api/brands', (request,response) => {
+//should return all brands
 });
 
+myRouter.get('/api/brands/:id/products', (request,response) => {
+//should return all products of a certain brand
+});
+
+myRouter.get('/api/products', (request,response)=>{
+//should return all products
+});
+
+myRouter.post('/api/login', (request,response)=>{
+//authentication    
+});
+
+myRouter.get('/api/me/cart', (request,response)=>{
+//cart content of authenticated user    
+});
+
+myRouter.post('/api/me/cart', (request,response)=>{
+//add item(s) to cart of authenticated user    
+});
+
+myRouter.delete('/api/me/cart/:productId', (request,response)=>{
+//removes item(s) from cart    
+});
+
+myRouter.post('/api/me/cart/productId', (request,response)=>{
+//update quantity of item(s) in cart 
+});
