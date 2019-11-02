b393b3d1a29d1a86846aa80ded3ed14903a48740
daveman235 <awad.david@gmail.com>
2019-11-02 17:59:08 -0400

    5 passing tests brands and products, not finished with products yet

--- app/server.js
+++ app/server.js
@@ -5,11 +5,13 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
+const url = require("url");
 
 const PORT = process.env.PORT || 3001;
 
 // State holding variables 
 let brands = [];
+let products = [];
 
 // Setup router
 const router = Router();
@@ -24,17 +26,44 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, err => {
     if (err) throw err;
     console.log(`server running on port ${PORT}`);
-    //populate brands  
-    brands = JSON.parse(fs.readFileSync("../initial-data/brands.json", "utf-8"));
+    //access to brands.json file 
+    brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf-8"));
+    //access to products.json file 
+    products = JSON.parse(fs.readFileSync("./initial-data/products.json", "utf-8"));
 });
 
 router.get("/api/brands", (request, response) => {
-
-
-
-
     response.writeHead(200, { "Content-Type": "application/json" });
     return response.end(JSON.stringify(brands));
+});
+
+router.get("/api/products", (request, response) => {
+    const parsedUrl = url.parse(request.originalUrl);
+    const { query } = queryString.parse(parsedUrl.query);
+
+    let productsToReturn = [];
+
+    if (query == '' && query.length == 0) {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        return response.end(JSON.stringify("Please enter a search"));
+
+    } else {
+
+        if (query !== undefined) {
+            productsToReturn = products.filter(product => product.description.includes(query));
+
+            if (productsToReturn.length == 0) {
+                response.writeHead(404, { "Content-Type": "application/json" });
+                return response.end(JSON.stringify("There are no products that fit your search"));
+            }
+            // const { query } = parsedQuery;
+        } else {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            return response.end(JSON.stringify(products));
+        }
+    }
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(productsToReturn));
 });
 
 
--- test/server-test.js
+++ test/server-test.js
@@ -7,9 +7,10 @@ const { expect } = require('chai');
 const should = chai.should();
 
 chai.use(chaiHTTP);
-// chai.use(require("chai-sorted"));
 
 
+
+//Get Brands 
 describe('/GET brands', () => {
     it.only('it should GET all the brands', done => {
         chai
@@ -17,11 +18,63 @@ describe('/GET brands', () => {
             .get('/api/brands')
             .end((err, res) => {
                 res.should.have.status(200);
+                res.should.not.be.null;
                 res.body.should.be.an('array');
                 res.body.length.should.be.eql(5);
                 done();
             });
     });
+
 });
+//Get Products 
+describe('/GET products', () => {
+    it.only('should GET all the products', done => {
+        chai
+            .request(server)
+            .get('/api/products')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.length.should.be.eql(11);
+                done();
+            });
+    });
+
+    it.only('should GET products based on query', done => {
+        chai
+            .request(server)
+            .get('/api/products?query=best')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.length.should.be.eql(4);
+                done();
+            });
+    });
+    it.only('should GET an error message if no query is entered in search field', done => {
+        chai
+            .request(server)
+            .get('/api/products?query=' + '')
+            .end((err, res) => {
+                res.should.have.status(404);
+                res.body.should.be.a('string');
+                res.body.length.should.be.eql(21);
+                done();
+            });
+    });
+
+    it.only('should GET error if there are no products that match query', done => {
+        chai
+            .request(server)
+            .get('/api/products?query=candy')
+            .end((err, res) => {
+                res.should.have.status(404);
+                res.body.should.be.a('string');
+                res.body.length.should.be.eql(42);
+                done();
+            });
+    });
+});
+
 
 
