cf98c0019f4e2bf81008f71e3bf232c7b26a75a1
Madison Hudson <madisonhudson@Madisons-MacBook-Pro.local>
2019-11-02 10:51:05 -0400

    working on testing if brands is an array

--- app/test.js
+++ app/test.js
@@ -7,4 +7,18 @@ let brands = require('./server.js');
 expect(brands._items).to.be.an('array');
 
 
- 
+describe('Brands', () => {
+    describe('/GET brands', () => {
+      it('it should GET all the books', done => {
+        chai
+          .request(server)
+          .get('/book')
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(0);
+            done();
+          });
+      });
+    });
+  });
