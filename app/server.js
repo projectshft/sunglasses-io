63c281b1b3dcc1c0c4aa368949330d90a7d255b1
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-11-24 18:07:36 -0500

    get products route updated

--- app/server.js
+++ app/server.js
@@ -74,76 +74,22 @@ patrickRouter.get("/api/brands", (request, response) => {
 
 
 patrickRouter.get("/api/products", (request, response) => {
-    response.writeHead(200, {'Content-Type': 'application/json'
-});
-    return response.end(JSON.stringify(products));
-    
+  const query = queryString.parse(parsedURL.query);
+  if (query !== Number) {
 
-})
-
-
-
-
-
-
-// patrickRouter.get("/api/users", (request, response) => {
-//     response.writeHead(200, {'Content-Type': 'application/json'
-// });
-//     return response.end(JSON.stringify(users));
-    
-
-// });
-
-
-// Login call
-patrickRouter.post('/api/users', function(request,response) {
-    var email = request.body.email;
-    var password = request.body.password;
-  // Make sure there is a username and password in the request
-  if (request.body.email && request.body.password) {
-    // See if there is a user that has that username and password
-    let userLogin = users.find((user)=>{
-      return user.body.email == request.body.email && user.login.password == request.body.password;
-    });
-
-    //access token if userLogin was fetched correctly
-    if (userLogin) {
-      
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      let currentAccessToken = accessTokens.find((tokenObject) => {
-        return tokenObject.username == user.login.username;
-      });
-
-      // Update the last updated value so we get another time period
-      if (currentAccessToken) {
-        currentAccessToken.lastUpdated = new Date();
-        return response.end(JSON.stringify(currentAccessToken.token));
-      } else {
-        // Create a new token with the user value and a "random" token
-        let newAccessToken = {
-          username: userLogin.login.username,
-          lastUpdated: new Date(),
-          token: uid(16)
-        }
-        accessTokens.push(newAccessToken);
-        response.writeHead(200, {'Content-Type': 'application/json'
-});
-        return response.end(JSON.stringify(newAccessToken.token));
-      }
-    } else {
-      // When a login fails, tell the client in a generic way that either the username or password was wrong
-      response.writeHead(400, {'Content-Type': 'application/json'
-    });
-    return response.end(JSON.stringify(users));
-    }
-
+    response.writeHead(400, "Please enter a number representing the id")
+    response.end();
   } else {
-    // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
-    response.writeHead(401, {'Content-Type': 'application/json'
-  });
-  return response.end(JSON.stringify(users));
+    response.writeHead(200, {'Content-Type': 'application/json'});
+    response.end(JSON.stringify(products));
+
   }
+   
 });
+  
+
+
+
 
 
 patrickRouter.get("/api/me/cart", (request, response) => {
