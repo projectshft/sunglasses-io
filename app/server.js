e25faba8c5810c91fcdb6b58211d5b166f4c6df8
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-02-27 14:32:14 -0500

    Initial commit, test and server are in files.

--- app/server.js
+++ app/server.js
@@ -2,12 +2,23 @@ var http = require('http');
 var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
+//used in goals worth to parse equery string for serch
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
+http.createServer(function (request, response) {
+    if (request.url.includes('api')) {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        myRouter(request, response, finalHandler(request, response))
+      } else {
+        // This call serves the webpage for the frontend rather than opening it directly
+        serve(request,response, finalHandler(request,response))
+      }
 }).listen(PORT);
---
+++ test/server-test.js
@@ -0,0 +1,8 @@
+const chai = require("chai");
+const chaiHTTP = require("chai-http");
+const server = require("../app/server");
+const expect = chai.expect;
+const assert = chai.assert;
+
+chai.use(chaiHTTP);
+chai.use(require("chai-sorted"));
