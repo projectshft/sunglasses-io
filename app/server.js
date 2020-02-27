2938ce43be49a06216e303892caaa0b99e7c942d
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-02-27 14:51:49 -0500

    with server working and first failing mocha test

--- app/server.js
+++ app/server.js
@@ -8,6 +8,51 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+let brands = [];
+let users = [];
+let products = [];
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+http.createServer((request, response) => {
+    // Handle CORS Preflight request
+  if (request.method === 'OPTIONS'){
+    response.writeHead(200, CORS_HEADERS);
+    return response.end();
+  }
+    // Verify that a valid API Key exists before we let anyone access our API
+//   if (!VALID_API_KEYS.includes(request.headers["x-authentication"])) {
+//     response.writeHead(401, "You need to have a valid API key to use this API", CORS_HEADERS);
+//     return response.end();
+//   }
+
+myRouter(request, response, finalHandler(request, response));
+}).listen(PORT, error => {
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
+  }
+  fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+  fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+  console.log(`Server is listening on ${PORT}`);
+});
+
+// Public route - all users of API can access
+myRouter.get('/api/brands', function(request,response) {
+    
+    return response.end(JSON.stringify(brands))
+
+  });
--- test/sunglasses-test.js
+++ test/sunglasses-test.js
@@ -1,22 +1,22 @@
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
-let server = require('../server');
-
+let server = require('../app/server');
 let should = chai.should();
-
 chai.use(chaiHttp);
 
-describe('/GET book', () => {
+
+describe('/GET brands', () => {
     it('it should GET all the brands', done => {
       chai
         .request(server)
-        .get('/book')
+        .get('/api/brands')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an('array');
-          res.body.length.should.be.eql(0);
+          res.body.length.should.be.eql(5);
           done();
         });
     });
   });
+
+  
