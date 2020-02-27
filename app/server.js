aa679fe231e6b27af00e2e5c68dc3de69566c0f1
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-02-27 15:00:51 -0500

    initial commit

--- app/server.js
+++ app/server.js
@@ -5,9 +5,51 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
-
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// State holding variables
+var brands = [];
+var users = [];
+var accessTokens = [];
+var products = {};
 
-}).listen(PORT);
+
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+http.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, (error) => {
+    if (error) {
+      return console.log('Error on Server Startup: ', error)
+    }
+    //Reads the products.JSON file
+    fs.readFile('initial-data/products.json', 'utf8', function (error, data) {
+        if (error) throw error;
+        products = JSON.parse(data);
+        console.log(`Server setup: ${products.length} products loaded`);
+    });
+    //Reads the brands.JSON file
+    fs.readFile('initial-data/brands.json', 'utf8', function (error, data) {
+      if (error) throw error;
+      brands = JSON.parse(data);
+      console.log(`Server setup: ${brands.length} brands loaded`);
+    });
+    //Reads the users.JSON file
+    fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
+      if (error) throw error;
+      users = JSON.parse(data);
+      console.log(`Server setup: ${users.length} users loaded`);
+    });
+    //console log so I know server is up and running
+    console.log(`Server is listening on ${PORT}`);
+  });  
+
+
+// First route to the brands. She return all brand names of sunglasses
+myRouter.get('/api/brands', function(request,response) {
+	// Return all brands in the db
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(brands))
+});
---
+++ test.js
@@ -0,0 +1,19 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('./app/server');
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('/GET brands', () => {
+    it('it should GET all the brands', done => {
+        chai
+            .request(server)
+            .get('/api/brands')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
