f228b33c104294be9d1ad7d6dd4ead4c8d372452
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-02-27 15:22:11 -0500

    server and GET/API/BRANDS rout tested (in postman)

--- app/server.js
+++ app/server.js
@@ -3,11 +3,44 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+// State holding variables
+let products = [];
+let users = [];
+let brands = [];
 
-http.createServer(function (request, response) {
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+http.createServer((request, response) => {
+    myRouter(request, response, finalHandler(request, response));
+}).listen(PORT, error => {
+    if (error) {
+        return console.log("Error on Server Startup: ", error);
+    }
+    fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+        if (error) throw error;
+        products = JSON.parse(data);
+        console.log(`Server setup: ${products.length} products loaded`);
+    });
+    fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
+        if (error) throw error;
+        brands = JSON.parse(data);
+        console.log(`Server setup: ${brands.length} brands loaded`);
+    });
+    fs.readFile("./initial-data/users.json", "utf8", (error, data) => {
+        if (error) throw error;
+        users = JSON.parse(data);
+        console.log(`Server setup: ${users.length} users loaded`);
+    });
+    console.log(`Server is listening on ${PORT}`);
+});
+
+// Public route - all users of API can access
+myRouter.get("/api/brands", (request, response) => {
+    response.end(JSON.stringify(brands));
+});
