2070640363c539dad540f78eb56ef01c07ea1914
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-02-28 14:03:08 -0500

    with products route and tests working and login tests working

--- app/server.js
+++ app/server.js
@@ -7,14 +7,53 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; 
+const failedLoginAttempts = {};
 
 let brands = [];
 let users = [];
 let products = [];
+let accessTokens = [];
+
+
+//functions to check for password attempts
+var getNumberOfFailedLoginRequestsForUsername = function(username) {
+    let currentNumberOfFailedRequests = failedLoginAttempts[username];
+    if (currentNumberOfFailedRequests) {
+      return currentNumberOfFailedRequests;
+    } else {
+      return 0;
+    }
+  }
+  
+  var setNumberOfFailedLoginRequestsForUsername = function(username,numFails) {
+    failedLoginAttempts[username] = numFails;
+  }
+  
+  var getValidTokenFromRequest = function(request) {
+    var parsedUrl = require('url').parse(request.url, true);
+    if (parsedUrl.query.accessToken) {
+      // Verify the access token to make sure it's valid and not expired
+      let currentAccessToken = accessTokens.find(accessToken => {
+        return accessToken.token == parsedUrl.query.accessToken  && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
+      });
+      if (currentAccessToken) {
+        return currentAccessToken;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
+
+
+
 // Setup router
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
+let server = 
 http.createServer((request, response) => {
     // Handle CORS Preflight request
   if (request.method === 'OPTIONS'){
@@ -50,9 +89,79 @@ myRouter(request, response, finalHandler(request, response));
   console.log(`Server is listening on ${PORT}`);
 });
 
-// Public route - all users of API can access
+// Route for the brands 
 myRouter.get('/api/brands', function(request,response) {
-    
+response.writeHead(200, {'Content-Type': 'application/json'})
     return response.end(JSON.stringify(brands))
 
   });
+
+myRouter.get("/api/brands/:id/products", (request, response) => {
+    const productlistbybrand = products.filter((product) => {
+       return product.categoryId == request.params.id;})
+    if(productlistbybrand.length === 0 ) {
+        response.writeHead(404, "That brand cannot be found") ;
+        return response.end(); 
+    } 
+      response.writeHead(200, {'Content-Type': 'application/json'})
+        return response.end(JSON.stringify(productlistbybrand));
+    });
+  
+  // Route for the products 
+myRouter.get('/api/products', function(request,response) {
+    response.writeHead(200, {'Content-Type': 'application/json'})
+        return response.end(JSON.stringify(products))
+    
+      });  
+
+// Login call
+myRouter.post('/api/login', function(request,response) {
+ 
+    // Make sure there is a username and password in the request
+    if (request.body.username && request.body.password && getNumberOfFailedLoginRequestsForUsername(request.body.username) < 3) {
+      // See if there is a user that has that username and password
+     
+      let user = users.find((user)=>{
+        return user.login.username == request.body.username && user.login.password == request.body.password;
+      });
+      if (user) {
+        setNumberOfFailedLoginRequestsForUsername(request.body.username, 0);
+        // Write the header because we know we will be returning successful at this point and that the response will be json
+        response.writeHead(200, {'Content-Type': 'application/json'});
+       
+        // We have a successful login, if we already have an existing access token, use that
+        let currentAccessToken = accessTokens.find((tokenObject) => {
+          return tokenObject.username == user.login.username;
+        });
+  
+        // Update the last updated value so we get another time period
+        if (currentAccessToken) {
+          currentAccessToken.lastUpdated = new Date();
+          return response.end(JSON.stringify(currentAccessToken.token));
+        } else {
+          // Create a new token with the user value and a "random" token
+          let newAccessToken = {
+            username: user.login.username,
+            lastUpdated: new Date(),
+            token: uid(16)
+          }
+          accessTokens.push(newAccessToken);
+          return response.end(JSON.stringify(newAccessToken.token));
+        }
+      } else {
+        let numFailedForUser = getNumberOfFailedLoginRequestsForUsername(request.body.username);
+        setNumberOfFailedLoginRequestsForUsername(request.body.username, ++numFailedForUser);
+        // When a login fails, tell the client in a generic way that either the username or password was wrong
+        response.writeHead(401, "Invalid username or password");
+        return response.end();
+      }
+  
+    } else {
+      // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
+      response.writeHead(400, "Incorrectly formatted response");
+      return response.end();
+    }
+  });
+
+
+  module.exports = server;
--- test/sunglasses-test.js
+++ test/sunglasses-test.js
@@ -5,11 +5,15 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 
-describe('/GET brands', () => {
+describe('/GET /api/brands', () => {
     it('it should GET all the brands', done => {
+        //arrange
+
+        //act
       chai
         .request(server)
         .get('/api/brands')
+        //assert
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an('array');
@@ -17,6 +21,159 @@ describe('/GET brands', () => {
           done();
         });
     });
+    // it ('it should return 404 error and the brand does not exist'), done => {
+    //     //arrange
+    //     const brands = [];
+    //     //act
+    //     chai
+    //         .request(server)
+    //         .get('/api/brands')
+    //         .end((err, res) => {
+    //             res.should.have.status(404);
+    //             res.body.should.be.an('array');
+    //             res.body.length.should.be.eql(0);
+    //             done();
+    //           });
+
+    // }
   });
 
-  
+describe('/GET /api/brands/:id/products', () => {
+    it('it should GET all the products from a specific brand by brand id', done => {
+        //arrange
+ 
+        //act
+      chai
+        .request(server)
+        .get('/api/brands/1/products')
+        //assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(3);
+          res.body[0].should.be.a('object');
+          res.body[0].should.have.property('categoryId');
+          res.body[0].should.have.property('description')
+          res.body[0].should.have.property('id');
+        //   res.body[0].should.have.property('imageUrls').should.be.an('array');
+        //   res.body[0].should.have.property('imageUrls').length.should.be.eql('3');
+          res.body[0].should.have.property('imageUrls');
+          res.body[0].should.have.property('name');
+          res.body[0].should.have.property('price');
+          done();
+        });
+    });
+    it('it should return a 404 error and the brand was not found', done => {
+        //arrange
+        //act
+      chai
+        .request(server)
+        .get('/api/brands/6/products')
+        //assert
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+    it('it should return a 404 error and the brand was not found', done => {
+        //arrange
+        //act
+      chai
+        .request(server)
+        .get('/api/brands/a/products')
+        //assert
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe('/GET /api/products', () => {
+    it('it should GET all the products', done => {
+        //arrange
+ 
+        //act
+      chai
+        .request(server)
+        .get('/api/products')
+        //assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(11);
+          done();
+        });
+    });
+  });
+
+describe ('/POST /api/login', () =>{
+    it('it should POST user login', done =>{
+        //arrange
+        let user = {
+            username: 'greenlion235',
+            password: 'waters'
+        } 
+        //act
+        chai 
+        .request(server)
+        .post('/api/login')
+        .send(user)
+        //assert
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('string');
+            // res.body.should.have.property('username');
+            // res.body.should.have.property('lastUpdated');
+            // res.body.should.have.property('token')
+            done();
+        })
+    })
+    // it('the login username and password cannot be empty', done =>{
+    //     //arrange
+    //     //TODO fil in 
+    //     //act
+    //     chai 
+    //     .request(server)
+    //     .post('/api/login')
+    //     //assert
+    //     .end((err, res) => {
+    //         res.should.have.status(400);
+    //         done();
+    //     })
+    // })
+    it('the login username is invalid', done =>{
+        //arrange
+        let user ={
+            username: 'greenlion23',
+            password: 'waters'
+        }
+        //act
+        chai 
+        .request(server)
+        .post('/api/login')
+        .send(user)
+        //assert
+        .end((err, res) => {
+            res.should.have.status(401);
+            done();
+        })
+    })
+    it('the login password is invalid', done =>{
+        //arrange
+        let user ={
+            username: 'greenlion235',
+            password: 'water'
+        }
+        //act
+        chai 
+        .request(server)
+        .post('/api/login')
+        .send(user)
+        //assert
+        .end((err, res) => {
+            res.should.have.status(401);
+            done();
+        })
+    })
+});
