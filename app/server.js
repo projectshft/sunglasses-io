8e97baa20e139f95102e0991c253486777c6f232
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-02-28 14:06:23 -0500

    added the login api and tokens tests and server.

--- app/server.js
+++ app/server.js
@@ -6,6 +6,8 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 const PORT = 3001;
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+
 
 // State holding variables
 var brands = [];
@@ -47,12 +49,71 @@ let server = http.createServer(function (request, response) {
   });  
 
 
-// First route to the brands. She return all brand names of sunglasses
+// First route to the brands. Should return all brand names of sunglasses
 myRouter.get('/api/brands', function(request,response) {
     response.writeHead(200, { "Content-Type": "application/json" });
 	// Return all brands in the db
 	return response.end(JSON.stringify(brands))
 });
+
+// Routes to products in specific brand. Should return all products with the brand name the user requested of sunglasses
+myRouter.get('/api/brands/:id/products', function(request,response) {
+    //finds the correct brand id requested
+    const productsWithSearchedBrand = products.filter((product=>product.categoryId == request.params.id))
+    //if array is empty the id did not exist
+    if (productsWithSearchedBrand.length === 0) {
+        // If there isn't a store with that id, then return a 404
+        response.writeHead(404, "Brand ID could not be found");
+        return response.end();
+      }
+    response.writeHead(200, { "Content-Type": "application/json" });
+	// Return all products of specific brand
+	return response.end(JSON.stringify(productsWithSearchedBrand))
+});
+
+// Routes to products in specific brand. Should return all products with the brand name the user requested of sunglasses
+myRouter.post('/api/login', function(request,response) {
+
+    //Check if both username and password were entered
+    if (request.body.username && request.body.password) {
+        
+        //search to see if username and password match
+        let user = users.find((user)=>{
+        return user.login.username == request.body.username && user.login.password == request.body.password;
+        });
+
+        //if user is found. We need to return current token or create new token
+        if (user){
+            
+            //user successfully logged status
+            response.writeHead(200, { "Content-Type": "application/json" });
+
+            //creates new access token when login is successful
+            let newAccessToken = {
+                username: user.login.username,
+                lastUpdated: new Date(),
+                token: uid(16)
+            }
+            
+            //pushes token to accessToken array to store current tokens
+            accessTokens.push(newAccessToken);
+            return response.end(JSON.stringify(newAccessToken.token));
+
+        
+        }else {
+
+            // When incorrect username or password is entered
+            response.writeHead(401, "Invalid username or password");
+            return response.end();
+        }
+    }
+    //when no username or password is entered
+    else {
+        response.writeHead(404, "invalid username or password");
+        return response.end();
+        }
+});
+
 
 module.exports = server
 
--- test.js
+++ test.js
@@ -31,7 +31,110 @@ describe('/GET brands/:id/products', () => {
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.an('array');
+                res.body.length.should.be.eql(3);
+                //maybe add more specifics
+                done();
+            });
+    });
+    it('it should not GET all the products with an invalid ID', done => {
+        chai
+            .request(server)
+            .get('/api/brands/6/products')
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+    });
+    it('it should not GET all the products if ID is not a number', done => {
+        chai
+            .request(server)
+            .get('/api/brands/hello/products')
+            .end((err, res) => {
+                res.should.have.status(404);
                 done();
             });
     });
 });
+
+describe('/POST login', () => {
+    it('it should POST users login', done => {
+        const user = {
+            username: "greenlion235",
+            password: "waters"
+        }
+        chai
+            .request(server)
+            .post('/api/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('string')
+                done();
+            });
+    });
+
+    it('it should return error if users password is incorrect', done => {
+        const user = {
+            username: "hello",
+            password: "waters"
+        }
+        chai
+            .request(server)
+            .post('/api/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
+            });
+    });
+    it('it should return error if username is invalid', done => {
+        const user = {
+            username: "greenlion235",
+            password: "hello"
+        }
+        chai
+            .request(server)
+            .post('/api/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(401);
+                done();
+            });
+    });
+    it('if user did not pass any username or password', done => {
+        chai
+            .request(server)
+            .post('/api/login')
+            .end((err, res) => {
+                res.should.have.status(404);
+                done();
+            });
+    });
+    it('if user already has a login token, it should return the current token', done => {
+        const user = {
+            username: "greenlion235",
+            password: "waters"
+        }
+        chai
+            .request(server)
+            .post('/api/login')
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('string')
+                const token = res.body
+                chai
+                .request(server)
+                .post('/api/login')
+                .send(user)
+                .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('string')
+                res.should.equal(token);
+              done();
+            })
+
+        });
+    });
+});
+
