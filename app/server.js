3202b9708811eef5a6bf6d08d34f92f9b26dba04
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-02-28 18:49:00 -0500

    brands and product endpoints allow for query search by name

--- app/server.js
+++ app/server.js
@@ -4,17 +4,49 @@ const Router = require("router");
 const bodyParser   = require('body-parser');
 const fs = require('fs');
 const url = require("url");
-const queryString = require('querystring');
+const querystring = require('querystring');
 var uid = require('rand-token').uid;
+
+const PORT = process.env.PORT || 3001;
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes
 
 // State holding variables
 let brands = [];
 let user = {};
 let products = [];
 let users = [];
+let accessTokens = [];
+ // An access token can look like this:
+ // {
+ //   username: 'sean',
+ //   lastUpdated: <A valid date>,
+ //   token: 'qwertyuiopasdfg1'
+ // }
+let failedLoginAttempts = {};
+ // failed atthempt can look like this:
+ // {
+ //   username: 'sean',
+ //   lastAttempt: <A valid date>,
+ //   count: 0
+ // }
 
-const PORT = process.env.PORT || 3001;
-
+ // Helper method to process access token
+var getValidTokenFromRequest = function(request) {
+  var parsedUrl = require('url').parse(request.url, true);
+  if (parsedUrl.query.accessToken) {
+    // Verify the access token to make sure it's valid and not expired
+    let currentAccessToken = accessTokens.find(accessToken => {
+      return accessToken.token == parsedUrl.query.accessToken;
+    });
+    if (currentAccessToken) {
+      return currentAccessToken;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 // Setup router
 const myRouter = Router();
 myRouter.use(bodyParser.json());
@@ -34,7 +66,7 @@ const server = http.createServer((req, res) => {
     //populate users
     users = JSON.parse(fs.readFileSync("initial-data/users.json","utf-8"));
     // hardcode "logged in" user
-    
+    user = users[0]
   });
 
 
@@ -46,9 +78,24 @@ const saveCurrentUser = (currentUser) => {
 
 //GET all the brands
 myRouter.get("/api/brands", (request, response) => {
+  const parsedUrl = url.parse(request.originalUrl);
+  const { query, sort } = querystring.parse(parsedUrl.query);
+  let brandsToReturn = [];
+  if (query !== undefined) {
+    brandsToReturn = brands.filter(brand => brand.name.includes(query));
 
+    if (!brandsToReturn) {
+      response.writeHead(404, "Sorry we cannot return the brand you searched.");
+      return response.end();
+    }
+  } else {
+    brandsToReturn = brands;
+  }
+  if (sort !== undefined) {
+    brandsToReturn.sort((a, b) => a[sort] - b[sort]);
+  }
   response.writeHead(200, { "Content-Type": "application/json" });
-  return response.end(JSON.stringify(brands));
+  return response.end(JSON.stringify(brandsToReturn));
 });
 
 //GET all products of a particular brand
@@ -69,10 +116,79 @@ myRouter.get("/api/brands/:id/products", (request, response) => {
 
 //GET all the products
 myRouter.get("/api/products", (request, response) => {
+  const parsedUrl = url.parse(request.originalUrl);
+  const { query, sort } = querystring.parse(parsedUrl.query);
+  let productsToReturn = [];
+  if (query !== undefined) {
+    productsToReturn = products.filter(product => product.name.includes(query));
 
+    if (!productsToReturn) {
+      response.writeHead(404, "Sorry we cannot return the product you searched.");
+      return response.end();
+    }
+  } else {
+    productsToReturn = products;
+  }
+  if (sort !== undefined) {
+    productsToReturn.sort((a, b) => a[sort] - b[sort]);
+  }
   response.writeHead(200, { "Content-Type": "application/json" });
-  return response.end(JSON.stringify(products));
+  return response.end(JSON.stringify(productsToReturn));
 });
 
+// Login call
+myRouter.post('/api/login', function(request,response) {
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (!failedLoginAttempts[request.body.username]){
+    failedLoginAttempts[request.body.username] = 0;
+  
+  // Make sure there is a username and password in the request
+  if (request.body.username && request.body.password && failedLoginAttempts[request.body.username] < 3) {
+    // See if there is a user that has that username and password
+    let user = users.find((user)=>{
+      return user.login.username == request.body.username && user.login.password == request.body.password;
+    });
+    if (user) {
+      // Reset our counter of failed logins
+      failedLoginAttempts[request.body.username] = 0;
+      // Write the header because we know we will be returning successful at this point and that the response will be json
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      }
+      // We have a successful login, if we already have an existing access token, use that
+      let currentAccessToken = accessTokens.find((accessToken) => {
+        return accessToken.token == parsedUrl.query.accessToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
+      });
+
+      // Update the last updated value so we get another time period
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        return response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        // Create a new token with the user value and a "random" token
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        }
+        accessTokens.push(newAccessToken);
+        return response.end(JSON.stringify(newAccessToken.token));
+      }
+
+    } else {
+      let numFailedForUser = failedLoginAttempts[request.body.username];
+      if (numFailedForUser) {
+        failedLoginAttempts[request.body.username]++;
+      } else {
+        failedLoginAttempts[request.body.username] = 1
+      }
+      response.writeHead(401, "Invalid username or password");
+      return response.end();
+    }
+  } else {
+    // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
+    response.writeHead(400, "Incorrectly formatted response");
+    return response.end();
+  }
+});
 
 module.exports = server;
--- test/server-test.js
+++ test/server-test.js
@@ -2,8 +2,6 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 const server = require("../app/server");
 const expect = chai.expect;
-const assert = chai.assert;
-
 let should = chai.should();
 
 
@@ -11,14 +9,29 @@ chai.use(chaiHttp);
 
 describe("/GET brands", () => {
     it("should GET all brands", done => {
-      chai
+        chai
         .request(server)
         .get("/api/brands")
         .end((err, res) => {
+            expect(err).to.be.null
+            expect("Content-Type", "application/json");
+            res.body.should.not.equal('null')
             res.should.have.status(200);
             res.body.should.be.an('array');
-            res.body.length.should.be.eql(5);
+            res.body.length.should.be.equal(5);
             done();
+        });
+    });
+    it("should limit results when a user search a brand with a query string", done => {
+        chai
+        .request(server)
+        .get("/v1/brands?query=Oakley")
+        .end((err, res) => {
+            // expect(err).to.be.null;
+            // expect(res).to.have.status(200);
+            // expect("Content-Type", "application/json");
+            // expect(res.body).to.be.an("array");
+            // expect(res.body).to.have.lengthOf(1);
         });
     });
 });
@@ -29,9 +42,29 @@ describe("/GET brands/:id/products", () => {
         .request(server)
         .get(`/api/brands/1/products`)
         .end((err, res) => {
+            expect(err).to.be.null
+            expect("Content-Type", "application/json");
+            res.body.should.not.equal('null')
             res.should.have.status(200);
             res.body.should.be.an('array');
             res.body.length.should.be.eql(1);
+            done();
+        });
+    });
+});
+
+describe("/GET products", () => {
+    it("should GET all products", done => {
+      chai
+        .request(server)
+        .get("/api/products")
+        .end((err, res) => {
+            expect(err).to.be.null
+            expect("Content-Type", "application/json");
+            res.body.should.not.equal('null')
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(11);
             done();
         });
     });
