4546171485ec096999b55941920880005d252a9e
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-06-12 17:04:57 -0400

    added in code and tests for getting products, getting products by brand id, and login

--- app/server.js
+++ app/server.js
@@ -19,7 +19,15 @@ var users = [];
 var accessTokens = [];
 var failedLoginAttempts = {};
 
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000; // 15 minutes
+const CORS_HEADERS = {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept, X-Authentication"};
+
 let server = http.createServer(function (request, response) {
+  if (request.method === 'OPTIONS'){
+    response.writeHead(200, CORS_HEADERS);
+    return response.end();
+  }
+
   myRouter(request, response, finalHandler(request, response))
 }).listen(PORT, (error) => {
   if (error) {
@@ -43,11 +51,115 @@ let server = http.createServer(function (request, response) {
   console.log(`Server is listening on ${PORT}`);
 });
 
+myRouter.post('/api/login', function(request,response) {
+  // Make sure there is a username and password in the request
+  if (request.body.username && request.body.password && getNumberOfFailedLoginRequestsForUsername(request.body.username) < 3) {
+    // See if there is a user that has that username and password
+    let user = users.find((user)=>{
+      return user.login.username == request.body.username && user.login.password == request.body.password;
+    });
+    if (user) {
+      // If we found a user, reset our counter of failed logins
+      setNumberOfFailedLoginRequestsForUsername(request.body.username,0);
+
+      // Write the header because we know we will be returning successful at this point and that the response will be json
+      response.writeHead(200, Object.assign(CORS_HEADERS,{'Content-Type': 'application/json'}));
+
+      // We have a successful login, if we already have an existing access token, use that
+      let currentAccessToken = accessTokens.find((tokenObject) => {
+        return tokenObject.username == user.login.username;
+      });
+
+      // Update the last updated value so we get another time period
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        return response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        // Create a new token with the user value and a "random" token
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        }
+        accessTokens.push(newAccessToken);
+        return response.end(JSON.stringify(newAccessToken.token));
+      }
+    } else {
+      // Update the number of failed login attempts
+      let numFailedForUser = getNumberOfFailedLoginRequestsForUsername(request.body.username);
+      setNumberOfFailedLoginRequestsForUsername(request.body.username,++numFailedForUser);
+      // When a login fails, tell the client in a generic way that either the username or password was wrong
+      response.writeHead(401, "Invalid username or password");
+      return response.end();
+    }
+  } else {
+    // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
+    response.writeHead(400, "Incorrectly formatted response");
+    return response.end();
+  }
+});
+
+// Helpers to get/set our number of failed requests per username
+var getNumberOfFailedLoginRequestsForUsername = function(username) {
+  let currentNumberOfFailedRequests = failedLoginAttempts[username];
+  if (currentNumberOfFailedRequests) {
+    return currentNumberOfFailedRequests;
+  } else {
+    return 0;
+  }
+}
+
+var setNumberOfFailedLoginRequestsForUsername = function(username,numFails) {
+  failedLoginAttempts[username] = numFails;
+}
+
+// Helper method to process access token
+var getValidTokenFromRequest = function(request) {
+  var parsedUrl = require('url').parse(request.url,true)
+  if (parsedUrl.query.accessToken) {
+    // Verify the access token to make sure its valid and not expired
+    let currentAccessToken = accessTokens.find((accessToken) => {
+      return accessToken.token == parsedUrl.query.accessToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
+    });
+    if (currentAccessToken) {
+      return currentAccessToken;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
+
 myRouter.get('/api/brands', function(request,response) {
   //response.writeHead(200, Object.assign(CORS_HEADERS,{'Content-Type': 'application/json'}));
   // Filter out the store issues since that is protected data
   response.writeHead(200, { "Content-Type": "application/json" });
   return response.end(JSON.stringify(brands));
 });
+
+myRouter.get('/api/products', function(request, response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(products));
+})
+
+myRouter.get('/api/brands/:brandId/products', function(request, response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(products.filter(product => product.categoryId === request.params.brandId)));
+})
+
+myRouter.get('/api/me/cart', function(request, response) {
+  let currentAccessToken = getValidTokenFromRequest(request);
+  if (!currentAccessToken) {
+    // If there isn't an access token in the request, we know that the user isn't logged in, so don't continue
+    response.writeHead(401, "You need to have access to this call to continue", CORS_HEADERS);
+    response.end();
+  } else {
+    let user = users.find(user => user.login.username === currentAccessToken.username);
+    response.writeHead(200, { "Content-Type": 'application/json' });
+    return response.end(JSON.stringify(user.cart));
+  }
+  
+})
 
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -20,4 +20,82 @@ describe('sunglasses', () => {
         });
     });
   });
+  describe('/POST login', () => {
+    it('it should log a user into the system', done => {
+      let login = {
+        "username": "yellowleopard753",
+        "password": "jonjon",
+        "salt": "eNuMvema",
+        "md5": "a8be2a69c8c91684588f4e1a29442dd7",
+        "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
+        "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(login)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('string');
+          done();
+        })
+    })
+  })
+  describe('/GET products', () => {
+    it('it should get all products', done => {
+      chai
+        .request(server)
+        .get('/api/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(11);
+          done();
+        })
+    })
+  })
+  describe('/GET products from brandID', () => {
+    it('it should get all products from a given brand', done => {
+      let brandId = 2;
+      chai
+        .request(server)
+        .get(`/api/brands/${brandId}/products`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(2);
+          done();
+        })
+    })
+  })
+  describe('/GET me/cart', () => {
+    it('it should get all items in a users cart', done => {
+      let login = {
+        "username": "yellowleopard753",
+        "password": "jonjon",
+        "salt": "eNuMvema",
+        "md5": "a8be2a69c8c91684588f4e1a29442dd7",
+        "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
+        "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(login)
+        .end((err, res) => {
+          let accessToken = res.body;
+          chai
+            .request(server)
+            .get(`/api/me/cart?accessToken=${accessToken}`)
+            .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.an('array');
+            res.body.length.should.be.eql(0);
+            done();
+        })
+        })
+      
+
+    })
+  })
 })
