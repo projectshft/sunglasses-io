0a83198248ec3c908a52882b0290f19d4bbd5976
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-06-12 12:53:16 -0400

    added test and code for get/brands

--- app/server.js
+++ app/server.js
@@ -8,6 +8,46 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+var brands = [];
+var products = [];
+var users = [];
+
+var users = [];
+var accessTokens = [];
+var failedLoginAttempts = {};
+
+let server = http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error on Server Startup: ', error)
+  }
+  fs.readFile('initial-data/brands.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  fs.readFile('initial-data/products.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+  fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+  console.log(`Server is listening on ${PORT}`);
+});
+
+myRouter.get('/api/brands', function(request,response) {
+  //response.writeHead(200, Object.assign(CORS_HEADERS,{'Content-Type': 'application/json'}));
+  // Filter out the store issues since that is protected data
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(brands));
+});
+
+module.exports = server;
---
+++ test/test.js
@@ -0,0 +1,23 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app/server');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('sunglasses', () => {
+  describe('/GET brands', () => {
+    it('it should GET all the brands', done => {
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(5);
+          done();
+        });
+    });
+  });
+})
