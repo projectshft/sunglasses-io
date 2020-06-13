76d561f8c3eee42030ad0be461316c8f04e8efb1
Pierre Lourens <plourens3@gmail.com>
2020-06-13 14:09:29 -0400

    Builds out test for get /api/brands

--- test/test.js
+++ test/test.js
@@ -4,10 +4,35 @@ let server = require("../app/server");
 let should = chai.should();
 
 chai.use(chaiHttp);
+chai.use(require("chai-things"));
 
 describe("Brands", () => {
   describe("/GET brands ", () => {
     it("it should GET all the brands as an array", (done) => {
+      // assemble
+      const duplicateIds = [];
+
+      // we'll want to check for duplicate IDs
+      const checkForDuplicates = (array) => {
+        let duplicateStatus = false;
+        const alreadySeen = [];
+
+        // want to compare the ID value for each object in array
+        // using a for loop so that we can break early
+        for (let i = 0; i < array.length; i++) {
+          // if the ID value has been seen before, return change status to true
+          if (alreadySeen.includes(array[i].id)) {
+            duplicateStatus = true;
+            break;
+          } else {
+            // for unique values, let's push them to array
+            alreadySeen.push(array[i].id);
+          }
+        }
+
+        return duplicateStatus;
+      };
+
       //act
       chai
         .request(server)
@@ -16,7 +41,9 @@ describe("Brands", () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          res.body.length.should.be.eql(5);
+          res.body.should.all.have.property("id");
+          res.body.should.all.have.property("name");
+          checkForDuplicates(res.body).should.equal(false);
           done();
         });
     });
