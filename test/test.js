35145899726639c5017fea83b4e2d7d80d048d97
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-06-13 14:32:35 -0400

    added test for posting product to cart

--- test/test.js
+++ test/test.js
@@ -98,4 +98,42 @@ describe('sunglasses', () => {
 
     })
   })
+
+  describe('/POST me/cart', () => {
+    it('it should add an item to a users cart', done => {
+      let login = {
+        "username": "yellowleopard753",
+        "password": "jonjon",
+        "salt": "eNuMvema",
+        "md5": "a8be2a69c8c91684588f4e1a29442dd7",
+        "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
+        "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
+      };
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(login)
+        .end((err, res) => {
+          let accessToken = res.body;
+          let product = {
+            "id": "1",
+            "categoryId": "1",
+            "name": "Superglasses",
+            "description": "The best glasses in the world",
+            "price":150,
+            "imageUrls":["https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg"]
+          }    
+          chai
+            .request(server)
+            .post(`/api/me/cart?accessToken=${accessToken}`)
+            .send(product)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.an('object');
+              res.body.should.be.eql(product);
+              done();
+            })
+        })
+    })
+  })
 })
