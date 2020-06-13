5a2a89078d50375d558c2a3c5bd11c1a0625347b
ADH <61984231+adholton@users.noreply.github.com>
2020-06-13 10:31:20 -0400

    completes test of properly populated or empty brands array

--- test/brands-test.js
+++ test/brands-test.js
@@ -8,13 +8,48 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-beforeEach(() => {
-  Brands.removeAll();
-});
+// beforeEach(() => {
+//   Brands.removeAll();
+// });
 
 describe('Brands', () => {
   describe('/GET Brands', () => {
-    it('it should GET all the brands in the array', done => {
+    it('it should GET full array of brands with ids and names', done => {
+      let brandsTestArray = [
+        {
+            "id": "1",
+            "name" : "Oakley"
+        },
+        {
+            "id": "2",
+            "name" : "Ray Ban"
+        },
+        {
+            "id": "3",
+            "name" : "Levi's"
+        },
+        {
+            "id": "4",
+            "name" : "DKNY"
+        },
+        {
+            "id": "5",
+            "name" : "Burberry"
+        }
+      ]
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.be.eql(brandsTestArray);
+          done();
+        });
+    });
+
+    it('it should GET an empty array if there are no brands available', done => {
+      Brands.removeAll();
       chai
         .request(server)
         .get('/api/brands')
