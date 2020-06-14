5645235ecf68d03f905e32f0dc4aae859f0df49f
Pierre Lourens <plourens3@gmail.com>
2020-06-13 20:34:19 -0400

    builds out get /api/products endpoint

--- app/server.js
+++ app/server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var finalHandler = require("finalhandler");
 var queryString = require("querystring");
+const url = require("url");
 var Router = require("router");
 var bodyParser = require("body-parser");
 var uid = require("rand-token").uid;
@@ -13,6 +14,7 @@ var myRouter = Router();
 myRouter.use(bodyParser.json());
 
 let brands = [];
+let products = [];
 
 let server = http
   .createServer(function (request, response) {
@@ -22,10 +24,19 @@ let server = http
     if (error) {
       return console.log("Error on Server Startup: ", error);
     }
+
+    // set up our brands in state
     fs.readFile("./initial-data/brands.json", "utf8", (error, data) => {
       if (error) throw error;
       brands = JSON.parse(data);
       console.log(`Server setup: ${brands.length} brands loaded`);
+    });
+
+    // set up our products in state
+    fs.readFile("./initial-data/products.json", "utf8", (error, data) => {
+      if (error) throw error;
+      products = JSON.parse(data);
+      console.log(`Server setup: ${products.length} products loaded`);
     });
     console.log(`Server is listening on ${PORT}`);
   });
@@ -34,6 +45,33 @@ let server = http
 myRouter.get("/api/brands", function (request, response) {
   response.writeHead(200, { "Content-Type": "application/json" });
   return response.end(JSON.stringify(brands));
+});
+
+myRouter.get("/api/products", function (request, response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+
+  // Get our query params from the query string
+  const queryParams = queryString.parse(url.parse(request.url).query);
+
+  let currentProducts;
+
+  // Look if there is a query param called search
+  if (queryParams.search) {
+    // grab the query that was passed
+    const queryValue = new RegExp(queryParams.search, "gi");
+
+    // return only those products which include the query value
+    // in their description
+    currentProducts = products.filter((product) => {
+      if (queryValue.test(product.description) || queryValue.test(product.name)) {
+        return product;
+      }
+    });
+  } else {
+    // we don't want to change state, and copying the whole in case no query term passed
+    currentProducts = products.slice();
+  }
+  return response.end(JSON.stringify(currentProducts));
 });
 
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -49,3 +49,64 @@ describe("Brands", () => {
     });
   });
 });
+
+describe("Products", () => {
+  describe("/GET products ", () => {
+    it("it should GET all the products as an array when given no search term", (done) => {
+      //act
+      chai
+        .request(server)
+        .get("/api/products")
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.should.all.have.keys("id", "categoryId", "name", "description", "price", "imageUrls");
+          done();
+        });
+    });
+    it("it should GET the products containing a search query when given a search term", (done) => {
+      //act
+      chai
+        .request(server)
+        .get("/api/products?search=Brown")
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          checkFilter(res.body, "Brown").should.equal(true);
+          done();
+        });
+    });
+    it("it should GET the products containing a case insensitive search query", (done) => {
+      //act
+      chai
+        .request(server)
+        .get("/api/products?search=sunGLASSES")
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          // chose a word likely to appear at least somewhere
+          checkFilter(res.body, "sunGLASSES").should.equal(true);
+          res.body.should.not.have.lengthOf(0);
+          done();
+        });
+    });
+  });
+});
+
+const checkFilter = (responseArray, term) => {
+  // use regEx to ignore case of term
+  const regEx = new RegExp(term, "gi");
+
+  // this reducer returns true only if all terms include the term in name or description
+  // if false is returned, it means the list is improperly filtered when passed to checkFilter
+  return responseArray.reduce((status, currentProduct) => {
+    if (regEx.test(currentProduct.description) || regEx.test(currentProduct.name)) {
+      return status;
+    } else {
+      return (status = false);
+    }
+  }, true);
+};
