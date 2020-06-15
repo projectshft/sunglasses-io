f6401cf3c7783e02465aa4a85391b776d55c050d
ADH <61984231+adholton@users.noreply.github.com>
2020-06-14 20:18:21 -0400

    corrects login tests to check for response token and correct credentials

--- test/login-test.js
+++ test/login-test.js
@@ -10,68 +10,38 @@ chai.use(chaiHttp);
 
 
 describe('/POST User', () => {
-  it('it should check if required user properties are present', done => {
-
-
-    //act  check for required user type (object) and required properties of user (will check login credentials in a separate way)
+  it('it should login the user and return back a token', done => {
+    //act  
+    //send a user that already exists in the "db"
+    let user = {
+      username: "yellowleopard753",
+      password: "jonjon"
+    }
     chai
       .request(server)
       .post('/api/login')
-      .send(testUser)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send(user)
       .end((err, res) => {
         res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('cart');
-        res.body.should.have.property('name')
-        res.body.should.have.property('email', 'susanna.richards@example.com');
-        res.body.should.have.property('login')
-        res.body.should.have.property('registered', '2003-08-03 01:12:24');
+        res.body.should.be.a('string');
         done();
       })
   });
 
   
-  it('it should fail with a 401 error if username or password is incorrect', done => {
+  it('it should fail with a 401 error if username is incorrect', done => {
     // arrange
-    let testUser2 = {
-      "gender": "male",
-      "cart":[],
-      "name": {
-          "title": "mr",
-          "first": "salvador",
-          "last": "jordan"
-      },
-      "location": {
-          "street": "9849 valley view ln",
-          "city": "burkburnett",
-          "state": "delaware",
-          "postcode": 78623
-      },
-      "email": "salvador.jordan@example.com",
-      "login": {
-          "username": "lazywolf342",
-          "password": "tucker",
-          "salt": "oSngghny",
-          "md5": "30079fb24f447efc355585fcd4d97494",
-          "sha1": "dbeb2d0155dad0de0ab9bbe21c062e260a61d741",
-          "sha256": "4f9416fa89bfd251e07da3ca0aed4d077a011d6ef7d6ed75e1d439c96d75d2b2"
-      },
-      "dob": "1955-07-28 22:32:14",
-      "registered": "2010-01-10 06:52:31",
-      "phone": "(944)-261-2164",
-      "cell": "(888)-556-7285",
-      "picture": {
-          "large": "https://randomuser.me/api/portraits/men/4.jpg",
-          "medium": "https://randomuser.me/api/portraits/med/men/4.jpg",
-          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/4.jpg"
-      },
-      "nat": "US"
-  }
+    let user = {
+      username: "yellowleopard",
+      password: "jonjon"
+    }
     // act
     chai
       .request(server)
       .post('/api/login')
-      .send(testUser2)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send(user)
       .end((err, res) => {
         // assert
         res.should.have.status(401);
@@ -79,38 +49,36 @@ describe('/POST User', () => {
       });
   });
 
-
-  it('it should fail with a 400 error if email is not provided', done => {
+  it('it should fail with a 401 error if password is incorrect', done => {
     // arrange
-    let testUser3 =  {
-      "cart":[],
-      "name": {
-          "title": "mrs",
-          "first": "susanna",
-          "last": "richards"
-      },
-      "location": {
-          "street": "2343 herbert road",
-          "city": "duleek",
-          "state": "donegal",
-          "postcode": 38567
-      },
-      "email": "",
-      "login": {
-          "username": "yellowleopard753",
-          "password": "jonjon",
-          "salt": "eNuMvema",
-          "md5": "a8be2a69c8c91684588f4e1a29442dd7",
-          "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
-          "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
-      },
-      "registered": "2003-08-03 01:12:24",
-  };
+    let user = {
+      username: "yellowleopard753",
+      password: "jon"
+    }
     // act
     chai
       .request(server)
       .post('/api/login')
-      .send(testUser3)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send(user)
+      .end((err, res) => {
+        // assert
+        res.should.have.status(401);
+        done();
+      });
+  });
+
+  it('it should fail with a 400 error if the username and password are not sent or improperly formatted', done => {
+    // arrange
+    let user = {
+      password: "jonjon"
+    }
+    // act
+    chai
+      .request(server)
+      .post('/api/login')
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send(user)
       .end((err, res) => {
         // assert
         res.should.have.status(400);
@@ -118,37 +86,17 @@ describe('/POST User', () => {
       });
   });
 
-  it('it should fail with a 400 error if username is not provided', done => {
+  it('it should fail with a 400 error if the username and password are not sent or improperly formatted', done => {
     // arrange
-    let testUser4 =  {
-      "cart":[],
-      "name": {
-          "title": "mrs",
-          "first": "susanna",
-          "last": "richards"
-      },
-      "location": {
-          "street": "2343 herbert road",
-          "city": "duleek",
-          "state": "donegal",
-          "postcode": 38567
-      },
-      "email": "susanna.richards@example.com",
-      "login": {
-          "username": "",
-          "password": "jonjon",
-          "salt": "eNuMvema",
-          "md5": "a8be2a69c8c91684588f4e1a29442dd7",
-          "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
-          "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
-      },
-      "registered": "2003-08-03 01:12:24",
-  };
+    let user = {
+      username: "yellowleopard753"
+    }
     // act
     chai
       .request(server)
       .post('/api/login')
-      .send(testUser4)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send(user)
       .end((err, res) => {
         // assert
         res.should.have.status(400);
@@ -157,41 +105,43 @@ describe('/POST User', () => {
   });
 
 
-  it('it should fail with a 400 error if password is not provided', done => {
-    // arrange
-    let testUser5 =  {
-      "cart":[],
-      "name": {
-          "title": "mrs",
-          "first": "susanna",
-          "last": "richards"
-      },
-      "location": {
-          "street": "2343 herbert road",
-          "city": "duleek",
-          "state": "donegal",
-          "postcode": 38567
-      },
-      "email": "susanna.richards@example.com",
-      "login": {
-          "username": "yellowleopard753",
-          "password": "",
-          "salt": "eNuMvema",
-          "md5": "a8be2a69c8c91684588f4e1a29442dd7",
-          "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
-          "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
-      },
-      "registered": "2003-08-03 01:12:24",
-  };
-    // act
-    chai
-      .request(server)
-      .post('/api/login')
-      .send(testUser5)
-      .end((err, res) => {
-        // assert
-        res.should.have.status(400);
-        done();
-      });
-  });
-});
+
+
+//   it('it should fail with a 400 error if password is not provided', done => {
+//     // arrange
+//     let testUser5 =  {
+//       "cart":[],
+//       "name": {
+//           "title": "mrs",
+//           "first": "susanna",
+//           "last": "richards"
+//       },
+//       "location": {
+//           "street": "2343 herbert road",
+//           "city": "duleek",
+//           "state": "donegal",
+//           "postcode": 38567
+//       },
+//       "email": "susanna.richards@example.com",
+//       "login": {
+//           "username": "yellowleopard753",
+//           "password": "",
+//           "salt": "eNuMvema",
+//           "md5": "a8be2a69c8c91684588f4e1a29442dd7",
+//           "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
+//           "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
+//       },
+//       "registered": "2003-08-03 01:12:24",
+//   };
+//     // act
+//     chai
+//       .request(server)
+//       .post('/api/login')
+//       .send(testUser5)
+//       .end((err, res) => {
+//         // assert
+//         res.should.have.status(400);
+//         done();
+//       });
+//   });
+ });
