ebf73309788cdde7904a9414c0a8f67c9c1fd558
MCola <mcolapietro502@gmail.com>
2020-06-24 00:55:58 -0400

    completed cart router calls; added error checks to login router call; added error responses to GET brands/:id/products call

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
-
+const CORS_HEADERS = {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept, X-Authentication"};
 const PORT = 3001;
+
 let products = [];
 let brands = [];
 let cart = [];
@@ -16,6 +17,10 @@ let users = [];
 
 //Setup router
 var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+// parse application/x-www-form-urlencoded
+myRouter.use(bodyParser.urlencoded({ extended: false }))
 
 let server = http.createServer(function (request, response) {
 
@@ -46,12 +51,20 @@ myRouter.get('/brands', function(request,response) {
 });
 
 myRouter.get('/brands/:id/products', function(request,response) {
-  let brandId = request.params
-  //find out brand id in request
-  //compare it to products categoryId
-  //return back whatever matches
+  //retrieve brand id from request
+  let brandId = request.params.id
+  
+  //compare brand id to product's categoryId
+  let productMatch = products.filter(product => product.categoryId === brandId)
+
+  if (productMatch. length === 0) {
+    response.writeHead(404, "No products match that Id");
+    return response.end();
+  }
+
+  //return back matches
 	response.writeHead(200, { "Content-Type": "application/json" });
-	return response.end(JSON.stringify(brands));
+	return response.end(JSON.stringify(productMatch));
 });
 
 myRouter.post('/api/login', function(request,response) {
@@ -84,44 +97,116 @@ myRouter.post('/api/login', function(request,response) {
         accessTokens.push(newAccessToken);
         return response.end(JSON.stringify(newAccessToken.token));
       }
+    } else {
+      // When a login fails, tell the client in a generic way that either the username or password was wrong
+      response.writeHead(401, "Invalid username or password");
+      return response.end();
     }
+  } else {
+    // If they are missing one of the parameters, tell the client that something was wrong in the formatting of the response
+    response.writeHead(400, "Incorrectly formatted response");
+    return response.end();
   }
 });
 
 myRouter.get('/me/cart', function(request,response) {
-  //look at request for access token
-  //if accesstoken; what user matches accesstoken
-  //return user's cart
+  //pull from request the access token
+  let token = request.body.token
   
-  // Return all products in the products array
+  //match access token from body of request to accessTokens array
+  let tokenMatch = accessTokens.find((access) => {
+    return access.token == token;
+  })
+
+  //match username in accessToken array to users array
+  let userMatch = users.find((user) =>  {
+    return user.login.username == tokenMatch.username
+  })
+
+  //return user's cart
 	response.writeHead(200, { "Content-Type": "application/json" });
-	return response.end(JSON.stringify(cart));
+  return response.end(JSON.stringify(userMatch.cart))
 });
 
 myRouter.post('/me/cart', function(request,response) {
-  const addedProductToCart = cart.push(request.body)
+  //pull from request the access token & product to post to cart
+  let token = request.body.token
+  let productId = request.body.item
+  
+  //match access token from body of request to accessTokens array
+  let tokenMatch = accessTokens.find((access) => {
+    return access.token == token;
+  })
 
-  // Return all success with added Product
+  //match username in accessToken array to users array
+  let userMatch = users.find((user) =>  {
+    return user.login.username == tokenMatch.username
+  });
+
+  let productToAdd = products.find((product) => {
+    return product.id == productId
+  });
+  
+  //add product from request body to user's cart
+  userMatch.cart.push(productToAdd)
+
 	response.writeHead(200, { "Content-Type": "application/json" });
-	return response.end(JSON.stringify(cart));
+	return response.end(JSON.stringify(productToAdd));
 });
 
 
 myRouter.delete('/me/cart/:productId', function(request,response) {
-  const foundCartItem = cart.filter((item => item.id == request.id))
+  //pull from request the access token & product to delete from cart
+  let productToDelete = request.params.productId;
+  let token = request.body.token;
 
-  cart.remove(foundCartItem)
+  //match access token from body of request to accessTokens array
+  let tokenMatch = accessTokens.find((access) => {
+    return access.token == token;
+  })
+
+  //match username in accessToken array to users array
+  let userMatch = users.find((user) =>  {
+    return user.login.username == tokenMatch.username
+  })
+
+  //find location in array of the first item in cart that matches
+  //the product id in the request
+  let productToDeleteInCart = userMatch.cart.findIndex((product) => {
+    return product.id == productToDelete
+  });
+  
+  //remove product from user's cart
+  userMatch.cart.splice(productToDeleteInCart, 1)
 
 	response.writeHead(200, { "Content-Type": "application/json" });
-	return response.end();
+	return response.end(`Product successfully deleted`);
 });
 
 myRouter.post('/me/cart/:productId', function(request,response) {
-  const addedProductToCart = cart.push(request.body)
+  //pull from request the access token & product to post to cart
+  let token = request.body.token
+  let productId = request.params.productId
+  
+  //match access token from body of request to accessTokens array
+  let tokenMatch = accessTokens.find((access) => {
+    return access.token == token;
+  })
 
-  // Return all success with added Product
+  //match username in accessToken array to users array
+  let userMatch = users.find((user) =>  {
+    return user.login.username == tokenMatch.username
+  });
+
+  let productToAdd = products.find((product) => {
+    return product.id == productId
+  });
+  
+  //add product from request body to user's cart
+  userMatch.cart.push(productToAdd)
+
 	response.writeHead(200, { "Content-Type": "application/json" });
-	return response.end(JSON.stringify(cart));
+	return response.end(JSON.stringify(productToAdd));
 });
 
 module.exports = {server, products}
