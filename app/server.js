3e8e238bcb34a0a8fe045ef26918731e49dbff41
PJ Foley <pjfoley@gmail.com>
2020-09-25 16:33:59 -0700

    preliminary products return and search

--- app/server.js
+++ app/server.js
@@ -5,9 +5,141 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
-
+//const TOKEN_VALIDITY_TIMEOUT = 900000 // 15 minutes
 const PORT = 3001;
+// states
+brands = [];
+products = [];
+users = [];
+cart = [];
+//var accessTokens = [];
+//var failedLoginAttempts = {};
 
-http.createServer(function (request, response) {
+// Setup router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+let server = http.createServer(function (request, response) {
+//before any REST requests, validation could happen here
+
+// 
+//route commands to myRouter
+myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error on Server Startup: ', error)
+  }
+ // load data upon verifing server is a go 
+ // load Brands data
+fs.readFile("initial-data/brands.json", "utf8", (error, data) => {
+  if (error) throw error;
+  brands = JSON.parse(data);
+  console.log(`Server setup: ${brands.length} Brands loaded`);
+});
+
+// load Products data
+fs.readFile("initial-data/products.json", "utf8", (error, data) => {
+  if (error) throw error;
+  products = JSON.parse(data);
+  console.log(`Server setup: ${products.length} Products loaded`);
+});
+
+// load Users data
+fs.readFile("initial-data/users.json", "utf8", (error, data) => {
+  if (error) throw error;
+  users = JSON.parse(data);
+  console.log(`Server setup: ${users.length} Users loaded`);
+}); 
+  
+console.log(`Server ready on ${PORT}`);
+});
+
+
+// Routing starts
+
+// Public route - no login required
+// Brands
+// -List of all brands
+// -List first X brands in list with number query
+myRouter.get('/api/brands/', function(request,response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  var parsedUrl = require('url').parse(request.url,true)
+  if (parsedUrl.query.number) {
+    return response.end(JSON.stringify(brands.slice(0,parsedUrl.query.number)));
+  } else {
+    return response.end(JSON.stringify(brands));
+}});
+// -Return all products of a specified brandId
+myRouter.get('/api/brands/:brandId/products', function(request,response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  // console.log('proper parsing test ', parsedUrl);
+  // console.log('query test', request.params.brandId)
+  if (request.params.brandId) {
+    let niceResults = products.filter(item => item.categoryId===request.params.brandId);
+    return response.end(JSON.stringify(niceResults));
+  } else {
+    // TODO return error
+    return response.end(JSON.stringify(products));
+}});
+// Products
+// -List all products
+// -Return product search results with search params
+myRouter.get('/api/products', function(request,response) {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  var parsedUrl = require('url').parse(request.url,true)
+  console.log('proper parsing test ', parsedUrl);
+  if (parsedUrl.query.search) {
+    let niceResults = products.filter(item => item.name===parsedUrl.query.search);
+    // TODO make a robust search of some type
+    return response.end(JSON.stringify(niceResults));
+  } else {
+    return response.end(JSON.stringify(products));
+}});    
+
+// Login
+// -Verify username and password, issue token
+myRouter.get('/api/login', function(request,response) {
+  response.writeHead(418, { "Content-Type": "application/json" });
+  var parsedUrl = require('url').parse(request.url,true)
+  console.log('proper parsing test ', parsedUrl);
+  if (parsedUrl.query.username && parsedUrl.query.password) {
+    
+    // TODO integrate login code
+    
+  } else {
+    return response.end('TODO Fail Message');
+}});    
+
+// Cart -- login required
+// -Verify valid token, then allow access
+myRouter.get('api/cart', function(request,response) {
+  response.writeHead(418, { "Content-Type": "application/json" });
+  // check for token
+  // else send login required error 401
+});
+// -Add product to shopping cart
+myRouter.post('api/cart', function(request,response) {
+  response.writeHead(418, { "Content-Type": "application/json" });
+  // check for token
+  // add product to cart (parseUrl)
+  // else send login required error 401
+});
+// -Update quantity of product in cart
+myRouter.post('api/cart/:prodId', function(request,response) {
+  response.writeHead(418, { "Content-Type": "application/json" });
+  // parseUrl and params
+  // check for token
+  // check for existing product
+  // increment product to cart
+  // else send login required error 401
+});
+// -Delete product from cart
+myRouter.delete('api/cart/:prodId', function(request,response) {
+  response.writeHead(418, { "Content-Type": "application/json" });
+  // parseUrl and params
+  // check for token
+  // check for existing product
+  // increment product to cart
+  // else send login required error 401
+});
+module.exports = server; 
--- test/server-test.js
+++ test/server-test.js
@@ -1,25 +1,298 @@
-
+const {
+  expect
+} = require('chai');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app/server.js');
 let should = chai.should();
 chai.use(chaiHttp);
 
-// describe('PRODUCTS: The server', () => {
-  describe('GET /brands should return', () => {
-    it('all brands and IDs as array of objects', done => {
-      
-     chai
+
+describe('GET /brands should return', () => {
+  it('all brands and IDs as array of objects', done => {
+
+    chai
       // arrange  
       .request(server)
-        // act
-        .get('/api/brands')
-        .end((err, res) => {
-          // assert
-          res.should.have.status(200);
-          // res.body.should.be.an('array');
-          done();
-        });
-    });
-    });
-  // });
+      // act
+      .get('/api/brands')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.eql(5);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('specified number of brands returned', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .get('/api/brands?number=3')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.eql(3);
+        done();
+      })
+  });
+  it('returns all products based on brand id', done => {
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .get('/api/brands/1/products')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.eql(3);
+        done();
+      })
+  });
+
+});
+
+describe('GET /products should return', () => {
+  it('all products in catalog as array of objects', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .get('/api/products')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.eql(11);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+  it('Products based on search term', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .get('/api/products?search=QDogs Glasses')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        res.body.should.be.an('array');
+        res.body.length.should.be.eql(1);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+});
+
+describe('POST /login should process login', () => {
+  it('known good username/password is granted token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/login')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('bad username/password is denied token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/login')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+  // this isn't in spec and is at-risk for implmentation
+  /* it('expires and removes token on /logout', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/logout') // for future notes: https://tinyurl.com/yy9ypzzw
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  }); */
+});
+
+describe('GET /cart return cart contents', () => {
+  it('requires valid token to return results', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/cart')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('denies access to cart without token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/cart')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+});
+
+describe('POST /cart adds product to cart', () => {
+  it('requires valid token to add product', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/me/cart')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('denies product added to cart if no valid token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/me/cart')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+});
+
+describe('DELETE /cart/:productId removes product from cart', () => {
+  it('requires valid token to remove product', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .delete('/api/me/cart/1')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('denies product removal from cart if no valid token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .delete('/api/me/cart/1')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+});
+
+describe('POST /cart/:productId removes product from cart', () => {
+  it('requires valid token to remove product', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/me/cart/1')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+
+  it('denies product removal from cart if no valid token', done => {
+
+    chai
+      // arrange  
+      .request(server)
+      // act
+      .post('/api/me/cart/1')
+      .end((err, res) => {
+        // assert
+        res.should.have.status(200);
+        var names = [];
+        // res.forEach(function(item) {names.push(item.name);});
+        // expect(names).to.have.members(['Oakley', 'Ray Ban', 'Levi\'s', 'DKNY', 'Burberry']);
+        done();
+      });
+  });
+});
