afb122c2c299c7e03ffe2929efb9d4e8f4d1f676
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-09-25 14:10:08 -0400

    initial commit, created test folder, added mocha test server into our launch.json, setup initial router, server and integrated our data into our project

--- app/server.js
+++ app/server.js
@@ -3,11 +3,44 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
+// to parse request bodies
 var bodyParser   = require('body-parser');
+// to generate access tokens
 var uid = require('rand-token').uid;
+// variables
+let brands = [];
+let products = [];
+let users = [];
 
 const PORT = 3001;
+// setup router
+const myRouter = Router();
+myRouter.use(bodyParser.json());
+// create server and import data from files in the project
+let server = http.createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, (error) => {
+  if (error) {
+    return console.log('Error on Server Startup: ', error)
+  }
+  fs.readFile('./initial-data/brands.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  fs.readFile('./initial-data/products.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+  fs.readFile('./initial-data/users.json', 'utf8', function (error, data) {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  }); 
+  console.log(`Server is listening on ${PORT}`);
+});
 
-http.createServer(function (request, response) {
 
-}).listen(PORT);
+module.exports = server;
+
---
+++ test/sunglasses.js
@@ -0,0 +1,8 @@
+// define our dependencies/required variables
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
