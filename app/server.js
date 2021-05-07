a56fc19a2cdbc18b0536416db6cb997018a9248d
unknown <joshkamrass@gmail.com>
2021-05-07 06:07:17 -0400

    initial server setup

--- app/server.js
+++ app/server.js
@@ -8,6 +8,33 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, error => {
+  if (error) {
+    return console.log("Error on server startup: ", error);
+  }
+
+  fs.readFile("initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server loaded brands file`)
+  })
+  
+  fs.readFile("initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server loaded products file`)
+  })
+  
+  fs.readFile("initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server loaded users file`)
+  })
+
+  console.log(`Server is listening on port ${PORT}`);
+});
