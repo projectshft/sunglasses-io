347ab125a98e0fb211a9239b879a3a7983790fc2
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-06-21 11:41:35 -0400

    Mocha first test up and running

---
+++ test/sunglasses-io.js
@@ -0,0 +1,24 @@
+let Sunglasses = require("../app/models/sunglasses-io");
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Sunglasses", () => {
+  describe("/GET brands", () => {
+    it("should get all the brands", (done) => {
+      chai
+        .request(server)
+        .get("/brands")
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("array");
+          done();
+        });
+    });
+  });
+});
