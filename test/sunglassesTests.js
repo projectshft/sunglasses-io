e8851171c1132cf05f4840646bf91a96712570e6
Jeremy Wheless <whelessjw@gmail.com>
2021-07-08 16:41:51 -0400

    wrote test for requesting items in users cart

--- test/sunglassesTests.js
+++ test/sunglassesTests.js
@@ -8,6 +8,22 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("Sunglasses.io", () => {
+  let token;
+  let user = { username: "yellowleopard753", password: "jonjon" };
+
+  before((done) => {
+    chai
+      .request(server)
+      .post(`/api/login`)
+      .set("content-type", "application/json")
+      .send(user)
+      .end((err, res) => {
+        res.should.have.status(200);
+        token = res.body;
+        done();
+      });
+  });
+
   describe("When requesting all the brands", () => {
     it("should list all the brands in the store", (done) => {
       chai
@@ -77,43 +93,54 @@ describe("Sunglasses.io", () => {
 
   describe("When requesting to login", () => {
     it("should return an error if login credentials are incorrect", (done) => {
-      fs.readFile("initial-data/users.json", "utf8", (error, data) => {
-        if (error) throw error;
-        let users = JSON.parse(data);
-        let username = users[0].login.username;
-        let password = users[0].login.password;
-
-        chai
-          .request(server)
-          .post(`/api/login`)
-          .set("content-type", "application/json")
-          .send({ username, password: password + password })
-          .end((err, res) => {
-            res.should.have.status(401, "Invalid username or password");
-            done();
-          });
-      });
+      chai
+        .request(server)
+        .post(`/api/login`)
+        .set("content-type", "application/json")
+        .send({ username: "invalidUsername", password: "invalidPassword" })
+        .end((err, res) => {
+          res.should.have.status(401, "Invalid username or password");
+          done();
+        });
     });
 
     it("should provide an access token if login credentials are valid", (done) => {
-      fs.readFile("initial-data/users.json", "utf8", (error, data) => {
-        if (error) throw error;
-        let users = JSON.parse(data);
-        let username = users[0].login.username;
-        let password = users[0].login.password;
+      chai
+        .request(server)
+        .post(`/api/login`)
+        .set("content-type", "application/json")
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("string");
+          res.body.length.should.be.eql(16);
+          done();
+        });
+    });
+  });
 
-        chai
-          .request(server)
-          .post(`/api/login`)
-          .set("content-type", "application/json")
-          .send({ username, password })
-          .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a("string");
-            res.body.length.should.be.eql(16);
-            done();
-          });
-      });
+  describe("When requesting the items from a user's cart", () => {
+    it("should return an error if the user is not logged in", (done) => {
+      chai
+        .request(server)
+        .get("/api/me/cart")
+        .query("invalidToken")
+        .end((err, res) => {
+          res.should.have.status(401, "You must be logged in to view the cart");
+          done();
+        });
+    });
+
+    it("should return a list of products from the user's cart", (done) => {
+      chai
+        .request(server)
+        .get("/api/me/cart")
+        .query(token)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
     });
   });
 });
