055986cbaad06a61dc0807a03154f8625ceb231d
Benjamin Pryor <bscott23@gmail.com>
2021-08-31 12:04:41 -0400

    added more tests updated swagger

--- test/server-test.js
+++ test/server-test.js
@@ -63,3 +63,103 @@ describe('Get sunglasses by brand', () => {
   })
 })
 
+describe('Login request', () => {
+  describe('/POST username and password to recieve an access token', () => {
+    it('it should provide an access token for a correct login', (done) => {
+      let user = {
+        "username": "yellowleopard753",
+        "password": "jonjon"
+      }
+      chai
+        .request(server)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('username');
+          res.body.should.have.property('access_token');
+          res.body.should.have.property('last_updated');
+          done();
+        })
+    })
+    it('it should throw an error if the username is incorrect', (done) => {
+      let user = {
+        "username": "greenleopard752",
+        "password": "jonjon"
+      }
+      chai
+        .request(server)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          err.should.have.status(401);
+          done();
+        })
+    })
+    it('it should throw an error if the password is incorrect', (done) => {
+      let user = {
+        "username": "yellowleopard753",
+        "password": "jon"
+      }
+      chai
+        .request(server)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          err.should.have.status(401);
+          done();
+        })
+    })
+    it('it should throw an error if the password is missing', (done) => {
+      let user = {
+        "username": "yellowleopard753",
+      }
+      chai
+        .request(server)
+        .post('/login')
+        .send(user)
+        .end((err, res) => {
+          err.should.have.status(400);
+          done();
+        })
+    })
+  })
+});
+
+describe('Cart', () => {
+  describe('/GET cart', () => {
+    it('should return a cart full of two products', (done) => {
+      //fill cart with 2 products
+      chai
+        .request(server)
+        .get('/me/cart')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.be.eql(2);
+          done();
+        })
+    })
+  })
+  describe('/POST to add to cart', () => {
+    it('should return a cart with three products including the one just added', (done) => {
+      //fill cart with 2 products
+      chai
+        .request(server)
+        .post('/me/cart')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.should.be.eql(3);
+          done();
+        })
+    })
+  })
+  describe('/DELETE item from cart', () => {
+
+  })
+  describe('/PUT to change quantity of item in cart', () => {
+
+  })
+})
