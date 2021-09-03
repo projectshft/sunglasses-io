862c3452f47d5ef10aac354c2f567565186e9e06
Otto Frankel <ottofrankel@comcast.net>
2021-09-03 09:21:28 -0700

    Initial server setup

--- app/server.js
+++ app/server.js
@@ -8,6 +8,38 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+let brands = [];
+let products = [];
+let users = [];
 
-}).listen(PORT);
+const router = Router();
+router.use(bodyParser.json());
+
+http.createServer(function (request, response) {
+  router(request, response, finalHandler(request, response))
+}).listen(PORT, (err) => {
+
+  if (err) {
+    return console.log('Error on Server Startup: ', err)
+  }
+
+  fs.readFile('./initial-data/brands.json', 'utf8', (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+
+  fs.readFile('./initial-data/products.json', 'utf-8', (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} users loaded`);
+  })
+
+  fs.readFile('./initial-data/users.json', 'utf8', (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+
+  console.log(`Server is listening on ${PORT}`);
+});
