ecaef803df7a643576760ce4492a851d35a02314
Steve Husmann <86138241+stevehusmann@users.noreply.github.com>
2021-12-04 00:33:15 -0600

    Products, Brands, and Login working

--- app/server.js
+++ app/server.js
@@ -6,11 +6,11 @@ var Router = require('router');
 var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
-// // State holding variables
-// let brands = [];
-// let user = {};
-// let products = [];
-// let users = [];
+// State holding variables
+let brands = [];
+let products = [];
+let users = [];
+let accessTokens = [];
 
 const PORT = 3001;
 
@@ -21,7 +21,8 @@ router.use(bodyParser.json());
 const server = http.createServer(function (request, response) {
   response.writeHead(200);
   router(request, response, finalHandler(request, response))
-}).listen(PORT);
+
+});
 
 server.listen(PORT, error => {
   if (error) throw error;
@@ -35,10 +36,73 @@ server.listen(PORT, error => {
 
   //populate users
   users = JSON.parse(fs.readFileSync("initial-data/users.json","utf-8"));
+  
+});
+
+//GET api/products
+router.get('/api/products', (request, response) => {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(products));
 });
 
 //GET api/brands
 router.get('/api/brands', (request, response) => {
   response.writeHead(200, { "Content-Type": "application/json" });
-  return response.end(JSON.parse(fs.readFileSync("initial-data/brands.json","utf-8")));
+  return response.end(JSON.stringify(brands));
 });
+
+//GET api/brands/:brandId/products
+router.get('/api/brands/:brandId/products', (request, response) => {
+  const { brandId } = request.params;
+  const brandToFilterBy = brands.find(brand => brand.id == brandId);
+  if (!brandToFilterBy) {
+    response.writeHead(404, "There are no products associated with that brand ID");
+    return response.end();
+  }
+  
+  response.writeHead(200, { "Content-Type": "application/json" });
+  const filteredProducts = products.filter(
+    products => products.categoryId === brandToFilterBy.id
+  );
+  
+  return response.end(JSON.stringify(filteredProducts));
+});
+
+//POST api/login
+router.post('/api/login', (request, response) => {
+  if (request.body.username && request.body.password) {
+    let user = users.find((user) => {
+      return user.login.username == request.body.username && user.login.password == request.body.password;
+    });
+    if (user) {
+      response.writeHead(200, {'Content-Type': 'application/json'});
+      
+      let currentAccessToken = accessTokens.find((tokenObject) => {
+        return tokenObject.username == user.login.username;
+      });
+
+      if (currentAccessToken) {
+        currentAccessToken.lastUpdated = new Date();
+        return response.end(JSON.stringify(currentAccessToken.token));
+        
+      } else {
+        let newAccessToken = {
+          username: user.login.username,
+          lastUpdated: new Date(),
+          token: uid(16)
+        }
+        accessTokens.push(newAccessToken);
+        return response.end(JSON.stringify(newAccessToken.token));
+      }
+
+    } else {
+      response.writeHead(401, "Invalid username or password");
+      return response.end();      
+    }
+} else {
+    response.writeHead(400, "Incorrectly formatted response");
+    return response.end();
+  }
+});
+
+module.exports = server;
---
+++ test/sunglasses-tests.js
@@ -0,0 +1,167 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app/server');
+var Router = require('router');
+var bodyParser = require('body-parser');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+//setup router
+const router = Router();
+router.use(bodyParser.json());
+
+describe ('Products', () => {
+  
+  describe('/GET api/products', () => {
+    it('it should GET all of the products', done => {
+      chai
+        .request(server)
+        .get('/api/products')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an('array');
+          response.body.forEach(e => {
+            e.should.have.property('id');
+            e.should.have.property('categoryId');
+            e.should.have.property('name');
+            e.should.have.property('description');
+            e.should.have.property('price');
+            e.should.have.property('imageUrls');
+          });
+          done();
+        });
+    });
+  });
+});
+
+describe ('Brands', () => {
+  
+  describe('/GET api/brands', () => {
+    it('it should GET the brands', done => {
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((error, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an('array');
+          response.body.forEach(e => {
+            e.should.have.property('id');
+            e.should.have.property('name');
+          });
+          done();
+        });
+    });
+  });
+
+  describe('/GET api/brands/:id/products', () => {
+    it('it should GET all products with a certain brand id.', done => {
+      chai.
+      request(server)
+      .get('/api/brands')
+      .end((error, response) => {
+        const brands = response.body;
+        if(brands) {
+          brands.forEach(brand => {
+            chai
+            .request(server)
+            .get(`/api/brands/${brand.id}/products`)
+            .end((error, response) => {
+              response.should.have.status(200);
+              response.body.should.be.an('array');
+              response.body.forEach(e => {
+                e.categoryId.should.equal(brand.id);
+              });
+            });
+          });
+        }
+      done();
+      })
+    });
+
+    it('it should FAIL if the brand id is not legitimate.', done => {
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((error, response) => {
+          const brands = response.body;
+          const allLegitBrandIds = [];
+          brands.forEach(brand => allLegitBrandIds.push(parseInt(brand.id)));
+          let illegitBrandId = Math.floor(Math.random() * 50);
+          while (allLegitBrandIds.includes(illegitBrandId)) {
+            illegitBrandId = Math.floor(Math.random() * 50);
+          }
+          chai
+            .request(server)
+            .get(`/api/brands/${illegitBrandId}/products`)
+            .end((error, response) => {
+              response.should.have.status(404);
+              done();
+            });
+      });
+    });
+
+  });
+});
+
+describe ('Login', () => {
+  let currentAccessToken = '';
+
+  describe ('/POST /api/login', () => {
+    it('it should LOGIN a user with a valid login', done => {
+      
+      let goodLoginAttempt = {
+        username: 'yellowleopard753',
+        password: "jonjon"
+      }
+      
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(goodLoginAttempt)
+        .end((error, response) => {
+          response.should.have.status(200)
+          response.body.should.be.a('string');
+          currentAccessToken = response.body;
+          done();
+        })
+    });
+
+    it('it should REJECT a user with an poorly formatted login', done => {
+      let poorlyFormattedLoginAttempt = {
+        username: 'redelephant864'
+      }
+      
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(poorlyFormattedLoginAttempt)
+        .end((error, response) => {
+          response.should.have.status(400)
+          done();
+        })
+    });
+
+
+    it('it should REJECT a user with an incorrect username or password', done => {
+      let badLoginAttempt = {
+        username: 'redelephant864',
+        password: 'whatever'
+      }
+      
+      chai
+        .request(server)
+        .post('/api/login')
+        .send(badLoginAttempt)
+        .end((error, response) => {
+          response.should.have.status(401);
+          done();
+        })
+    });
+
+  });
+
+  
+
+});
