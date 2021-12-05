61b8282164e318449d44fa9870293246961c933e
Kush Patel <knp314@gmail.com>
2021-12-05 00:53:33 -0500

    Initial commit

--- app/server.js
+++ app/server.js
@@ -1,13 +1,67 @@
 var http = require('http');
 var fs = require('fs');
 var finalHandler = require('finalhandler');
+const url = require("url");
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
-
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+let brands = [];
+let products = [];
+let users = [];
 
-}).listen(PORT);
+var router = Router();
+router.use(bodyParser.json());
+
+let server = http.createServer(function (request, response) {
+  router(request, response, finalHandler(request, response))
+}).listen(PORT, ()=> {
+  // load initial data from files
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json", "utf-8"));
+  products = JSON.parse(fs.readFileSync("initial-data/products.json", "utf-8"));
+  users = JSON.parse(fs.readFileSync("initial-data/users.json", "utf-8"));
+});
+
+// GET all brands
+router.get('/api/brands', (request, response) => {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(brands));
+});
+
+// GET products of a given brand
+router.get('/api/brands/:id/products', (request, response) => {
+  const brandId = request.params.id;
+  const brand = brands.find(brand => brand.id === brandId);
+  
+  if (!brand) {
+    response.writeHead(404, "That brand was not found");
+    return response.end();
+  }
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  const productsOfGivenBrand = products.filter(product => product.categoryId === brandId);
+  return response.end(JSON.stringify(productsOfGivenBrand));
+});
+
+// GET products matching a given search query
+router.get('/api/products', (request, response) => {
+  const parsedUrl = url.parse(request.originalUrl);
+  const { query } = queryString.parse(parsedUrl.query);
+  let productsWithQuery = [];
+
+  if (query) {
+    productsWithQuery = products.filter(product => product.description.includes(query))
+  }
+
+  if (productsWithQuery.length === 0) {
+    response.writeHead(404, "There are no products matching that query");
+    return response.end();
+  }
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(productsWithQuery));
+});
+
+module.exports = server;
---
+++ test/brands.js
@@ -0,0 +1,49 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("it should GET all the brands", (done) => {
+      chai
+        .request(server)
+        .get("/api/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(5);
+          done();
+        });
+    });
+  });
+
+  describe("/GET brands/:id/products", () => {
+    it("it should GET all products of the requested brand", (done) => {
+      // brand id 1 is Oakley, which has 3 products in the store, including the Superglasses
+      let id = "1";
+      let superglasses = {
+        "id": "1",
+        "categoryId": "1",
+        "name": "Superglasses",
+        "description": "The best glasses in the world",
+        "price":150,
+        "imageUrls":["https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg"]
+      }
+
+      chai
+        .request(server)
+        .get(`/api/brands/${id}/products`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(3);
+          res.body.should.deep.include.members(superglasses);
+          done();
+      });
+    });
+  })
+});
