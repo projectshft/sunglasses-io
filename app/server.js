44c561e328ded9d1e575d478dde07dba1231caa3
Jason Stover <jstover8807@gmail.com>
2021-12-07 12:38:57 -0500

    three tests passing, working on login test

--- app/server.js
+++ app/server.js
@@ -5,12 +5,13 @@ var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
+const newAccessToken = uid(32);
 
 // stand in variables for dummy api values
 let brands = [];
 let products = [];
 let users = [];
-let user = [];
+let accessTokens = [];
 
 // setup router
 var myRouter = Router();
@@ -21,12 +22,11 @@ const PORT = 3001;
 let server = http.createServer(function (request, response) {
     myRouter(request, response, finalHandler(request, response))
 }).listen(PORT, () => {
-    brands = JSON.parse(fs.readFileSync("brands.json", "utf-8"));
-    products = JSON.parse(fs.readFileSync("products.json", "utf-8"));
-    users = JSON.parse(fs.readFileSync("users.json","utf-8"));
-    user = [0];
-
-    //console.log(brands);
+    brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf-8"));
+    products = JSON.parse(fs.readFileSync("./initial-data/products.json", "utf-8"));
+    users = JSON.parse(fs.readFileSync("./initial-data/users.json","utf-8"));
+   
+    
 });
 
 myRouter.get('/brands', function(request,response) {    
@@ -34,14 +34,59 @@ myRouter.get('/brands', function(request,response) {
     return response.end(JSON.stringify(brands));
 }); 
 
+myRouter.get('/products', function(request,response) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(products));
+});
 
 
+// Multiple params destructured in order based on URL
+myRouter.get('/brands/:id/products', function(request,response) {
+    // const {ID1, ID2} = request.params  with a URL of brands/:ID1/products/:ID2
+    const { id } = request.params;  //Will this pull the ":id" from the url?
+    const product = products.find(product => product.id == id);
 
+    if (!product) {
+        response.writeHead(404, "That product does not exist");
+        return response.end();
+    }
+    response.writeHead(200, { "Content-Type": "application/json" });  //Content-Type = [headers]
+    return response.end(JSON.stringify(product));
+});
 
+myRouter.post('/login', (request,response) => {
+    // See if there is a user that has that name and password
+    if (request.body.username && request.body.password) {
+        let user = users.find((user) => {
+            return user.login.username == request.body.username && user.login.password == request.body.password;
+        });
+        if (user) {
+            response.writeHead(200, { "Content-Type": "application/json" });
+            let currentAccessToken = accessTokens.find((tokenObject) => {
+                return tokenObject.username == user.login.username;
+            });
 
-
-
-
+            if (currentAccessToken) {
+                currentAccessToken.lasUpdated = new Date();
+                return response.end(JSON.stringify(currentAccessToken.token));
+            } else {
+                let newAccessToken = {
+                    username: user.login.username,
+                    lastUpdated: new Date(),
+                    token: uid(32)                 
+                }
+                accessTokens.push(newAccessToken);
+                return response.end(JSON.stringify(newAccesstoken.token));
+            }
+        } else {
+            response.writeHead(401, "Invalid username or password");
+            return response.end();
+        }    
+    } else {
+        response.writeHead(400, "One or more parameters are missing");
+        return response.end();
+    }    
+});
 
 
 
--- test/server-test.js
+++ test/server-test.js
@@ -6,37 +6,22 @@ let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('/GET brands', () => {
-    it('it should GET all of the brands', done => {
-        chai
+/*
+describe('/GET me/cart', () => {
+    it('it should return the users cart', done => {
+        chai    
             .request(server)
-            .get('/brands')
+            .post('/login')
+            .send({ username: '', password: ''})
+            .get('/me/cart')
             .end((err, res) => {
                 res.should.have.status(200);
                 res.body.should.be.an('array');
-                res.body.length.should.be.eql(5);
                 done();
             });
     });
 });
-
-
-
-/*
-describe('/GET brands/:id/products', () => {
-    it('it should GET a single product', done => {
-        chai
-            .request(server)
-            .get('/brands/:id/products')
-            .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.an('object');
-                res.body.length.should.be.eql(1);
-                done();
-            });
-    });
-});
-
+ */
 describe('/GET products', () => {
     it('it should GET all products', done => {
         chai    
@@ -51,18 +36,94 @@ describe('/GET products', () => {
     });
 });
 
-describe('/GET me/cart', () => {
-    it('it should return the users cart', done => {
-        chai    
+describe('/GET brands', () => {
+    it('it should GET all of the brands', done => {
+        chai
             .request(server)
-            .get('/me/cart')
+            .get('/brands')
             .end((err, res) => {
+                console.log(res);
                 res.should.have.status(200);
                 res.body.should.be.an('array');
+                res.body.length.should.be.eql(5);
                 done();
             });
     });
 });
- */
+
+describe('/GET brands/:id/products', () => {
+    it('it should GET a single product', done => {        
+        chai
+            .request(server)            
+            .get('/brands/' + 5 + '/products')            
+            .end((err, res) => {                
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('id');
+                res.body.should.have.property('categoryId');
+                res.body.should.have.property('name');
+                res.body.should.have.property('description');
+                res.body.should.have.property('price');
+                res.body.should.have.property('imageUrls');
+                done();
+            });
+    });
+});
 
 
+
+describe('/POST login', () => {
+    it('it should POST a login', done => {        
+        chai
+            .request(server)
+            .post('/login')
+            .send({ username: 'yellowleopard753', password: 'jonjon' })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('username');
+                res.body.should.have.property('password');                
+                done();
+            });
+    });
+});
+
+
+
+
+/* 
+
+{
+            "gender": "male",
+            "cart":[],
+            "name": {
+                "title": "mr",
+                "first": "eddie",
+                "last": "dean"
+            },
+            "location": {
+                "street": "19 tower street",
+                "city": "new york",
+                "state": "new york",
+                "postcode": 64589
+            },
+            "email": "eddie.dean@example.com",
+            "login": {
+                "username": "redrose456",
+                "password": "susannah19",
+                "salt": "eNuMvema",
+                "md5": "a8be2a69c8c91684588f4e1a29442dd7",
+                "sha1": "f9a60bbf8b550c10712e470d713784c3ba78a68e",
+                "sha256": "4dca9535634c102fbadbe62dc5b37cd608f9f3ced9aacf42a5669e5a312690a0"
+            },
+            "dob": "1972-10-19 10:47:17",
+            "registered": "2003-08-03 01:12:24",
+            "phone": "031-941-6700",
+            "cell": "081-032-7884",
+            "picture": {
+                "large": "https://randomuser.me/api/portraits/women/55.jpg",
+                "medium": "https://randomuser.me/api/portraits/med/women/55.jpg",
+                "thumbnail": "https://randomuser.me/api/portraits/thumb/women/55.jpg"
+            },
+            "nat": "IE"
+        };       , */
