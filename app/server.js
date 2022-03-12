1d78e0f86056000249fc71d6930c3eb97df5b5e1
sbistransin <48774173+sbistransin@users.noreply.github.com>
2022-03-12 17:18:03 -0500

    initial routes, testing, and loaded data

--- app/server.js
+++ app/server.js
@@ -7,7 +7,79 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+let products = [];
+let brands = [];
+let users = [];
+let cart = [];
+let accessToken = 'adi49f20fj20v2ft';
 
-http.createServer(function (request, response) {
+var myRouter = Router();
+myRouter.use(bodyParser.json());
 
-}).listen(PORT);
+let server = http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, error => {
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
+  }
+
+  fs.readFile("/Users/StephB/code/node-js/sunglasses-io/initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} stores loaded`);
+  });
+
+  fs.readFile("/Users/StephB/code/node-js/sunglasses-io/initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+
+  fs.readFile("/Users/StephB/code/node-js/sunglasses-io/initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} users loaded`);
+  });
+});
+
+myRouter.get('/api/products', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(products));
+});
+
+myRouter.get('/api/brands', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(brands));
+});
+
+myRouter.get('/api/brands/:id/products', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify([]));
+});
+
+myRouter.post('/api/login', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify(accessToken));
+});
+
+myRouter.get('/api/me/cart', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify([]));
+});
+
+myRouter.post('/api/me/cart', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify([]));
+});
+
+myRouter.delete('/api/me/cart/:id', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify([]));
+});
+
+myRouter.put('/api/me/cart/:id', function(request,response) {
+	response.writeHead(200, { "Content-Type": "application/json" });
+	return response.end(JSON.stringify([]));
+});
+
+module.exports = server;
---
+++ tests/sunglasses-io-tests.js
@@ -0,0 +1,133 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Sunglasses-io", () => {
+
+  describe("/GET products", () => {
+    it("it should GET all the products", (done) => {
+      chai
+        .request(server)
+        .get("/api/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+  });
+
+  describe("/GET brands", () => {
+    it("it should GET all the brands sold", (done) => {
+      chai
+        .request(server)
+        .get("/api/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+  });
+
+  describe("/GET/brands/:id products", () => {
+    it("it should GET all the brands sold", (done) => {
+      chai
+        .request(server)
+        .get("/api/brands/1/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+  });
+
+  describe("/POST login", () => {
+    it("it should log a user in", (done) => {
+      let loginData = {
+        username : "john",
+        password : "smith"
+      };
+
+      chai
+        .request(server)
+        .post("/api/login")
+        .send(loginData)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("string");
+          res.body.length.should.be.eql(16);
+          done();
+        });
+    });
+  });
+
+  describe("/GET/me/cart", () => {
+    it("it should GET all products in current user's cart", (done) => {
+      chai
+        .request(server)
+        .get("/api/me/cart")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          // must be logged in
+          done();
+        });
+    });
+  });
+
+  describe("/POST/me/cart", () => {
+    it("it should add an item to the user's cart", (done) => {
+      let item = {
+        id: 1,
+        quantity: 5
+      }
+
+      chai
+        .request(server)
+        .post("/api/me/cart")
+        .send(item)
+        .end((err, res) => {
+          res.should.have.status(200);
+          //res.body.should.be.an("object");
+          // must be logged in
+          done();
+        });
+    });
+  });
+
+  describe("/DELETE/me/cart/:productId", () => {
+    it("it should add an item to the user's cart", (done) => {
+
+      chai
+        .request(server)
+        .delete("/api/me/cart/1")
+        .end((err, res) => {
+          res.should.have.status(200);
+          //res.body.should.be.an("object");
+          // must be logged in
+          done();
+        });
+    });
+  });
+
+  describe("/PUT/me/cart/:productId", () => {
+    it("it should update an item to the user's cart", (done) => {
+
+      chai
+        .request(server)
+        .put("/api/me/cart/1")
+        .end((err, res) => {
+          res.should.have.status(200);
+          //res.body.should.be.an("object");
+          // must be logged in
+          done();
+        });
+    });
+  });
+});
