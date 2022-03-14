06c2d96b6d6af5187ae4d0c62b03497a17d8b547
dougcwest <93220603+dougcwest@users.noreply.github.com>
2022-03-14 16:08:33 -0500

    added get products test

---
+++ Tests/products.js
@@ -0,0 +1,29 @@
+let products = require("../initial-data/products.json");
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+
+let should = chai.should();
+
+chai.use(chaiHttp); 
+
+beforeEach(() => {
+  products.removeAll();
+});
+
+describe("products", () => {
+  describe("/GET product", () => {
+    it("it should GET all of the products", (done) => {
+      chai
+        .request(server)
+        .get("/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+});
