324d29b468ee2a8063f7eb75d150062296b0db6a
Michael Grumbine <mgrumbine3@gmail.com>
2022-03-13 22:03:00 -0700

    stubbed out routes

--- app/server.js
+++ app/server.js
@@ -6,10 +6,48 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
-const PORT = 3002;
+const myRouter = Router();
+myRouter.use(bodyParser.json());
+const PORT = 3001;
 
 const server = http.createServer(function (request, response) {
-
+  myRouter(request, response, finalHandler(request, response));
 }).listen(PORT);
+
+myRouter.get('/api/sunglasses', (request, response) => {
+
+});
+
+myRouter.get('/api/sunglasses/:glassId', (request, response) => {
+
+});
+
+myRouter.get('/api/brands', (request, response) => {
+
+});
+
+myRouter.get('/api/brands/:brandId/sunglasses', (request, response) => {
+
+});
+
+myRouter.post('/api/login', (request, response) => {
+
+});
+
+myRouter.get('/api/me/cart', (request, response) => {
+
+});
+
+myRouter.post('/api/me/cart/:glassId/add', (request, response) => {
+
+});
+
+myRouter.delete('/api/me/cart/:glassId/remove', (request, response) => {
+  
+});
+
+myRouter.post('/api/me/cart/:glassId/update', (request, response) => {
+  
+});
 
 module.exports = server;
--- tests/server-test.js
+++ tests/server-test.js
@@ -4,3 +4,4 @@ const chaiHttp = require('chai-http');
 
 const should = chai.should();
 chai.use(chaiHttp);
+
