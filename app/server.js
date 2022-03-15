d7cbab69301e5a0158006abeb5e8b2884d2dc619
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-03-15 17:53:13 -0400

    created router.get for getting all the brands

--- app/server.js
+++ app/server.js
@@ -6,12 +6,20 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
+const Brands = require('../initial-data/brands.json')
+
 const PORT = 3001;
 
 const router = Router();
 router.use(bodyParser.json());
 
-http.createServer(function (request, response) {
-
+let server = http.createServer(function (request, response) {
+  router(request, response, finalHandler(request, response))
 }).listen(PORT);
 
+router.get("/sunglasses{brands}", (req, res) => {
+  res.writehead(200);
+  return res.end(JSON.stringify(Brands.getAll()))
+})
+
+module.exports = server;
--- testing/server-test.js
+++ testing/server-test.js
@@ -1,3 +1,19 @@
-let {expect} = require('chai');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
 const server = require('../app/server');
 
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("it should GET all the brands", (done) => {
+      chai
+        .request(server)
+        .get("/book")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+});
