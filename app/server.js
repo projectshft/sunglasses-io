0c3589a6dae7e99dc0aa9bb19834eb60fdc72217
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-05-22 16:02:54 -0500

    tested get products - failed

--- app/server.js
+++ app/server.js
@@ -3,11 +3,28 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
+
+let brands = require('../initial-data/brands.json');
+let products = require('../initial-data/products.json');
+let users = require('../initial-data/users.json');
+
+var myRouter = Router();
+myRouter.use(bodyParser.json())
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
-
+let server = http.createServer(function (request, response) {
+  myRouter(request, response), finalHandler(request, response)
 }).listen(PORT);
+
+myRouter.get('/products', (request, response) => {
+  if(products.length == 0) {
+    response.writeHead(404, 'No products found')
+    return response.end();
+  } else {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(products));
+  }
+})
--- sunglasses-test.js
+++ sunglasses-test.js
@@ -7,3 +7,21 @@ let chaiHttp = require('chai-http');
 let server = require('./app/server');
 
 let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Products', () => {
+  describe('/GET products', () => {
+    it('should get all products', (done) => {
+      chai
+        .request(server)
+        .get('/api/products')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(11)
+          done();
+        })
+    })
+  })
+})
