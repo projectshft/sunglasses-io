547aaa5c4b453c4c98a130982b820d0ff7a0a410
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2022-05-22 20:14:25 -0500

    tested product and brand endpoints - passed

--- app/server.js
+++ app/server.js
@@ -6,25 +6,52 @@ var Router = require('router');
 var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
 
-let brands = require('../initial-data/brands.json');
-let products = require('../initial-data/products.json');
-let users = require('../initial-data/users.json');
+let brands = [];
+let products = [];
+let users = [];
 
 var myRouter = Router();
 myRouter.use(bodyParser.json())
 
 const PORT = 3001;
 
-let server = http.createServer(function (request, response) {
-  myRouter(request, response), finalHandler(request, response)
-}).listen(PORT);
-
-myRouter.get('/products', (request, response) => {
-  if(products.length == 0) {
-    response.writeHead(404, 'No products found')
-    return response.end();
-  } else {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    return response.end(JSON.stringify(products));
+const server = http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response));
+}).listen(PORT, error => {
+  
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
   }
+
+  fs.readFile('initial-data/brands.json', 'utf8', (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+  
+  fs.readFile('initial-data/users.json', 'utf8', (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+
+  fs.readFile('initial-data/products.json', 'utf8', (err, data) => {
+    if (error) throw error;
+    products = JSON.parse(data)
+    console.log(`Server setup: ${products.length} products loaded`)
+  })
+
+  console.log(`Server is listening on ${PORT}`);
+});;
+
+myRouter.get('/api/products', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  return response.end(JSON.stringify(products));
+});
+
+myRouter.get('/api/brands', (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  return response.end(JSON.stringify(brands));
 })
+
+module.exports = server;
--- sunglasses-test.js
+++ sunglasses-test.js
@@ -1,25 +1,36 @@
-let Brands = require('./initial-data/brands.json');
-let Products = require('./initial-data/products.json');
-let Users = require('./initial-data/users.json');
-
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('./app/server');
 
-let should = chai.should();
-
 chai.use(chaiHttp);
+let should = chai.should();
 
 describe('Products', () => {
   describe('/GET products', () => {
-    it('should get all products', (done) => {
+    it('should GET all the products', (done) => {
       chai
         .request(server)
         .get('/api/products')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an('array');
-          res.body.length.should.be.eql(11)
+          res.body.length.should.be.eql(11);
+          done();
+        })
+    })
+  })
+})
+
+describe('Brands', () => {
+  describe('/GET brands', () => {
+    it('should GET all the brands', (done) => {
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(5);
           done();
         })
     })
