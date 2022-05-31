77fc15ccbbf33d660d31e6559efbe8d1015fdc06
Emily Freeman <freeman.ecf@gmail.com>
2022-05-30 21:48:55 -0400

    tests and functionality for brands and products

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var finalHandler = require('finalhandler');
+const url = require("url");
 var queryString = require('querystring');
 var Router = require('router');
 var bodyParser   = require('body-parser');
@@ -15,16 +16,71 @@ let brands = [];
 let products = [];
 let users = [];
 
-http.createServer(function (request, response) {
+
+let server = http.createServer(function (request, response) {
   response.writeHead(200);
   router(request, response, finalHandler(request, response));
 }).listen(PORT, err => {
   if (err) throw err;
-  console.log(`server running on port ${PORT}`);
 
-  brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf-8'));
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json", "utf-8"));
   products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf-8'));
   users = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf-8'));
+
+  console.log(`server running on port ${PORT}`);
 });
 
+router.get("/api/brands", (request, response) => {
+  const parsedUrl = url.parse(request.originalUrl);
+  const { query } = queryString.parse(parsedUrl.query);
+  
+  let brandToReturn = [];
+  if (query !== undefined) {
+    brandToReturn = brands.filter(brand => brand.name.includes(query))
+    // if (!brandToReturn) {
+    //   response.writeHead(404, "No brand with that name found");
+    //   return response.end();
+    // }
+  }
+  else {
+    brandToReturn = brands;
+  }
 
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(brandToReturn));
+})
+
+router.get("/api/brands/:id/products", (request, response) => {
+  const { id } = request.params;
+  const brand = brands.find(brand => brand.id === id);
+  if (brand) {
+    let brandsProducts = products.filter(product => product.categoryId === brand.id);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(brandsProducts));
+  }
+  else {
+    response.writeHead(404, "No brand found");
+    return response.end();
+  }
+});
+
+router.get("/api/products", (request, response) => {
+  const parsedUrl = url.parse(request.originalUrl);
+  const { query } = queryString.parse(parsedUrl.query);
+
+  let productsToReturn = []
+  if (query !== undefined) {
+    productsToReturn = products.filter(product => product.name.includes(query) || product.description.includes(query))
+  }
+  else {
+    productsToReturn = products
+  }
+ 
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(productsToReturn))
+})
+
+router.post("/api/login", (request, response) => {
+  
+})
+// module.exports = server;
--- test/sunglassesio-test.js
+++ test/sunglassesio-test.js
@@ -4,13 +4,15 @@ const server = require("../app/server");
 const expect = chai.expect;
 const assert = chai.assert;
 
+let should = chai.should();
+
 chai.use(chaiHTTP);
 
 describe('/GET brands', () => {
   it('should GET all brands', done => {
     chai
       .request(server)
-      .get('api/brands')
+      .get("/api/brands")
       .end((err, res) => {
         assert.isNotNull(res.body);
         expect(err).to.be.null;
@@ -19,6 +21,119 @@ describe('/GET brands', () => {
         expect(res.body).to.be.an('array');
         expect(res.body).to.have.lengthOf(5);
         done();
-      })
-  })
-})
+      });
+  });
+  it("should limit results to those with a query string", done => {
+    chai
+      .request(server)
+      .get("/api/brands?query=DKNY")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(1);
+        done();
+      });
+  });
+  it("returns all goals if query is missing", done => {
+    chai
+      .request(server)
+      .get("/api/brands?query=")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(5);
+        done();
+      });
+  });
+  // it("should return error if query does not match data", done => {
+  //   chai
+  //     .request(server)
+  //     .get("/api/brands?query=sfdksl")
+  //     .end((err, res) => {
+  //       expect(res).to.have.status(404);;
+  //       done();
+  //     })
+  // })
+});
+
+describe("/GET products of a brand", () => {
+  it("should GET the products of a brand", done => {
+    chai
+      .request(server)
+      .get("/api/brands/2/products")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(2);
+        done();
+      });
+  });
+  it("should return error if id does not exist", done => {
+    chai
+      .request(server)
+      .get("/api/brands/12/products")
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+  });
+});
+
+describe("/GET products", () => {
+  it("should GET all available products", done => {
+    chai
+      .request(server)
+      .get("/api/products")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(11);
+        done();
+      });
+  });
+  it("should limit results to those with a query string", done => {
+    chai
+      .request(server)
+      .get("/api/products?query=Brown")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(1);
+        done();
+      });
+  });
+  it("returns all goals if query is missing", done => {
+    chai
+      .request(server)
+      .get("/api/products?query=")
+      .end((err, res) => {
+        assert.isNotNull(res.body);
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect("Content-Type", "application/json");
+        expect(res.body).to.be.an("array");
+        expect(res.body).to.have.lengthOf(11);
+        done();
+      });
+  });
+});
+
+// describe("/POST login", () => {
+//   it("")
+// })
+
