1d4e0ab5ba7e6775c732955c0f58e5cd195dfc31
Alex Rodgers <skyewulff@outlook.com>
2022-06-01 22:24:56 -0400

    add initial tests for GET route for all products

---
+++ tests/products.js
@@ -0,0 +1,21 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Products", () => {
+    describe("/GET products", () => {
+        it("it should GET all of the products", (done) => {
+            chai.request(server)
+            .get("products")
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an("array");
+                res.body.should.have.lengthOf(11);
+            });
+        });
+    });
+});
