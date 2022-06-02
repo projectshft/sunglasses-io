2cc73de923727a6a6661192f79606637bf80e0b0
Alex Rodgers <skyewulff@outlook.com>
2022-06-01 22:32:43 -0400

    set up initial server without any routes

--- app/server.js
+++ app/server.js
@@ -7,7 +7,9 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+const router = require('router');
+router.use(bodyParser.json());
 
-http.createServer(function (request, response) {
-
+let server = http.createServer(function (request, response) {
+    router(request, response, finalHandler(request, response));
 }).listen(PORT);
