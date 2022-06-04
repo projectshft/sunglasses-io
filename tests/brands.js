1ebefaacea231587fc97db7f7bc54e7eaea27306
Alex Rodgers <skyewulff@outlook.com>
2022-06-04 16:42:46 -0400

    add tests for GET route for all brands

---
+++ tests/brands.js
@@ -0,0 +1,26 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Brands", () => {
+    describe("/GET brands", () => {
+        it("it should GET all of the brands", (done) => {
+            chai
+                .request(server)
+                .get("brands")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("array");
+                    res.body.should.have.lengthOf(5);
+                    res.body[0].should.be.an("object");
+                    res.body[0].should.have.property("id");
+                    res.body[0].should.have.property("name");
+                    res.body[0].name.should.be("Oakley");
+                });
+        });
+    });
+});
