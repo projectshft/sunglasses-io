2224cf50cd2d6ffda51556309c3883e7aee05cad
Alex Rodgers <skyewulff@outlook.com>
2022-06-04 22:14:09 -0400

    create tests for login route

---
+++ tests/login.js
@@ -0,0 +1,25 @@
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Login", () => {
+    describe("/POST login", () => {
+        it("it should create an access token for a valid user", (done) => {
+            chai
+                .request(server)
+                .get("/login")
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an("object");
+                    res.body.should.have.property("username");
+                    res.body.should.have.property("lastUpdated");
+                    res.body.should.have.property("token");
+                    done();
+                });
+        });
+    });
+});
