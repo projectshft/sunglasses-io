590f3342d0fa6fd0e342cc7465e2596deb2d7697
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-08 17:07:02 -0400

    I successfully tested for the 200 status

--- app/server.js
+++ app/server.js
@@ -1,10 +1,11 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser   = require('body-parser');
+const uid = require('rand-token').uid;
+
 
 const PORT = 3001;
 const myRouter = Router();
@@ -13,5 +14,12 @@ myRouter.use(bodyParser.json());
 let server = http.createServer( (request, response) => {
   myRouter(request, response, finalHandler(request, response))
 }).listen(PORT);
+
+//LOGIN
+
+myRouter.get('/sunglasses', (req, res) => {
+  res.writeHead(200)
+  res.end()
+})
 
 module.exports = server;
--- server-test.js
+++ server-test.js
@@ -4,3 +4,16 @@ const server = require('./app/server');
 
 let should = chai.should();
 chai.use(chaiHttp);
+
+describe('Sunglasses', () => {
+  describe('/GET search in all sunglasses', () => {
+    it('should return all sunglasses by default', (done) => {
+      chai.request(server)
+      .get('/sunglasses')
+      .end((err, res) => {
+        res.should.have.status(200)
+        done()
+      })
+    })
+  })
+})
