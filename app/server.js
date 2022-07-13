56b75961e51776c1f40072ece2cb9ec597cae102
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-07-13 11:50:57 -0400

    refactered POST to /cart to use arrayEquals() instead of prototype method

--- app/server.js
+++ app/server.js
@@ -77,30 +77,6 @@ myRouter.get('/sunglasses/brands/:brand', (req, res) => {
   }
 })
 
-Array.prototype.equals = function (array) {
-  // if the other array is a falsy value, return
-  if (!array)
-      return false;
-
-  // compare lengths - can save a lot of time 
-  if (this.length != array.length)
-      return false;
-
-  for (var i = 0, l=this.length; i < l; i++) {
-      // Check if we have nested arrays
-      if (this[i] instanceof Array && array[i] instanceof Array) {
-          // recurse into the nested arrays
-          if (!this[i].equals(array[i]))
-              return false;       
-      }           
-      else if (this[i] != array[i]) { 
-          // Warning - two different object instances will never be equal: {x:20} != {x:20}
-          return false;   
-      }           
-  }       
-  return true;
-}
-
 //CART
 //POST /cart
 myRouter.post('/cart', (req, res) => {
@@ -115,9 +91,7 @@ myRouter.post('/cart', (req, res) => {
     listToCheck.push(prop)
   }
 
-  console.log(arrayEquals(canonList, listToCheck))
-
-  if (!canonList.equals(listToCheck)) {
+  if (!arrayEquals(canonList, listToCheck)) {
     const message = `In order to post, product must include the properties 'id', 'categoryId', 'name', 'description', 'price', and 'imageUrls'. 
     1) Make sure that the posted item contains each of these properties. 
     2) Make sure that the spelling is accurate for each property.
