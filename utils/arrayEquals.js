19fa5d316aff5596875cac519e8735c15d598d58
Joshua Clarke Cushing <joshuacushing@Joshuas-MacBook-Air.local>
2022-07-13 11:47:42 -0400

    the spellchecker function on arrayEquals.js works

--- utils/arrayEquals.js
+++ utils/arrayEquals.js
@@ -31,7 +31,19 @@ const arrayEquals = (arr1, arr2) => {
     return false;
   }
 
-  return true;
+  let tester = true;
+
+  arr1.map((post, i) => {
+    if (post != arr2[i]) {
+      tester = false;
+    }
+  })
+
+  if (tester) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 module.exports = arrayEquals;
