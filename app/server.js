0a5b617e0375c0eecf63bf1194ecf64c77495495
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2022-07-14 10:59:42 -0400

    able to access url through req.url

--- app/server.js
+++ app/server.js
@@ -132,7 +132,7 @@ myRouter.post('/login', (req, res) => {
 })
 
 myRouter.get('/sunglasses', (req, res) => {
-  
+  console.log(req.url)
   res.writeHead(200, { "Content-Type": "application/json" })
   res.end(JSON.stringify(products))
 })
