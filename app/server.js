6f8fc3a343f7565b8b1738605dafbdc86158415e
drkstn <86159096+drkstn@users.noreply.github.com>
2022-08-15 12:51:02 -0700

    Initial commit w/ setup for server, router, and data

--- app/server.js
+++ app/server.js
@@ -6,8 +6,38 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
-const PORT = 3001;
+// State-holding variables
+let brands = [];
+let products = [];
+let users = [];
 
-http.createServer(function (request, response) {
+const PORT = 3111;
 
-}).listen(PORT);
+// Router setup
+const myRouter = Router();
+myRouter.use(bodyParser.json());
+
+// Server setup
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  myRouter(req, res, finalHandler(req, res));
+});
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  console.log(`Server running on port ${PORT}`);
+  // Populate brands  
+  brands = JSON.parse(fs.readFileSync("initial-data/brands.json","utf-8"));
+  console.log(`Brands: ${brands.length} loaded`)
+
+  // Populate products
+  products = JSON.parse(fs.readFileSync("initial-data/products.json","utf-8"));
+  console.log(`Products: ${products.length} loaded`)
+
+  // Populate users
+  users = JSON.parse(fs.readFileSync("initial-data/users.json","utf-8"));
+  console.log(`Users: ${users.length} loaded`)
+
+  // Hardcode "logged in" user
+  user = users[0];
+});
