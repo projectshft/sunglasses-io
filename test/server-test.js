ace3be1c59bf5502c2b0dddc95d13cbfc2219923
drkstn <86159096+drkstn@users.noreply.github.com>
2022-08-16 13:55:31 -0700

    Fix formatting in server-test.js

--- test/server-test.js
+++ test/server-test.js
@@ -1,49 +1,49 @@
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../app/server');
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
 
 let should = chai.should();
 
 chai.use(chaiHttp);
 
-describe('GET /api/products', () => {
-  it('should GET all products', done => {
+describe("GET /api/products", () => {
+  it("should GET all products", (done) => {
     chai
       .request(server)
-      .get('/api/products')
+      .get("/api/products")
       .end((err, res) => {
         res.should.have.status(200);
-        res.body.should.be.an('array');
+        res.body.should.be.an("array");
         res.body.length.should.eql(11);
         done();
       });
   });
 });
 
-describe('GET /api/brands', () => {
-  it('should GET all brands', done => {
+describe("GET /api/brands", () => {
+  it("should GET all brands", (done) => {
     chai
       .request(server)
-      .get('/api/brands')
+      .get("/api/brands")
       .end((err, res) => {
         res.should.have.status(200);
-        res.body.should.be.an('array');
+        res.body.should.be.an("array");
         res.body.length.should.eql(5);
         done();
       });
   });
 });
 
-describe('GET /api/brands/:id/products', () => {
-  it('should GET all products of given brand', done => {        
-      chai
-          .request(server)            
-          .get('/api/brands/1/products')            
-          .end((err, res) => {                            
-              res.should.have.status(200);
-              res.body.should.be.an('array');
-              res.body.length.should.eql(3);
-              done();
-          });
+describe("GET /api/brands/:id/products", () => {
+  it("should GET all products of given brand", (done) => {
+    chai
+      .request(server)
+      .get("/api/brands/1/products")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an("array");
+        res.body.length.should.eql(3);
+        done();
+      });
   });
 });
