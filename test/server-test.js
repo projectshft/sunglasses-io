1f69d14e19f18eb73ad2f2e7b69cc02500a2bee5
drkstn <86159096+drkstn@users.noreply.github.com>
2022-08-16 14:08:57 -0700

    Add and fail 'POST /api/login' test

--- test/server-test.js
+++ test/server-test.js
@@ -47,3 +47,24 @@ describe("GET /api/brands/:id/products", () => {
       });
   });
 });
+
+describe("POST /api/login", () => {
+  it("should POST user credentials", (done) => {
+    const userCredentials = {
+      "username": "greenlion235",
+      "password": "waters"
+    }
+    chai
+      .request(server)
+      .post("/api/login")
+      .send(userCredentials)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a('object');
+        res.body.should.have.property('username');
+        res.body.should.have.property('lastUpdated');
+        res.body.should.have.property('token');
+        done();
+      });
+  });
+});
