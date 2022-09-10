b4aac4eef83aec147e26722145ba544041a2e577
nathanjpak <nathan.j.pak@gmail.com>
2022-09-09 20:23:36 -0700

    start tests for GET /products route

--- test/server.js
+++ test/server.js
@@ -54,3 +54,71 @@ describe("Brands", () => {
     });
   });
 });
+
+describe("Products", () => {
+  describe("/GET products", () => {
+    it("it should GET all of the products with a relevant name", (done) => {
+      // arrange
+      let search = "sunglasses";
+      // act
+      chai
+        .request(server)
+        .get("/products")
+        .query(search)
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(2);
+          done();
+        });
+    });
+
+    it("it should GET all of the products with a relevant description", (done) => {
+      // arrange
+      let search = "AWFUL";
+      // act
+      chai
+        .request(server)
+        .get("/products")
+        .query(search)
+        // assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eql(1);
+          done();
+        });
+    });
+
+    it("it should return a 400 if the query is less than 3 letters", (done) => {
+      // arrange
+      let search = "in";
+      // act
+      chai
+        .request(server)
+        .get("/products")
+        .query(search)
+        // assert
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+    });
+
+    it("it should return a 404 if no relevant products are found", (done) => {
+      // arrange
+      let search = "California";
+      // act
+      chai
+        .request(server)
+        .get("/products")
+        .query(search)
+        // assert
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        });
+    });
+  });
+});
