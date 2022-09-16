12cb1e2041574ffd59df938797f0c0d558cd30f0
Dillon Hess <dhess268@gmail.com>
2022-09-15 22:27:52 -0400

    redundency removal

--- app/server.js
+++ app/server.js
@@ -1,12 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const finalHandler = require('finalhandler');
-const queryString = require('querystring');
 const Router = require('router');
 const bodyParser = require('body-parser');
 const { uid } = require('rand-token');
 const urlParser = require('url');
-const { parse } = require('path');
 
 const PORT = 3001;
 
