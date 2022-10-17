6ebabd012a7503d51c27ad811c764015df14bbae
Natalia Kirejeva <nataliaki@icloud.com>
2022-10-17 13:30:12 +0300

    refactoring made and issues with post-product-in-cart to be resolved

--- app/server.js
+++ app/server.js
@@ -1,46 +1,46 @@
 const http = require('http');
 const fs = require('fs');
 const finalHandler = require('finalhandler');
-const queryString = require('querystring');
 const Router = require('router');
-const bodyParser  = require('body-parser');
-const uid = require('rand-token').uid;
+const bodyParser = require('body-parser');
+const { uid } = require('rand-token');
 const url = require('url');
 
 const PORT = 3001;
 let brands = [];
 let products = [];
 let users = [];
-let accessTokens = [];
+const accessTokens = [];
 
-const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000;
 
 const router = Router();
 router.use(bodyParser.json());
 
-const server = http.createServer(function (req, res) {
+const server = http.createServer((req, res) => {
   res.writeHead(200);
-  router(req, res, finalHandler(req, res))
-})
+  router(req, res, finalHandler(req, res));
+});
 
-server.listen(PORT, err => {
+server.listen(PORT, (err) => {
   if (err) throw err;
   console.log(`server running on port ${PORT}`);
-  products = JSON.parse(fs.readFileSync("initial-data/products.json","utf-8"))
-  brands = JSON.parse(fs.readFileSync("initial-data/brands.json","utf-8"));
-  users = JSON.parse(fs.readFileSync("initial-data/users.json","utf-8"));
-})
-//GET all brands
-router.get("/app/brands", (request, response) => {
+  products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf-8'));
+  brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf-8'));
+  users = JSON.parse(fs.readFileSync('initial-data/users.json', 'utf-8'));
+});
+
+// GET all brands
+router.get('/app/brands', (request, response) => {
   if (!brands) {
-    response.writeHead(404, "That brands do not exist");
+    response.writeHead(404, 'That brands do not exist');
     return response.end();
   }
-  response.writeHead(200, { "Content-Type": "application/json" });
+  response.writeHead(200, { 'Content-Type': 'application/json' });
   return response.end(JSON.stringify(brands));
 });
-//GET all products
-router.get("/app/products", (request, response) => {
+// GET all products
+router.get('/app/products', (request, response) => {
   // const parsedUrl = url.parse(request.originalUrl);
   // const { query } = querystring.parse(parsedUrl.query);
   // let productsToReturn = [];
@@ -59,178 +59,169 @@ router.get("/app/products", (request, response) => {
   // response.writeHead(200, { "Content-Type": "application/json" });
   // return response.end(JSON.stringify(productsToReturn));
   if (!products) {
-    response.writeHead(404, "No products found");
+    response.writeHead(404, 'No products found');
     return response.end();
   }
-  response.writeHead(200, { "Content-Type": "application/json" });
+  response.writeHead(200, { 'Content-Type': 'application/json' });
   return response.end(JSON.stringify(products));
 });
 
-//GET products of specified brand
+// GET products of specified brand
 router.get('/app/brands/:id/products', (request, response) => {
   const { id } = request.params;
-  const brand = brands.find(brand => brand.id == id);
+  const brand = brands.find((brand) => brand.id == id);
   if (!brand) {
-    response.writeHead(404, "No products of this brand found");
+    response.writeHead(404, 'No products of this brand found');
     return response.end();
   }
-  response.writeHead(200, { "Content-Type": "application/json" });
+  response.writeHead(200, { 'Content-Type': 'application/json' });
   const relatedProducts = products.filter(
-    product => product.categoryId == id
+    (product) => product.categoryId == id
   );
   return response.end(JSON.stringify(relatedProducts));
+});
 
-})
-
-//POST login user
-router.post('/app/login', (request,response)=>{
+// POST login user
+router.post('/app/login', (request, response) => {
   if (request.body.username && request.body.password) {
-    let user = users.find((user) => {
-      return user.login.username == request.body.username && user.login.password == request.body.password;
-    });
+    const user = users.find(
+      (user) =>
+        user.login.username == request.body.username &&
+        user.login.password == request.body.password
+    );
     if (user) {
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      let currentAccessToken = accessTokens.find((tokenObject) => {
-        return tokenObject.username == user.login.username;
-      });
+      response.writeHead(200, { 'Content-Type': 'application/json' });
+      const currentToken = accessTokens.find(
+        (tokenObject) => tokenObject.username == user.login.username
+      );
 
-      if (currentAccessToken) {
-        currentAccessToken.lastUpdated = new Date();
-        return response.end(JSON.stringify(currentAccessToken.token));
-      } else {
-        let newAccessToken = {
-          username: user.login.username,
-          lastUpdated: new Date(),
-          token: uid(16)
-        }
-        accessTokens.push(newAccessToken);
-        return response.end(JSON.stringify(newAccessToken.token));
+      if (currentToken) {
+        currentToken.lastUpdated = new Date();
+        return response.end(JSON.stringify(currentToken.token));
       }
-    } else {
-      response.writeHead(401, "Invalid username or password");
-      return response.end();
+      const newAccessToken = {
+        username: user.login.username,
+        lastUpdated: new Date(),
+        token: uid(16),
+      };
+      accessTokens.push(newAccessToken);
+      return response.end(JSON.stringify(newAccessToken.token));
     }
-  } else {
-    response.writeHead(400, "Incorrectly formatted response");
+    response.writeHead(401, 'Invalid username or password');
     return response.end();
   }
+  response.writeHead(400, 'Incorrectly formatted response');
+  return response.end();
 });
 
-//helper func
-var getValidTokenFromRequest = function(request) {
-  var parsedUrl = url.parse(request.url, true);
-  if (parsedUrl.query.accessToken) {
-    let currentAccessToken = accessTokens.find((accessToken) => {
-      return accessToken.token == parsedUrl.query.accessToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
-    });
-
-    if (currentAccessToken) {
-      return currentAccessToken;
-    } else {
-      return null;
-    }
-  } else {
-    return null;
-  }
-};
-
-//GET cart of logged in user
-router.get('/app/me/cart', function(request, response){
-  const currentAccessToken = getValidTokenFromRequest(request)
-  if(currentAccessToken){
-      const currentUser = users.find(
-        (user) => user.login.username === currentAccessToken.username
-      );
-    response.writeHead(200, {'Content-Type':'application/json'})
-    return response.end(JSON.stringify(currentUser.cart))
-  } else {
-    response.writeHead(401, "You need to log in to see this page")
-    return response.end()
-  }
-})
-
-//POST product to cart of logged in user
-router.post("/app/me/cart", (request, response) => {
-  const currentAccessToken = getValidTokenFromRequest(request)
-  if(currentAccessToken){
-    const { productId } = request.body
-    const currentUser = users.find(
-      (user) => user.login.username === currentAccessToken.username
-    );
-    const productToAdd = products.find(
-      (product) => product.id === productId
-    );
-    if (!productId){
-      response.writeHead(404, "No products with this id found");
-      return response.end();
-    }
-    const checkIfProductInCart=currentUser.cart.find(product=>product.id===productId)
-    if(!checkIfProductInCart){
-        currentUser.cart.push({
-        product: productToAdd,
-        quantity: 1,
-        id: productToAdd.id
-      })
-    }
-    else {
-      const index=currentUser.cart.indexOf(checkIfProductInCart)
-      currentUser.cart[index].quantity++
-    }
-
-    response.writeHead(200, { "Content-Type": "application/json" });
+// GET cart of logged in user
+router.get('/app/me/cart', (request, response) => {
+  const currentToken = getToken(request);
+  if (currentToken) {
+    const currentUser = getUser(currentToken);
+    response.writeHead(200, { 'Content-Type': 'application/json' });
     return response.end(JSON.stringify(currentUser.cart));
   }
-  else {
-    response.writeHead(401, "You need to log in to see this page")
-    return response.end()
-  }
+  response.writeHead(401, 'You need to log in to see this page');
+  return response.end();
 });
 
-//DELETE product (based on its id placed in path) from cart of logged in user
-router.delete("/app/me/cart/:productId", (request, response) => {
-  const currentAccessToken = getValidTokenFromRequest(request)
-  if(currentAccessToken){
+router.post('/app/me/cart', (request, response) => {
+  const currentToken = getToken(request);
+  if (currentToken) {
+    const { productId } = request.body;
+    const currentUser = getUser(currentToken);
+    const productToAdd = getProductFromStorage(productId);
+    if (!productToAdd) {
+      response.writeHead(404, 'No products with this id found');
+      return response.end();
+    }
+    const checkIfProductInCart = getProductFromCart(productId)
+    if (!checkIfProductInCart) {
+      currentUser.cart.push({ ...productToAdd, quantity: 1 });
+    } else {
+      const index = currentUser.cart.indexOf(checkIfProductInCart);
+      currentUser.cart[index].quantity++;
+    }
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    return response.end(JSON.stringify(currentUser.cart));
+  }
+
+  response.writeHead(401, 'You need to log in to see this page');
+  return response.end();
+});
+
+// DELETE product (based on its id placed in path) from cart of logged in user
+router.delete('/app/me/cart/:productId', (request, response) => {
+  const currentToken = getToken(request);
+  if (currentToken) {
     const { productId } = request.params;
-    const currentUser = users.find(
-      (user) => user.login.username === currentAccessToken.username
-    );
-    const productToDelete = currentUser.cart.find(product => product.id === productId)
+    const currentUser = getUser(currentToken);
+    const productToDelete = getProductFromCart(productId);
     if (!productToDelete) {
       response.writeHead(404, "That product doesn't exist");
       return response.end();
     }
-    const cartToReturn=currentUser.cart.filter(product=>product.id !== productToDelete.id)
-    response.writeHead(200, { "Content-Type": "application/json" });
+    const cartToReturn = currentUser.cart.filter(
+      (product) => product.id != productToDelete.id
+    );
+    response.writeHead(200, { 'Content-Type': 'application/json' });
     return response.end(JSON.stringify(cartToReturn));
   }
-  else {
-    response.writeHead(401, "You need to log in to see this page")
-    return response.end()
-  }
+
+  response.writeHead(401, 'You need to log in to see this page');
+  return response.end();
 });
 
-//POST change quantity of product (based on its id placed in path) from cart of logged in user
-router.post("/app/me/cart/:productId", (request, response) => {
-  const currentAccessToken = getValidTokenFromRequest(request)
-  if(currentAccessToken){
+// POST change quantity of product (based on its id placed in path) from cart of logged in user
+router.post('/app/me/cart/:productId', (request, response) => {
+  const currentToken = getToken(request);
+  if (currentToken) {
     const { productId } = request.params;
-    const currentUser = users.find(
-      (user) => user.login.username === currentAccessToken.username
-    );
-    const productToUpdate = currentUser.cart.find(product => product.id === productId)
+    const currentUser = getUser(currentToken);
+    const productToUpdate = getProductFromCart(productId);
     if (!productToUpdate) {
       response.writeHead(404, "That product doesn't exist for being updated");
       return response.end();
     }
-    productToUpdate.quantity = request.body.quantity;
-    const cartToReturn=currentUser.cart
-    response.writeHead(200, { "Content-Type": "application/json" });
+    productToUpdate.quantity = Number(request.body.quantity);
+    const cartToReturn = currentUser.cart;
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
     return response.end(JSON.stringify(cartToReturn));
   }
-  else {
-    response.writeHead(401, "You need to log in to see this page")
-    return response.end()
-  }
+
+  response.writeHead(401, 'You need to log in to see this page');
+  return response.end();
 });
 
-module.exports = server
+// helper functions 
+const getToken = function (request) {
+  const parsedUrl = url.parse(request.url, true);
+  if (parsedUrl.query.accessToken) {
+    const currentToken = accessTokens.find(
+      (accessToken) =>
+        accessToken.token == parsedUrl.query.accessToken &&
+        new Date() - accessToken.lastUpdated < TOKEN_VALIDITY_TIMEOUT
+    );
+
+    if (currentToken) {
+      return currentToken;
+    }
+    return null;
+  }
+  return null;
+};
+
+const getProductFromStorage=(productId) =>
+products.find((product) => product.id == productId)
+
+const getProductFromCart = (productId) =>
+  currentUser.cart.find((product) => product.id == productId);
+
+const getUser = (currentToken) =>
+  (currentUser = users.find(
+    (user) => user.login.username == currentToken.username
+  ));
+
+module.exports = server;
--- sunglasses-test.js
+++ sunglasses-test.js
@@ -7,16 +7,14 @@ chai.use(chaiHttp);
 
 
 describe("Brands", () => {
-  describe("/GET app/brands", () => {
+  describe("GET app/brands", () => {
     it("should GET all the brands", (done) => {
-      let brandsLength=0
       chai
       .request(server)
       .get("/app/brands")
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.an("array");
-        brandsLength=res.body.length
         res.body.forEach((brand) => {
           brand.should.have.property('id');
           brand.should.have.property('name');
@@ -26,7 +24,7 @@ describe("Brands", () => {
     });
   })
 
-  describe("/GET app/brands/:id/products", () => {
+  describe("GET app/brands/:id/products", () => {
     it("should GET products array of a specified brand", (done)=> {
       chai
       .request(server)
@@ -59,16 +57,15 @@ describe("Brands", () => {
 
 
 describe("Products", () => {
-  describe("/GET app/products", () => {
+  describe("GET app/products", () => {
     it("should GET all the products", (done) => {
-      // let productsLength=0
       chai
         .request(server)
         .get("/app/products")
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          // productsLength=res.body.length
+          res.body.length.should.be.eql(11)
           res.body.forEach((product) => {
             product.should.have.property('id');
             product.should.have.property('categoryId');
@@ -86,7 +83,7 @@ describe("Products", () => {
 
 describe ("Login", () => {
   let token=""
-  describe("/POST login", () => {
+  describe("POST login", () => {
     it('should return a token if valid username and password', (done) => {
       const validUserData={
         username: "lazywolf342",
@@ -129,7 +126,12 @@ describe ("Login", () => {
     })
   })
   
-  describe ("Cart", ()=>{
+  describe ("Cart", ()=>{      
+    let productIdNotValid='123'      
+    let productId='5'
+    let idForDelete='2'
+    let productIdForChangeQuant='1'
+    
     describe("GET app/me/cart", ()=>{
       it('should return an array of products of logged in user', (done) => {
         chai
@@ -138,9 +140,11 @@ describe ("Login", () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a("array");
+          res.body.length.should.be.eql(2)
           done();
         })
       })
+
       it('should return an error 401 if the user is not logged in', (done)=>{
         chai
         .request(server)
@@ -150,25 +154,26 @@ describe ("Login", () => {
           done();
         })
       })
+
     })
 
     describe("POST app/me/cart", ()=>{
-      
       it('should return an error 401 if the user is not logged in', (done) => {
         chai
         .request(server)
         .post('/app/me/cart')
+        .send(productId)
         .end((err, res) => {
           res.should.have.status(401);
           done();
         })
       })
-      
-      it('should return an error 404 if no product id was found', (done) => {
+
+      it('should return error 404 if no products with this id were found', (done) => {
         chai
         .request(server)
         .post(`/app/me/cart?accessToken=${token}`)
-        .send(1231)
+        .send(productIdNotValid)
         .end((err, res) => {
           res.should.have.status(404);
           done();
@@ -176,59 +181,101 @@ describe ("Login", () => {
       })
       
       it('should post a product to a cart', (done) => {
-        const productId=products[0].id
+        chai
+        .request(server)
+        .post(`/app/me/cart?accessToken=${token}`)
+        .send('5')
+        .end((err, res) => {
+          res.should.have.status(200);
+          // res.body.should.not.be.eql([])
+          done();
+        })
+      })
+
+      it('should increase quantity of the product if it is already in cart', (done) => {
         chai
         .request(server)
         .post(`/app/me/cart?accessToken=${token}`)
         .send(productId)
-        .end((error, response) => {
-          response.should.have.status(200);
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.not.be.eql([])
           done();
         })
       })
-      // it('should increase the quantity of the product if the one adding is already  in cart', (done) => {
-      //   const currentUser.cart=[products[0]]
-      //   chai
-      //   .request(server)
-      //   .post(`/app/me/cart?accessToken=${token}`)
-      //   .send()
-      //   .end((err, res) => {
-      //     res.should.have.status(400);
-      //     done();
-      //   })
-      // })
+
     })
+
 
     describe("DELETE app/me/cart/:productId", ()=>{
+      it('should DELETE an existing product (based on its id) from the cart of logged in user', (done)=>{    
 
-      it('should DELETE an existing product (based in its id) from the cart of logged in user', (done)=>{
         chai
         .request(server)
-        .delete(`/app/me/cart/1?accessToken=${token}`)
-        .end((error, response) => {
-          response.should.have.status(200);
+        .delete(`/app/me/cart/${idForDelete}?accessToken=${token}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.length.should.be.eql(1)
           done();
         })
+      })
+  
+      it('should return an error 401 if the user is not logged in', (done) => {
+        chai
+        .request(server)
+        .delete(`/app/me/cart/${productId}`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        })
+      })
 
-
+      it('should return an error 404 if product id us not found', (done) => {
+        chai
+        .request(server)
+        .delete(`/app/me/cart/${productIdNotValid}?accessToken=${token}`)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.not.be.eql([])
+          done();
+        })
       })
     })
+
 
     describe("POST app/me/cart/:productId", ()=>{
-      it('should cahnge quantity of an existing product (based in its id) from the cart of logged in user', (done)=>{
 
-        it('it should UPDATE the product in the cart to a new quantity', done => {
-          chai
-            .request(server)
-            .post(`/app/me/cart/5?accessToken=${token}`)
-            .send(5)
-            .end((error, response) => {
-              response.should.have.status(200);
-              done();
-            })
-        });
+      it('should cahnge quantity of an existing product (based on its id) from the cart of logged in user', (done)=>{
+        chai
+        .request(server)
+        .post(`/app/me/cart/${productIdForChangeQuant}?accessToken=${token}`)
+        .send('8')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.not.be.eql([])
+          done();
+          })
       })
-    })
-    
+
+      it('should return an error 401 if the user is not logged in', (done) => {
+        chai
+        .request(server)
+        .post(`/app/me/cart/${productId}`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        })
+      })
+
+      it('should return an error 404 if product id us not found', (done) => {
+        chai
+        .request(server)
+        .post(`/app/me/cart/${productIdNotValid}?accessToken=${token}`)
+        .end((err, res) => {
+          res.should.have.status(404);
+          done();
+        })
+      })
+    })  
   })
 })
