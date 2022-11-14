4f1a88b8bceb3ee34127e62d99ab402b909444f7
Jill Arnold <jillarnold2913@gmail.com>
2022-11-14 12:33:15 -0500

    Initialize server and add Chai as devDependency

--- app/server.js
+++ app/server.js
@@ -1,13 +1,34 @@
 var http = require('http');
 var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
+var finalHandler = require('finalHandler');
+var queryString = require('queryString');
 var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
+var { expect } = require('chai');
 
-const PORT = 3001;
+// // var myRouter = Router()
+// // myRouter.use(bodyParser.json())
 
-http.createServer(function (request, response) {
+ const hostname = 'localhost';
+ const port = 3001;
 
-}).listen(PORT);
+ let person = JSON.stringify({
+  name: 'Jill',
+  title: 'Software Engineer',
+  graduated: false,
+});
+
+http.createServer(function(req,res) {
+  res.setHeader('Content-type', 'application/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.writeHead(200)
+  res.end(person);
+  })
+  .listen(port, hostname, function() {
+    console.log('Server running at http://' + hostname + ':' + port + '/');
+  });
+
+
+
+
