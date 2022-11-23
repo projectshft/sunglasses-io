997cfbdeafe46ac59f4578a8643b64c23a00d825
ryrob <ryanrobertson242@gmail.com>
2022-11-23 16:12:47 -0600

    Commit for Windows

--- app/server.js
+++ app/server.js
@@ -6,8 +6,101 @@ var Router = require('router');
 var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
-const PORT = 3001;
+let brands = []
+let products = []
+let users = []
+let user = {}
 
-http.createServer(function (request, response) {
+const router = Router();
+router.use(bodyParser.json());
 
-}).listen(PORT);
+const CORS_HEADERS = {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"Origin, X-Requested-With, Content-Type, Accept, X-Authentication", "Access-Control-Allow-Methods": "GET, PUT, POST, DELETE"};
+
+const VALID_API_KEYS = ["88312679-04c9-4351-85ce-3ed75293b449","1a5c45d3-8ce7-44da-9e78-02fb3c1a71b7"];
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) =>  {
+  if(req.method === 'OPTIONS') {
+    res.writeHead(200, CORS_HEADERS)
+    return res.end()
+  }
+
+  // if(!VALID_API_KEYS.includes(req.headers['x-authentication'])) {
+  //   res.writeHead(401, "You need a valid API key", CORS_HEADERS)
+  //   return res.end()
+  // }
+  console.log(req.headers)
+
+  router(req, res, finalHandler(req, res));
+})
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`server running on port: ${PORT}`)
+
+  brands = JSON.parse(fs.readFileSync('../initial-data/brands.json'))
+
+  products = JSON.parse(fs.readFileSync('../initial-data/products.json'))
+
+  users = JSON.parse(fs.readFileSync('../initial-data/users.json'))
+
+  user = users[0];
+});
+
+//GET all brands
+router.get('/v1/brands', (req, res) => {
+  if(!brands) {
+    res.writeHead(404, "No Brands Found")
+    return res.end();
+  }
+  res.writeHead(200, Object.assign(CORS_HEADERS, {"Content-Type": "application/json"}))
+  return res.end(JSON.stringify(brands))
+})
+
+//GET all Products
+router.get('/v1/products', (req, res) => {
+  if(!products) {
+    res.writeHead(404, "No Brands Found")
+    return res.end();
+  } 
+  res.writeHead(200, {"Content-Type": "application/json"})
+  return res.end(JSON.stringify(products))
+})
+
+//GET all products in brand
+router.get('/v1/brands/:brandId/products', (req, res) => {
+  const { brandId } = req.params;
+  const brand = brands.find(brand => brand.id === brandId)
+  if(!brand) {
+    res.writeHead(404, "Brand does not exist")
+    return res.end();
+  }
+  res.writeHead(200, Object.assign(CORS_HEADERS, {"Content-Type": "application/json"}))
+  const relatedProducts = products.filter(product => product.brandId === brandId)
+  console.log(brandId)
+  return res.end(JSON.stringify(relatedProducts))
+})
+
+//Add Product To Cart
+router.post('/v1/cart/:productId', (req, res) => {
+  const { productId } = req.params;
+  const product = products.find(product => product.id === productId)
+  res.writeHead(200, Object.assign(CORS_HEADERS, {"Content-Type": "application/json"}))
+  user.cart.push(product);
+  return res.end()
+})
+
+//Get All Products in Cart
+router.get('/v1/cart', (req, res) => {
+  res.writeHead(200, Object.assign(CORS_HEADERS, {"Content-Type": "application/json"}))
+  return res.end(JSON.stringify(user.cart))
+})
+
+router.delete('v1/cart/clear', (req, res) => {
+  res.writeHead(200, Object.assign(CORS_HEADERS, {"Content-Type": "application/json"}))
+  user.cart.length(0)
+  return res.end();
+})
+
+module.exports = server;
