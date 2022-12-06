b3b306a8cdea14f11beb518ce2cc1fb017b20162
Damian Goodenough <dgoodenough@proton.me>
2022-12-06 14:48:51 -0800

    Setup server so that it is listening, and loads in data

--- app/server.js
+++ app/server.js
@@ -7,7 +7,52 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const PORT = 3001;
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': "*", "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, X-Authentication",
+}
+
+let users = [];
+let brands = [];
+let products = [];
+
+let router = Router();
+router.use(bodyParser.json())
 
 http.createServer(function (request, response) {
+  if(request.method === 'OPTIONS') {
+    response.writeHead(200, CORS_HEADERS);
+    return response.end();
+  }
 
-}).listen(PORT);
+  router(request, response, finalHandler(request, response));
+}).listen(PORT, error => {
+  if(error){
+    console.log("Error on server startup: ", error);
+  } else {
+    console.log(`Server is listening at http://localhost:${PORT}`)
+  }
+
+ fs.readFile('initial-data/users.json', 'utf-8', (err, data) => {
+    if(err){
+      throw err;
+    }
+    users = JSON.parse(data);
+    console.log("User data loaded")
+  })
+
+  fs.readFile('initial-data/brands.json', 'utf-8', (err, data) => {
+    if(err){
+      throw err;
+    }
+    brands = JSON.parse(data);
+    console.log("Brand data loaded")
+  });
+
+fs.readFile('initial-data/products.json', 'utf-8', (err, data) => {
+  if(err){
+    throw err;
+  }
+  products = JSON.parse(data);
+  console.log("Product data loaded")
+})
+});
