efc8f4f99f535afc6d4249809ebecda71551139d
Damian Goodenough <dgoodenough@proton.me>
2022-12-07 14:07:19 -0800

    Handle malformed requests

--- app/server.js
+++ app/server.js
@@ -8,6 +8,8 @@ const { uid } = require('rand-token');
 const cors = require('cors');
 const url = require('url');
 
+const checkValidParams = require('../helpers/checkValidParams.js');
+
 const PORT = 3001;
 const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
@@ -68,6 +70,13 @@ router.get('/api/brands', (request, response) => {
   const params = queryString.parse(url.parse(request.url).query);
   let brandsRequested = [...brands];
 
+  if (!checkValidParams(params, ['query', 'alphabetical'])) {
+    response.writeHead(400);
+    return response.end(
+      'this endpoint only accepts "query" and "alphabetical" as parameters'
+    );
+  }
+
   if (params.query !== undefined) {
     brandsRequested = brands.filter((brand) =>
       brand.name.toLowerCase().includes(params.query)
@@ -75,33 +84,45 @@ router.get('/api/brands', (request, response) => {
   }
 
   if (params.alphabetical !== undefined) {
-    brandsRequested.sort((a, b) => {
-      const brand1 = a.name.toLowerCase();
-      const brand2 = b.name.toLowerCase();
+    // if (params.alphabetical !== 'za' || params.alphabetical !== 'az') {
+    //   response.writeHead(400);
+    //   return response.end(
+    //     'alphabetical paramter only accepts "za" and "az" and values'
+    //   );
+    // }
+    if (params.alphabetical === 'za' || params.alphabetical === 'az') {
+      brandsRequested.sort((a, b) => {
+        const brand1 = a.name.toLowerCase();
+        const brand2 = b.name.toLowerCase();
 
-      if (params.alphabetical === 'az') {
-        if (brand1 > brand2) {
-          return 1;
+        if (params.alphabetical === 'az') {
+          if (brand1 > brand2) {
+            return 1;
+          }
+
+          if (brand1 < brand2) {
+            return -1;
+          }
         }
 
-        if (brand1 < brand2) {
-          return -1;
-        }
-      }
+        if (params.alphabetical === 'za') {
+          if (brand1 > brand2) {
+            return -1;
+          }
 
-      if (params.alphabetical === 'za') {
-        if (brand1 > brand2) {
-          return -1;
+          if (brand1 < brand2) {
+            return 1;
+          }
         }
-
-        if (brand1 < brand2) {
-          return 1;
-        }
-      }
-      return 0;
-    });
+        return 0;
+      });
+    } else {
+      response.writeHead(400);
+      return response.end(
+        'alphabetical paramter only accepts "za" and "az" and values'
+      );
+    }
   }
-
   response.writeHead(200, { 'Content-Type': 'application/json' });
   return response.end(JSON.stringify(brandsRequested));
 });
