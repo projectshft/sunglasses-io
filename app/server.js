733210556dfc76d97e1de701957f70cce3904cbd
Jack Mullinkosson <jackmullinkosson@Jacks-MacBook-Pro.local>
2022-12-10 12:03:33 -0600

    initial commit

--- app/server.js
+++ app/server.js
@@ -1,13 +1,144 @@
 var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const url = require('url');
+const querystring = require('querystring');
+const fs = require('fs');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const uid = require('rand-token').uid;
+const finalhandler = require('finalhandler');
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+const products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf8'));
+const brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf8'));
+const users = JSON.parse(fs.readFileSync('initial-data/users.json', 'utf8'));
+const accessTokens = [{
+    username: 'yellowleopard753',
+    password: 'jonjon',
+    accessToken: 'qwertyuiopasdfgh'
+}];
 
-}).listen(PORT);
+const cart = [];
+
+const router = Router();
+
+router.get('/brands', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(brands));
+});
+
+router.get('/products/:id', (req, res) => {
+    try{
+  const productId = req.params.id;
+  const product = products.find(p => p.id === productId);
+  if (!product) {
+    throw new Error('Product simply does not exist');
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(product));
+    }
+    catch(error) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: error.message }));
+
+    }
+});
+
+router.get('/products', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(products));
+});
+
+router.use(bodyParser.json());
+
+
+router.post('/login', (req, res) => {
+    try{
+  const { username, password } = req.body;
+  const user = users.find(u => u.login.username === username && u.login.password === password);
+  if (!user) {
+    throw new Error('Could not find either that user name or password. Maybe it is my fault. But maybe it is yours?');
+  }
+    let newAccessToken = {
+        username: user.username,
+        password: user.password,
+        accessToken: uid(16)
+    }
+    accessTokens.push(newAccessToken)
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(newAccessToken.accessToken));
+  } 
+  catch(error){
+    res.writeHead(401, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+
+//get items in cart
+router.get('/cart', (req, res) => {
+    try {
+      const accessToken = req.headers['access-token'];
+      const userInfo = accessTokens.find(token => token.accessToken === accessToken);
+      if (!userInfo) {
+        throw new Error('You have no face. How can a man with no face have a cart?');
+      }
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(cart));
+    } catch (error) {
+      res.writeHead(401, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: error.message }));
+    }
+  });
+
+
+//add an item to cart 
+router.post('/cart', (req, res) => {
+    try {
+      const accessToken = req.headers['access-token'];
+      const userInfo = accessTokens.find(token => token.accessToken === accessToken);
+      if (!userInfo) {
+        throw new Error('Invalid access token');
+      }
+   
+      const product = products.find(product => product.id === req.body.productId);
+      if (!product) {
+        throw new Error('Invalid item id');
+      }
+      cart.push(product)
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(cart));
+    } catch (error) {
+      res.writeHead(401, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: error.message }));
+    }
+  });
+
+  router.delete('/cart/:itemId', (req, res) => {
+    try {
+      const accessToken = req.headers['access-token'];
+      const userInfo = accessTokens.find(token => token.accessToken === accessToken);
+      if (!userInfo) {
+        throw new Error('Invalid access token');
+      }
+      const itemId = req.params.itemId;
+      const product = products.find(product => product.id === itemId);
+      if (!product) {
+        throw new Error('Invalid item id');
+      }
+      cart = cart.filter(cartItem => cartItem.item.id !== itemId);
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(cart));
+    } catch (error) {
+      res.writeHead(401, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ message: error.message }));
+    }
+  });
+
+
+const server = http.createServer((req, res) => {
+  router(req, res, finalhandler(req, res));
+});
+
+module.exports = server.listen(PORT);
+
---
+++ test/test.js
@@ -0,0 +1,110 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app/server.js');
+require('should-http');
+const { send } = require('process');
+
+chai.use(chaiHttp);
+
+describe('/products', () => {
+  it('should return a list of all products', done => {
+    chai
+      .request(server)
+      .get('/products')
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200);
+        chai.expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+});
+
+  describe('/products/:id', () => {
+    it('should return a single product with the specified id', (done) => {
+      let id = '1';
+      chai
+        .request(server)
+        .get(`/products/${id}`)
+        .end((err, res) => {
+          chai.expect(res.status).to.equal(200);
+          chai.expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+    it('should return a 404 error if product does not exist', (done) =>{
+      chai
+      .request(server)
+      .get('/products/9000')
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(404);
+        chai.expect(res.body.message).to.equal('Product simply does not exist');
+        done();
+      });
+    })
+});
+
+describe('/cart', () => {
+  it('should return a list of the items in a users cart', (done) => {
+    chai
+      .request(server)
+      .get('/cart')
+      .set('access-token', 'qwertyuiopasdfgh')
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200);
+        chai.expect(res.body).to.be.an('array');
+        done();
+      });
+  });
+  it('should return a 401 error if the user does not have a valid access token', (done) =>{
+    chai
+    .request(server)
+    .get('/cart')
+    .end((err, res) => {
+      chai.expect(res.status).to.equal(401);
+      chai.expect(res.body.message).to.equal('You have no face. How can a man with no face have a cart?');
+      done();
+    });
+  })
+});
+
+describe('/cart', () => {
+  it('should add an item to the cart', (done) =>{
+    chai
+    .request(server)
+    .post('/cart')
+    .set('access-token', 'qwertyuiopasdfgh')
+    .send({productId: '1'})
+    .end((err, res) => {
+      chai.expect(res.status).to.equal(200);
+      chai.expect(res.body).to.be.an('array');
+      chai.expect(res.body).to.have.length.at.least(1);
+      done();
+    });
+  })
+})
+
+describe('/login', () => {
+  it('should return an access token to the header', (done) => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({ username: 'yellowleopard753', password: 'jonjon' })
+      .end((err, res) => {
+        chai.expect(res.status).to.equal(200);
+        chai.expect(res.body).to.be.a('string');
+        done();
+      });
+  });
+  it('should return a 401 error if the user name or password do not exist', (done) =>{
+    chai
+    .request(server)
+    .post('/login')
+    .send({ username: 'faceless', password: 'man' })
+    .end((err, res) => {
+      chai.expect(res.status).to.equal(401);
+      chai.expect(res.body.message).to.equal('Could not find either that user name or password. Maybe it is my fault. But maybe it is yours?');
+      done();
+    });
+  })
+});
+
