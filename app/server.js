a402d26839d6a47a790c73cd2fbcd789de98092c
funkee <xxtrm98@protonmail.com>
2022-12-27 09:10:03 -0800

    bulk code

--- app/server.js
+++ app/server.js
@@ -1,13 +1,243 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+var http = require("http");
+var fs = require("fs");
+var finalHandler = require("finalhandler");
+var queryString = require("querystring");
+var Router = require("router");
+var bodyParser = require("body-parser");
+var uid = require("rand-token").uid;
 
-const PORT = 3001;
+const PORT = 8001;
 
-http.createServer(function (request, response) {
+let users = [];
+let products = [];
+let brands = [];
+let cart = [
+  {
+    id: "4",
+    categoryId: "2",
+    name: "Better glasses",
+    description: "The best glasses in the world",
+    price: 1500,
+    imageUrls: [
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+    ],
+  },
+  {
+    id: "5",
+    categoryId: "2",
+    name: "Glasses",
+    description: "The most normal glasses in the world",
+    price: 150,
+    imageUrls: [
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+      "https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg",
+    ],
+  },
+];
+let accessTokens = [
+  {
+    username: "lazywolf342",
+    password: "tucker",
+    accessToken: "kmQWrczyuEkRE6VV",
+  },
+];
 
-}).listen(PORT);
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+const CORS_HEADERS = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, X-Authentication",
+};
+
+const server = http
+  .createServer(function (request, response) {
+    if (request.method === "OPTIONS") {
+      response.writeHead(200, CORS_HEADERS);
+    }
+    myRouter(request, response, finalHandler(request, response));
+  })
+  .listen(PORT, (error) => {
+    if (error) {
+      return console.log("Error on Server Startup", error);
+    } else {
+      console.log(`listening on port ${PORT} `);
+    }
+    fs.readFile("brands.json", "utf8", (error, data) => {
+      if (error) throw error;
+      brands = JSON.parse(data);
+      console.log(`Server setup: ${brands.length} brands loaded`);
+    });
+    fs.readFile("products.json", "utf8", (error, data) => {
+      if (error) throw error;
+      products = JSON.parse(data);
+      console.log(`Server setup: ${products.length} products loaded`);
+    });
+    fs.readFile("users.json", "utf8", (error, data) => {
+      if (error) throw error;
+      users = JSON.parse(data);
+      console.log(`Server setup: ${users.length} users loaded`);
+    });
+  });
+
+// Gives directions to access api.
+myRouter.get("/", (request, response) => {
+  response.writeHead(
+    200,
+    Object.assign({ "Content-Type": "application/json" })
+  );
+  response.end("There's nothing to see here, please visit /api/products");
+});
+
+// Gets list of products
+myRouter.get("/api/products", (request, response) => {
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(products));
+});
+
+myRouter.get("/api/products/:id", (request, response) => {
+  try {
+    const productId = request.params.id;
+    const product = products.find((p) => p.id === productId);
+    if (!product) {
+      throw new Error("Product not found.");
+    }
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(product));
+  } catch (error) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+// Gets list of brands
+myRouter.get("/api/brands", (request, response) => {
+  response.writeHead(
+    200,
+    Object.assign(CORS_HEADERS, { "Content-Type": "application/json" })
+  );
+  return response.end(JSON.stringify(brands));
+});
+
+// needs to generate all items with brandId.
+// Gets sunglasses products according to brand id.
+myRouter.get("/api/brands/:id/products", (request, response) => {
+  try {
+    const brandId = request.params.id;
+    const brandItem = products.find((item) => item.categoryId === brandId);
+    if (!brandItem) {
+      throw new Error("Brand does not exist");
+    }
+    const searchedBrand = products.filter(
+      (item) => item.categoryId === brandId
+    );
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(searchedBrand));
+  } catch (error) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+myRouter.post("/api/login", (request, response) => {
+  try {
+    const user = users.find(
+      (user) =>
+        user.login.username === request.body.username &&
+        user.login.password === request.body.password
+    );
+
+    if (!user) {
+      throw new Error("User not found.");
+    }
+    let newAccessToken = {
+      username: user.username,
+      password: user.password,
+      accessToken: uid(16),
+    };
+    accessTokens.push(newAccessToken);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(newAccessToken.accessToken));
+  } catch (error) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+// Need to evaluate if user is logged in at this point.
+myRouter.get("/api/me/cart", (request, response) => {
+  try {
+    const accessToken = request.headers["access-token"];
+    const accountLogin = accessTokens.find(
+      (code) => code.accessToken === accessToken
+    );
+    if (!accountLogin) {
+      throw new Error("Please login to view cart contents.");
+    }
+    response.writeHead(
+      200,
+      Object.assign(CORS_HEADERS, { "Content-Type": "application/json" })
+    );
+    return response.end(JSON.stringify(cart));
+  } catch (error) {
+    response.writeHead(400, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+// delete item by id.
+// User permission will be evaluated for this request.
+myRouter.delete("/api/me/cart/:cartItemId", (request, response) => {
+  try {
+    const cartItemId = request.params.cartItemId;
+    const accessToken = request.headers["access-token"];
+    const accountLogin = accessTokens.find(
+      (code) => code.accessToken === accessToken
+    );
+    if (!accountLogin) {
+      throw new Error("Please login to view cart contents.");
+    }
+
+    const product = cart.find((product) => product.id === cartItemId);
+    if (!product) {
+      throw new Error("Not a product in cart.");
+    }
+    cart = cart.filter((cartItem) => cartItem.id !== cartItemId);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(cart));
+  } catch (error) {
+    response.writeHead(400, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+// User permission will be evaluated for this request.
+myRouter.post("/api/me/cart/", (request, response) => {
+  try {
+    const productItem = request.body.productItem;
+    const accessToken = request.headers["access-token"];
+    const accountLogin = accessTokens.find(
+      (code) => code.accessToken === accessToken
+    );
+    if (!accountLogin) {
+      throw new Error("Please login to view cart contents.");
+    }
+    // This helper method will check the request body and
+    const product = products.find((product) => product.id === productItem);
+    if (!product) {
+      throw new Error("Not a product.");
+    }
+    cart.push(product);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(cart));
+  } catch (error) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    response.end(JSON.stringify({ message: error.message }));
+  }
+});
+
+module.exports = server;
---
+++ test/test.js
@@ -0,0 +1,237 @@
+const { should } = require("chai");
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server");
+
+// Assertion style
+chai.should();
+chai.use(chaiHttp);
+
+describe("Cart API", () => {
+  // GET Tests
+  describe("GET /api/products", () => {
+    it("should GET all the products", (done) => {
+      chai
+        .request(server)
+        .get("/api/products")
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("array");
+          done();
+        });
+    });
+    // Error test for wrong URL.
+    it("should not GET all the products", (done) => {
+      chai
+        .request(server)
+        .get("/api/product")
+        .end((err, response) => {
+          response.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/products/:id", () => {
+    it("should GET the product by ID", (done) => {
+      let id = "1";
+      chai
+        .request(server)
+        .get(`/api/products/${id}`)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("object");
+          done();
+        });
+    });
+    it("should return an error if no product in range", (done) => {
+      chai
+        .request(server)
+        .get("/api/products/-1")
+        .end((err, response) => {
+          response.should.have.status(404);
+          response.body.message.should.equal("Product not found.");
+          done();
+        });
+    });
+  });
+
+  describe("GET /api/brands", () => {
+    it("should get products by brand ID", (done) => {
+      chai
+        .request(server)
+        .get("/api/brands")
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("array");
+          done();
+        });
+    });
+    it("should not get brands by ID", (done) => {
+      chai
+        .request(server)
+        .get("/api/brand")
+        .end((err, response) => {
+          response.should.have.status(404);
+          done();
+        });
+    });
+  });
+
+  describe("GET api/brands/:id/products", () => {
+    it("should return products according to brand ID", (done) => {
+      let id = "1";
+      chai
+        .request(server)
+        .get(`/api/brands/${id}/products`)
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("array");
+          done();
+        });
+    });
+    it("should error if brand ID is out of range", (done) => {
+      let id = "-1";
+      chai
+        .request(server)
+        .get(`/api/brands/${id}/products`)
+        .end((err, response) => {
+          response.should.have.status(404);
+          response.body.message.should.equal("Brand does not exist");
+          done();
+        });
+    });
+  });
+
+  // needs authentication
+  describe("GET /api/me/cart", () => {
+    it("should retrieve contents in cart", (done) => {
+      chai
+        .request(server)
+        .get("/api/me/cart")
+        .set("access-token", "kmQWrczyuEkRE6VV")
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.an("array");
+          done();
+        });
+    });
+
+    it("should return an error if user is not logged in", (done) => {
+      chai
+        .request(server)
+        .get("/api/me/cart")
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.message.should.equal(
+            "Please login to view cart contents."
+          );
+          done();
+        });
+    });
+  });
+
+  // Post Tests
+  describe("POST /api/products/me/cart", () => {
+    it("should post the product to cart by product ID", (done) => {
+      chai
+        .request(server)
+        .post("/api/me/cart")
+        .set("access-token", "kmQWrczyuEkRE6VV")
+        .send({ productItem: "1" })
+        .end((err, response) => {
+          response.should.have.status(200);
+          done();
+        });
+    });
+    it("should return an error if post product ID is not in range", (done) => {
+      chai
+        .request(server)
+        .post("/api/me/cart")
+        .send({ productItem: "-1" })
+        .end((err, response) => {
+          response.should.have.status(401);
+          done();
+        });
+    });
+    it("should return an error if user is not logged in", (done) => {
+      chai
+        .request(server)
+        .post("/api/me/cart")
+        .send({ productItem: "1" })
+        .end((err, response) => {
+          response.should.have.status(401);
+          response.body.message.should.equal(
+            "Please login to view cart contents."
+          );
+          done();
+        });
+    });
+  });
+
+  // Login
+  describe("POST /api/login", () => {
+    it("should return an access token as a string", (done) => {
+      chai
+        .request(server)
+        .post("/api/login")
+        .send({ username: "lazywolf342", password: "tucker" })
+        .end((err, response) => {
+          response.should.have.status(200);
+          response.body.should.be.a("string");
+          done();
+        });
+    });
+    it("should error if incorrect credentials", (done) => {
+      chai
+        .request(server)
+        .post("/api/login")
+        .send({ username: "johndoe", password: "password" })
+        .end((err, response) => {
+          response.should.have.status(401);
+          response.body.message.should.equal("User not found.");
+          done();
+        });
+    });
+  });
+
+  // Delete Test - needs credentials
+  describe("DELETE /api/me/cart/:id", () => {
+    it("should DELETE item in cart by product ID", (done) => {
+      let id = "5";
+      chai
+        .request(server)
+        .delete(`/api/me/cart/${id}`)
+        .set("access-token", "kmQWrczyuEkRE6VV")
+        .end((err, response) => {
+          response.should.have.status(200);
+          done();
+        });
+    });
+    it("should Error if product ID is not in cart", (done) => {
+      let id = "-1";
+      chai
+        .request(server)
+        .delete(`/api/me/cart/${id}`)
+        .set("access-token", "kmQWrczyuEkRE6VV")
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.message.should.equal("Not a product in cart.");
+          done();
+        });
+    });
+    it("should return an error if user is not logged in", (done) => {
+      let id = "1";
+      chai
+        .request(server)
+        .delete(`/api/me/cart/${id}`)
+        .end((err, response) => {
+          response.should.have.status(400);
+          response.body.message.should.equal(
+            "Please login to view cart contents."
+          );
+          done();
+        });
+    });
+  });
+});
