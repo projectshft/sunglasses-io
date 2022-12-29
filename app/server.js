c71f3291844bd8f7b18b750ab44fa16a08b79088
Daniel Bui <dnlbui@proton.me>
2022-12-29 13:54:02 -0600

    bodyParser setup; 15minute var; server err handl;

--- app/server.js
+++ app/server.js
@@ -8,8 +8,19 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
+// Setup router
+var myRouter = Router();
+//makes it so router uses middleware bodyParser to parse body to json
+myRouter.use(bodyParser.json());
+
+// variable value for 15 minutes
+const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000;
+
 server = http.createServer(function (request, response) {
+  //handle error
+  if (error) {
+    return console.log("Error on Server Startup: ", error);
+  };
 
-})
 
-server.listen(PORT);
+}).listen(PORT);
