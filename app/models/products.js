464d8f6a9d51256c87e456476c6d04127a5597b9
Daniel Bui <dnlbui@proton.me>
2022-12-29 15:01:44 -0600

    product class; addProducts fxn; getAll fxn

---
+++ app/models/products.js
@@ -0,0 +1,16 @@
+let products = [];
+
+class Products {
+  constructor(params) {
+    Object.assign(this, params)
+  };
+
+  static addProducts(newProducts = [...newProducts]) {
+    newProducts.forEach((product) => products.push(product));
+  }
+
+  static getAll() {
+    return products
+  }
+}
+
