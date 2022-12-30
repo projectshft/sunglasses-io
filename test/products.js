942847212c60c124513ee567d8b283c0ef248ad4
Daniel Bui <dnlbui@proton.me>
2022-12-30 12:44:18 -0600

    test products route working

--- test/products.js
+++ test/products.js
@@ -3,22 +3,22 @@ let Products = require("../app/models/products");
 
 let chai = require("chai");
 let chaiHttp = require("chai-http");
-let server = require("../app/server");
+let server = require("../app/server.js");
 
 let should = chai.should();
 
 chai.use(chaiHttp);
 
 describe("Products", () => {
-  describe ("/GET brands", () =>{
-    it("it should GET all the brands", done =>{
+  describe ("/GET products", () =>{
+    it("it should GET all the products", done => {
       chai
         .request(server)
-        .get("/brands")
-        .end((err, req) => {
-          res.should.have.status("200");
-          res.body.should.be.an("object");
-          res.body.length.should.eql(5);
+        .get("/v1/products")
+        .end((err, res) => {
+          res.should.have.a.status("200");
+          res.body.should.be.an("array");
+          res.body.length.should.eql(11);
           done();
         })
     })
