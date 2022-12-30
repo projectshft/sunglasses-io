e041298b8e898d1f08911564d6b353f4424114af
Daniel Bui <dnlbui@proton.me>
2022-12-30 14:53:14 -0600

    added brands array and fxns

--- app/models/products.js
+++ app/models/products.js
@@ -1,17 +1,35 @@
 let products = [];
+let brands = [];
 
 class Products {
   constructor(params) {
     Object.assign(this, params);
   };
 
+  static deleteBoth() {
+    brands = [];
+    products = [];
+  }
+
   static addProducts(newProducts) {
     newProducts.forEach((product) => products.push(product));
   };
 
-  static getAll() {
-    return products
+  static getAllProducts() {
+    return products;
   };
+
+  static getProductsById(id) {
+    return products.find(product => product.id = id);
+  }
+
+  static addBrands(newBrands) {
+    newBrands.forEach((brand) => brands.push(brand));
+  };
+
+  static getAllBrands() {
+    return brands;
+  }
 };
 
 //Exports the product for use elsewhere
