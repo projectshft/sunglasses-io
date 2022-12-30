5fc7d8fc35957fbe04895516a6eaeb965c1de1f9
Daniel Bui <dnlbui@proton.me>
2022-12-30 12:12:25 -0600

    + server data; +products route

--- app/server.js
+++ app/server.js
@@ -1,10 +1,12 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser   = require('body-parser');
+const uid = require('rand-token').uid;
+const Products = require('./models/products')
+
 
 const PORT = 3001;
 
@@ -17,15 +19,21 @@ myRouter.use(bodyParser.json());
 const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000;
 
 //creates web server object
-server = http.createServer(function (request, response) {
+let server = http.createServer(function (request, response) {
+  const loadedProducts = JSON.parse(fs.readFileSync("./initial-data/products.json","utf-8"));
+  console.log("hello");
+  Products.addProducts(loadedProducts);
+  Products.getAll();
   //final handler (have googled plenty of time and need someone to explain)
   myRouter(request, response, finalHandler(request,response));
 })
 
-server.listen(PORT, error => {
-  //handle error
-  if (error) {
-    return console.log("Error on Server Startup: ", error);
-  };
-});
+server.listen(PORT);
 
+myRouter
+.get("/v1/products", (request, response) => {
+  //Returns list of brands
+  console.log("Bye");
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(Products.getAll()))
+})
