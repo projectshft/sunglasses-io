5029d6b22df26723def5a179c39f2c3325d172e4
Daniel Bui <dnlbui@proton.me>
2023-01-01 17:56:22 -0600

    started users.js model

---
+++ app/models/users.js
@@ -0,0 +1,19 @@
+let users = [];
+let accessTokens = [];
+
+class Users {
+  constructor(params) {
+    Object.assign(this, params);
+  };
+
+  static addUsers(_users){
+    users = [..._users]
+  };
+
+
+
+  // static logIn(username, password) {
+
+  // }
+}
+
