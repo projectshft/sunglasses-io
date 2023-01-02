d672f57b3dbc320188a5e1b09220bd77d40771f0
Daniel Bui <dnlbui@proton.me>
2023-01-01 23:10:05 -0600

    added add, get, and delete from cart fxns

--- app/models/users.js
+++ app/models/users.js
@@ -15,6 +15,23 @@ class Users {
     })
     return foundUser;
   }
+
+  static addToUserCart(_product, _currentAccessToken){
+    const currentUser = users.find((userObject) => userObject.login.username == _currentAccessToken.username);
+    currentUser.cart.push(_product);
+  };
+
+  static getUserCart(_currentAccessToken){
+    const currentUser = users.find((userObject) => userObject.login.username == _currentAccessToken.username);
+    return currentUser.cart;
+  }
+
+  static deleteFromUserCart(_id, _currentAccessToken){
+    const currentUser = users.find((userObject) => userObject.login.username == _currentAccessToken.username);
+    currentUser.cart = currentUser.cart.filter(productObject => {
+      productObject.id !== _id
+    })
+  }
 }
 
 module.exports = Users;
