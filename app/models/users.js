836e371bacbbde835225e13be36155c6a25f321c
Daniel Bui <dnlbui@proton.me>
2023-01-02 19:50:30 -0600

    removed repeativive cart emptying +  comments

--- app/models/users.js
+++ app/models/users.js
@@ -5,17 +5,18 @@ class Users {
   constructor(params) {
     Object.assign(this, params);
   };
-
+  
+  //populates all the users into users variable
   static addUsers(_users){
     users = [..._users];
-    cart = [];
   };
 
+//populate cart with user's object's cart
   static addCart(_user) {
-    cart = [];
     cart = [..._user.cart];
   }
 
+  //find user by matching with paramter inputs
   static authenticateUser(_email,_password){
     const foundUser = users.find(user => {
       return user.email == _email && user.login.password == _password;
@@ -23,10 +24,12 @@ class Users {
     return foundUser;
   }
 
+  //returns user's cart
   static getUserCart(){
     return cart;
   }
 
+  //uses parameters to add input product to current user
   static addToUserCart(_product, _currentAccessToken){
     cart.push(_product);
     //update users cart in users object
@@ -37,6 +40,7 @@ class Users {
     return;
   };
 
+  //uses parameters to delete an item from current user's cart
   static deleteFromUserCart(_id, _currentAccessToken){
     let currentUserIndex = users.findIndex((userObject) => {
       return userObject.login.username.length === _currentAccessToken.username.length;
