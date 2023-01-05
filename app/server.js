9d9d11f086f4cf172e3e73cb63e5417d3874ad28
Ezra Godde <ezragodde@Ezras-MacBook-Air.local>
2023-01-05 12:55:40 -0800

    test 1 passing, test 2 async timeout error

--- app/server.js
+++ app/server.js
@@ -3,11 +3,50 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+// to parse request bodies
+var bodyParser = require('body-parser');
+// to generate access tokens
 var uid = require('rand-token').uid;
 
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+// State holding variables
 const PORT = 3001;
+let brands = [];
+let products = [];
+let users = [];
+const accessTokens = [];
 
-http.createServer(function (request, response) {
+chai.request('http://localhost:3001');
+should = chai.should();
+chai.use(chaiHttp);  
 
-}).listen(PORT);
+// Server
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  myRouter(req, res, finalHandler(req, res));
+});
+
+// Setup Router
+let myRouter = Router();
+myRouter.use(bodyParser.json());
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf-8'));
+  products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf-8'));
+  users = JSON.parse(fs.readFileSync('initial-data/users.json', 'utf-8'));
+});
+
+
+// test 1 (passing, no err test written):
+myRouter.get('/api/brands', (req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  return res.end(JSON.stringify(brands));
+})
+
+
+
+module.exports = server;
---
+++ test/server-test.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+let server = require('../app/server');
+// const products = require('../initial-data/products.json');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+
+//test 1 return all brands
+describe('api/brands', () => {
+  describe('GET', () => {
+    it('it should return an array of brands', (done) => {
+      chai
+        .request(server)
+        .get('/api/brands')
+        .end((err, res) => {
+          res.should.have.status('200');
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(5);
+          res.body[0].should.have.property('id');
+          res.body[0].should.have.property('name');
+          
+          done();
+        });
+    });
+  })
+})
+
+//test 2 return all products
+describe('api/products', () => {
+  describe('GET', () => {
+    it('it should return all products', (done) => {
+      chai
+        .request(server)
+        .get('/api/products')
+        .end((err, res) => {
+          res.should.have.status('200');
+          res.body.should.be.an('array');
+          res.body.should.have.lengthOf(11);
+          res.body[0].should.have.properties('id', 'categoryId', 'name', 'price');
+          
+          done();
+        });
+    });
+  })
+})
