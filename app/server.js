c28570ef38ee6976bbdd45e76a65313d5333ca40
Ezra Godde <ezragodde@Ezras-MacBook-Air.local>
2023-01-15 22:50:10 -0800

    all tests passing

--- app/server.js
+++ app/server.js
@@ -1,48 +1,45 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-// to parse req bodies
-var bodyParser = require('body-parser');
-// to generate access tokens
-var uid = require('rand-token').uid;
+var http = require("http");
+var fs = require("fs");
+var finalHandler = require("finalhandler");
+var queryString = require("querystring");
+var Router = require("router");
+var bodyParser = require("body-parser");
+var uid = require("rand-token").uid;
 
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-chai.use(chaiHttp);
-
-// State holding variables
-const PORT = 3001;
 let brands = [];
 let products = [];
 let users = [];
-const accessTokens = [
-  { username: 'lazywolf342', password: 'tucker' }
+let accessTokens = [
+  {
+    token: "9DdyGguPUCVfw9hH",
+    username: "lazywolf342",
+  },
 ];
 
-chai.request('http://localhost:3001');
-should = chai.should();
-chai.use(chaiHttp);  
+const PORT = 3001;
+const Token_Timeout = 15 * 60 * 1000; 
 
-// Server
-const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  myRouter(req, res, finalHandler(req, res));
-});
-
-// Setup Router
-let myRouter = Router();
+var myRouter = Router();
 myRouter.use(bodyParser.json());
 
-server.listen(PORT, err => {
-  if (err) throw err;
-  brands = JSON.parse(fs.readFileSync('initial-data/brands.json', 'utf-8'));
-  products = JSON.parse(fs.readFileSync('initial-data/products.json', 'utf-8'));
-  users = JSON.parse(fs.readFileSync('initial-data/users.json', 'utf-8'));
-});
-
-
+let server = http
+  .createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response));
+  })
+  .listen(PORT, (error) => {
+    fs.readFile("initial-data/brands.json", "utf8", (error, data) => {
+      if (error) throw error;
+      brands = JSON.parse(data);
+    });
+    fs.readFile("initial-data/users.json", "utf8", (error, data) => {
+      if (error) throw error;
+      users = JSON.parse(data);
+    });
+    fs.readFile("initial-data/products.json", "utf-8", (error, data) => {
+      products = JSON.parse(data);
+    });
+  });
+//test 1
 // test 1 (passing)
 myRouter.get('/api/brands', (req, res) => {
   if(!brands) {
@@ -53,7 +50,6 @@ myRouter.get('/api/brands', (req, res) => {
   return res.end(JSON.stringify(brands));
   }
 })
-
 //test 2 (passing)
 myRouter.get("/api/products", function (req, res) {
   if(!products) {
@@ -64,6 +60,7 @@ myRouter.get("/api/products", function (req, res) {
   return res.end(JSON.stringify(products));
   }
 });
+
 
 //test 3 (passing)
 myRouter.get('/api/brands/:id/products', (req, res) => {
@@ -78,47 +75,144 @@ myRouter.get('/api/brands/:id/products', (req, res) => {
   return res.end(JSON.stringify(returnProducts));
 }});
 
+
 //test 4 (passing)
-
-myRouter.post('/api/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-  if (username && password) {
-    const currentUser = users.find(
-      (currentUser) =>  currentUser.login.username === username && currentUser.login.password === password
-    );
-
-    if (currentUser) {
-      res.writeHead(200, { 'Content-Type': 'Application/json' });
-
-      const currentAccessToken = accessTokens.find(
-        (token) => token.username === username
+myRouter.post("/api/login", (request, response) => {
+  if (request.body.username && request.body.password) {
+    let user = users.find((user) => {
+      return (
+        user.login.username == request.body.username &&
+        user.login.password == request.body.password
       );
-
+    });
+    if (user) {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      let currentAccessToken = accessTokens.find((tokenUser) => {
+        return tokenUser.username == user.login.username;
+      });
       if (currentAccessToken) {
         currentAccessToken.lastUpdated = new Date();
-        return res.end(JSON.stringify(currentAccessToken.token));
+        return response.end(JSON.stringify(currentAccessToken.token));
+      } else {
+        let newAccessToken = {
+          username: user.login.username,
+          token: uid(16),
+          lastUpdated: new Date(),
+        };
+        accessTokens.push(newAccessToken);
+        return response.end(JSON.stringify(newAccessToken.token));
       }
-      const addAccessToken = {
-        username,
-        token: uid(16),
-      };
-      accessTokens.push(addAccessToken);
-      return res.end(JSON.stringify(addAccessToken.token));
+    } else {
+      response.writeHead(401, "Invalid username or password");
+      return response.end();
     }
-    res.writeHead(401, 'incorrect username or password');
-    return res.end();
+  } else {
+    response.writeHead(400, "invalid request");
+    return response.end();
   }
-  res.writeHead(400, 'invalid request');
-  return res.end();
 });
 
-// test 5
+
+//function after login for token
+const getTokenRequest = function(request) {
+  const parsedUrl = require("url").parse(request.url, true);
+  if (parsedUrl.query.accessToken) {
+    let currentAccessToken = accessTokens.find((accessToken) => {
+      return (
+        accessToken.token == parsedUrl.query.accessToken &&
+        new Date() - accessToken.lastUpdated < Token_Timeout
+      );
+    });
+    if (currentAccessToken) {
+      return currentAccessToken;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
 
 
+//test 5 (passing)
+myRouter.get("/api/me/cart", (request, response) => {
+  let currentAccessToken = getTokenRequest(request);
+  if (!currentAccessToken) {
+    response.writeHead(401, "cannot access cart");
+    return response.end();
+  } else {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    if (!user) {
+      response.writeHead(401, "You're not signed in");
+      return response.end();
+    } else {
+      response.writeHead(200, { "Content-Type": "application/json" });
+      return response.end(JSON.stringify(user.cart));
+    }
+  }
+});
 
 
+//test 6 (passing)
+myRouter.post("/api/me/cart", (request, response) => {
+  let currentAccessToken = getTokenRequest(request);
+  if (!currentAccessToken) {
+    response.writeHead(401, "cannot access cart");
+    return response.end();
+  } else {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    user.cart.push(request.body);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(user.cart));
+  }
+});
 
 
+//test 7 (passing)
+myRouter.delete("/api/me/cart/:productId", (request, response) => {
+  let currentAccessToken = getTokenRequest(request);
+  if (!currentAccessToken) {
+    response.writeHead(401, "cannot access cart");
+    return response.end();
+  } else {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    const { productId } = request.params;
+    user.cart.filter((cart) => cart.id != productId);
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(user.cart));
+  }
+});
+
+
+//test 8 (passing)
+myRouter.post("/api/me/cart/:productId", (request, response) => {
+  let currentAccessToken = getTokenRequest(request);
+  if (!currentAccessToken) {
+    response.writeHead(401, "cannot access cart");
+    return response.end();
+  } else {
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    let product = products.find((product) => {
+      const { productId } = request.params;
+      return product.id == productId;
+    });
+    if (!product) {
+      response.writeHead(404, "cannot find product");
+      return response.end();
+    } else {
+      user.cart.push(product);
+    }
+    response.writeHead(200, { "Content-Type": "application/json" });
+    return response.end(JSON.stringify(user.cart));
+  }
+});
 
 module.exports = server;
--- test/server-test.js
+++ test/server-test.js
@@ -3,11 +3,12 @@ const chaiHttp = require('chai-http');
 let { expect } = require('chai');
 let server = require('../app/server');
 
-const brands = require('../initial-data/brands.json')
-const products = require('../initial-data/products.json')
+let products = require("../initial-data/products.json");
 
 const should = chai.should();
 chai.use(chaiHttp);
+
+const accessToken = "9DdyGguPUCVfw9hH";
 
 
 //test 1 return all brands
@@ -23,7 +24,6 @@ describe('api/brands', () => {
           res.body.should.have.lengthOf(5);
           res.body[0].should.have.property('id');
           res.body[0].should.have.property('name');
-          
           done();
         });
     });
@@ -31,12 +31,10 @@ describe('api/brands', () => {
 })
 
 
-
 //test 2 return all products
 describe('api/products', () => {
   describe('GET', () => {
     it('it should return all products', done => {
-
       chai
         .request(server)
         .get('/api/products')
@@ -47,15 +45,13 @@ describe('api/products', () => {
           res.body[0].should.have.property('id');
           res.body[0].should.have.property('price');
           res.body[0].should.have.property('categoryId');
-
-        
           done();
         });
     });
   })
 })
 
-const id = '2';
+let id = '2';
 //test 3 brand id products
 describe(`/api/brands/:${id}/products`, () => {
   it(`return an array of products with the category id of ${id}`, (done) => {
@@ -77,23 +73,143 @@ describe(`/api/brands/:${id}/products`, () => {
   })
 
 
-//test 4 POST /api/login
+// //test 4 POST /api/login
   
 describe('/api/login', () => {
   describe('POST', () => {
     it('returns an access token if login is valid', (done) => {
+      console.log('test');
       chai
         .request(server)
         .post('/api/login')
         .send({ username: 'lazywolf342', password: 'tucker' })
         .end((err, res) => {
           res.should.have.status('200');
-          res.body.should.be.a('string');
           done();
         });
     });
   })
-})
+    it('should not return access token if doesnt match user', (done) => {
+      chai
+      .request(server)
+      .post('/api/login')
+      .send({ username: 'lazyguy342', password: 'rucker' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        done();
+      });
+    });
+    it("it should not return access token if user or password missing", (done) => {
+      chai
+        .request(server)
+        .post("/api/login")
+        .send({ username: 'lazyguy342' })
+        .end((err, res) => {
+          res.should.have.status(400);
+          done();
+        });
+  });
+});
 
 
-//test 5 
+  //test 5
+  describe('/api/me/cart', () => {
+  describe("GET", () => {
+    it("shows the user's cart", (done) => {
+      chai
+        .request(server)
+        .get(`/api/me/cart?accessToken=${accessToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+    it("wont show user cart if no access token", (done) => {
+      chai
+        .request(server)
+        .get(`/api/me/cart?`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+  });
+
+
+  // test 6
+  describe("POST add item to user cart", () => {
+    it("adds an item to user's cart", (done) => {
+      let product = products[0];
+      chai
+        .request(server)
+        .post(`/api/me/cart?accessToken=${accessToken}`)
+        .send(product)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body[0].should.be.an("object");
+          done();
+        });
+    });
+    it("wont show user cart if wrong access token", (done) => {
+      chai
+        .request(server)
+        .post(`/api/me/cart?accessToken=123`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+  });
+});
+
+
+// test 7    
+describe("DELETE remove item from cart", () => {
+    it("delete selected item from cart", (done) => {
+      let id = "4";
+      chai
+        .request(server)
+        .delete(`/api/me/cart/${id}?accessToken=${accessToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done();
+        });
+    });
+    it("wont show user cart if wrong access token", (done) => {
+      let id = "4";
+      chai
+        .request(server)
+        .delete(`/api/me/cart/${id}?accessToken=123`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+  });
+
+  describe("POST change cart quantity", () => {
+    it("changes the quantity of item in cart", (done) => {
+      let id = "4";
+      chai
+        .request(server)
+        .post(`/api/me/cart/${id}?accessToken=${accessToken}`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body[0].should.be.an("object");
+          done();
+        });
+    });
+    it("wont show user cart if wrong access token", (done) => {
+      chai
+        .request(server)
+        .post(`/api/me/cart?accessToken=123`)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+  });
