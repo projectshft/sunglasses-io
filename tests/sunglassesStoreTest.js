dc4847626cb975e2e35c8027769ae59fe064626e
Nils Landsberg <nils.landsberg78@gmail.com>
2023-02-22 16:03:25 -0500

    Initial commit. Create tests folder and test.js

---
+++ tests/sunglassesStoreTest.js
@@ -0,0 +1,22 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../app/server');
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it('it should return a list of all brands', (done) => {
+      chai
+        .request(server)
+        .get('/brands')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(5);
+        })
+    })
+  })
+})
