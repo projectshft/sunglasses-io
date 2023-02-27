e78bef93a054df94bd768851e4e4d2e3ac804ad1
Michael Byers <108441006+mike242424@users.noreply.github.com>
2023-02-26 19:45:55 -0500

    working login, get and post me/cart, and delete and post me/car/:productId, all with working tests

--- app/server.js
+++ app/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const finalHandler = require('finalhandler');
 const queryString = require('querystring');
 const Router = require('router');
@@ -10,7 +11,7 @@ const uid = require('rand-token').uid;
 let brands = [];
 let products = [];
 let users = [];
-let accessTokens = [];
+let accessTokens = ["oPblTB0dxgWMOBzr"];
 let failedLoginAttempts = {};
 
 const PORT = process.env.PORT || 3001;
@@ -21,6 +22,7 @@ const TOKEN_VALIDITY_TIMEOUT = 15 * 60 * 1000;
 const router = Router();
 router.use(bodyParser.json());
 
+// Create server
 const server = http.createServer((req, res) => {
   if (!VALID_API_KEYS.includes(req.headers["x-authentication"])) {
     res.writeHead(401, "You need to have a valid API key to use this API");
@@ -31,6 +33,7 @@ const server = http.createServer((req, res) => {
   
 });
 
+// Listen to requests
 server.listen(PORT, "127.0.0.1", (err) => {
   if (err) {
     return console.log('Error on Server Startup: ', err)
@@ -69,7 +72,7 @@ server.listen(PORT, "127.0.0.1", (err) => {
   }
 });
 
-// GET all brands - All users can access with API key
+// GET all brands - all users can access with API key
 router.get("/api/brands", (req, res) => {
   if (!brands) {
     res.writeHead(404, "There aren't any brands to return");
@@ -105,8 +108,8 @@ router.get("/api/products", (req, res) => {
   }
 });
 
-// Helper to get the number of failed requests per username
-const getNumberOfFailedLoginRequestsForUsername = function(username) {
+// Function to get the number of failed requests per username
+const getNumberOfFailedLoginRequestsForUsername = username => {
   let currentNumberOfFailedRequests = failedLoginAttempts[username];
   if (currentNumberOfFailedRequests) {
     return currentNumberOfFailedRequests;
@@ -115,17 +118,15 @@ const getNumberOfFailedLoginRequestsForUsername = function(username) {
   }
 }
 
-// Helper to set the number of failed requests per username
-const setNumberOfFailedLoginRequestsForUsername = function(username, numFails) {
-  failedLoginAttempts[username] = numFails;
-}
+// Function to set the number of failed requests per username
+const setNumberOfFailedLoginRequestsForUsername = (username, numFails) => failedLoginAttempts[username] = numFails;
 
-// POST Login call
-router.post('/api/login', function(req, res) {
+// POST Login call - username and password
+router.post('/api/login', (req, res) => {
   // Make sure there is a username and password in the request
   if (req.body.username && req.body.password) {
     // See if there is a user that has that username and password
-    let user = users.find((user) => {
+    let user = users.find(user => {
       return user.login.username == req.body.username && user.login.password == req.body.password;
     });
 
@@ -170,11 +171,11 @@ router.post('/api/login', function(req, res) {
   }
 });
 
-// Helper method to process access token
-const getValidTokenFromRequest = function(req) {
-  const parsedUrl = require('url').parse(req.url,true)
+// Function to process access token
+const getValidTokenFromRequest = (req) => {
+  const parsedUrl = url.parse(req.url, true);
   if (parsedUrl.query.accessToken) {
-    // Verify the access token to make sure its valid and not expired
+    // Verify the access token to make sure it's valid and not expired
     let currentAccessToken = accessTokens.find((accessToken) => {
       return accessToken.token == parsedUrl.query.accessToken && ((new Date) - accessToken.lastUpdated) < TOKEN_VALIDITY_TIMEOUT;
     });
@@ -188,34 +189,121 @@ const getValidTokenFromRequest = function(req) {
   }
 };
 
-// GET cart - Only logged in users can access their cart
+// GET Users cart - Only logged in users can access their cart
 router.get("/api/me/cart", (req, res) => {
-  // Check if the current user has access to the store
-  let user = users.find((user) => {
-    return user.email == currentAccessToken.email;
-  });
-  if (!users) {
-    res.writeHead(404, "There aren't any brands to return");
-    res.end();
+  let currentAccessToken = getValidTokenFromRequest(req);
+
+  if (!currentAccessToken) {
+    // If there isn't an access token in the request, we know that the user isn't logged in, so don't continue
+    res.writeHead(401, "You need to have access to this call to continue");
+    return res.end();
   } else {
-    res.writeHead(200, {"Content-Type": "application/json"});
-    return res.end(JSON.stringify(user));
+    // Check if current user has a valid access token
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    // Only if the user has a valid access token do we return their cart contents
+    if (user) {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(user.cart));
+    } else {
+      res.writeHead(403, "You don't have access to that cart");
+      return res.end();
+    }
   }
 });
 
-// POST
+// POST Add a product to your cart - Only logged in users can access their cart
 router.post("/api/me/cart", (req, res) => {
-  
+  let currentAccessToken = getValidTokenFromRequest(req);
+
+  if (!currentAccessToken) {
+    // If there isn't an access token in the request, we know that the user isn't logged in, so don't continue
+    res.writeHead(401, "You need to have access to this call to continue");
+    return res.end();
+  } else {
+    // Check if current user has a valid access token
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    // Only if the user has a valid access token can they add a product to their cart
+    if (user) {
+      user.cart = [
+        ...user.cart,
+        {...req.body, quantity: 0}, 
+      ];
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(user.cart));
+    } else {
+      res.writeHead(403, "You don't have access to that cart");
+      return res.end();
+    }
+  }
 });
 
-// DELETE
+// DELETE a specific product from your cart - Only logged in users can access their cart
 router.delete("/api/me/cart/:productID", (req, res) => {
-  
+  let currentAccessToken = getValidTokenFromRequest(req);
+
+  if (!currentAccessToken) {
+    // If there isn't an access token in the request, we know that the user isn't logged in, so don't continue
+    res.writeHead(401, "You need to have access to this call to continue");
+    return res.end();
+  } else {
+    // Check if current user has a valid access token
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    // Only if the user has a valid access token can they delete a product from their cart
+    if (user) {
+      const { productID } = req.params;
+      if (!user.cart) {
+        res.writeHead(404, "Your shopping cart is empty");
+        return res.end();
+      } else {
+        const newCart = user.cart.filter(cartItem => cartItem.id !== productID);
+        user.cart = newCart;
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify(user.cart));
+      }
+    } else {
+      res.writeHead(403, "You don't have access to that cart");
+      return res.end();
+    }
+  }
 });
 
-// POST
+// POST Change quantity of a product in your cart - Only logged in users can access their cart
 router.post("/api/me/cart/:productID", (req, res) => {
-  
+  let currentAccessToken = getValidTokenFromRequest(req);
+
+  if (!currentAccessToken) {
+    // If there isn't a access token in the request, we know that the user isn't logged in, so don't continue
+    res.writeHead(401, "You need to have access to this call to continue");
+    return res.end();
+  } else {
+    // Check if current user has a valid access token
+    let user = users.find((user) => {
+      return user.login.username == currentAccessToken.username;
+    });
+    // Only if the user has a valid access token can they increase the product quantity in their cart
+    if (user) {
+      const { productID } = req.params;
+      if (!user.cart) {
+        res.writeHead(404, "Your shopping cart is empty");
+        return res.end();
+      } else {
+        if (user.cart.id === productID) {
+          user.cart.quantity +=1;
+        }
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        return res.end(JSON.stringify(user.cart));
+      }
+    } else {
+      res.writeHead(403, "You don't have access to that cart");
+      return res.end();
+    }
+  }
 });
 
 module.exports = server;
--- sunglasses-tdd/sunglasses-test.js
+++ sunglasses-tdd/sunglasses-test.js
@@ -1,4 +1,5 @@
 let chai = require("chai");
+let {expect, assert} = require("chai");
 let chaiHttp = require("chai-http");
 let server = require("../app/server");
 
@@ -12,12 +13,16 @@ let brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf-8"));
 let products = JSON.parse(fs.readFileSync("./initial-data/products.json", "utf-8"));
 
 describe("Brands", () => {
+  // Test the GET /api/brands route
   describe("/GET brands", () => {
     it("it should GET all the brands", (done) => {
       chai.request(server)
       .get("/api/brands")
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
       .end((err, res) => {
         res.should.have.status(200);
+        expect(err).to.be.null;
+        res.should.be.json;
         res.body.should.be.an("array");
         res.body.should.be.eql(brands);
         done();
@@ -25,12 +30,15 @@ describe("Brands", () => {
     });
   });
 
-  describe("/GET products from a selected brand using products' categoryId", () => {
+  // Test the GET /api/brands/:id/products route
+  describe("/GET products from a selected brand using categoryId", () => {
     it("it should GET all the products in a certain brand ", (done) => {
       chai.request(server)
-      .get(`/api/brands/${categoryId}/products`)
+      .get(`/api/brands/1/products`)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
       .end((err, res) => {
         res.should.have.status(200);
+        expect(err).to.be.null;
         res.body.should.be.an("array");
         done();
       });
@@ -39,14 +47,106 @@ describe("Brands", () => {
 });
 
 describe("Products", () => {
+  // Test the GET /api/products route
   describe("/GET products", () => {
     it("it should GET all the products", (done) => {
       chai.request(server)
       .get("/api/products")
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
       .end((err, res) => {
         res.should.have.status(200);
+        expect(err).to.be.null;
+        res.should.be.json;
         res.body.should.be.an("array");
         res.body.should.be.eql(products);
+        done();
+      });
+    });
+  });
+});
+
+describe("Login", () => {
+  // Test the POST /api/login route
+  describe("/POST login", () => {
+    it("it should GET all the products", (done) => {
+      chai.request(server)
+      .post("/api/login")
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send({"username": "yellowleopard753", "password": "jonjon"})
+      .end((err, res) => {
+        accessToken = 'accessToken=' + res.body;
+        res.should.have.status(200);
+        expect(err).to.be.null;
+        res.should.be.json;
+        res.body.should.be.an("string");
+        done();
+      });
+    });
+  });
+});
+
+describe("User", () => {
+  // Test the GET /api/me/cart route
+  describe("/GET Users Cart", () => {
+    it("it should GET all the products ina  users cart", (done) => {
+      chai.request(server)
+      .get(`/api/me/cart?${accessToken}`)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send({"username": "yellowleopard753", "password": "jonjon"})
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(err).to.be.null;
+        res.should.be.json;
+        res.body.should.be.an("array");
+        done();
+      });
+    });
+  });
+
+  // Test the POST /api/me/cart route
+  describe("/POST Users Cart ", () => {
+    it("it should allow users to add products to their cart", (done) => {
+      chai.request(server)
+      .post(`/api/me/cart?${accessToken}`)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send({"username": "yellowleopard753", "password": "jonjon"})
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect(err).to.be.null;
+        res.should.be.json;
+        res.body.should.be.an("array");
+        done();
+      });
+    });
+  });
+
+  // Test the DELETE /api/me/cart/:productId route
+  describe("/DELETE Item In Users Cart ", () => {
+    it("it should delete product in users cart", (done) => {
+      chai.request(server)
+      .delete(`/api/me/cart/1?${accessToken}`)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send({"username": "yellowleopard753", "password": "jonjon"})
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an("array");
+        done();
+      });
+    });
+  });
+
+  // Test the POST /api/me/cart/:productId route
+  describe("/POST Item In Users Cart ", () => {
+    it("it change the quantity of a product in a users cart", (done) => {
+      chai.request(server)
+      .post(`/api/me/cart/1?${accessToken}`)
+      .set('x-authentication', '88312679-04c9-4351-85ce-3ed75293b449')
+      .send({"username": "yellowleopard753", "password": "jonjon"})
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.an("array");
         done();
       });
     });
