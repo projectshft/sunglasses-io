39db2cb2cbd8407f719afce044d27c902bd4e5e8
Johnny Wright <johnnywrightiv@gmail.com>
2023-03-01 16:54:29 -0600

    Create server, router, and swagger file

--- app/server.js
+++ app/server.js
@@ -8,6 +8,53 @@ var uid = require('rand-token').uid;
 
 const PORT = 3001;
 
-http.createServer(function (request, response) {
+// Define local state variables to store data (in place of database)
+let brands = [];
+let products = [];
+let users = [];
 
-}).listen(PORT);
+
+// Setup Router
+var myRouter = Router();
+myRouter.use(bodyParser.json());
+
+// Create Server
+http.createServer(function (request, response) {
+  
+  // --- Logic for CORS Preflight and Verify API key here ---
+  
+  myRouter(request, response, finalHandler(request, response))
+}).listen(PORT, error => {
+  if (!error) {
+    console.log(`Server running on ${PORT}`)
+  } else {
+    return console.log("Error on Server Startup: ", error);
+  }
+  
+  // Load in Brands data
+  fs.readFile("initial-data/brands.json", "utf8", (error, data) => {
+    if (error) throw error;
+    brands = JSON.parse(data);
+    console.log(`Server setup: ${brands.length} brands loaded`);
+  });
+
+  // Load in Products data
+  fs.readFile("initial-data/products.json", "utf8", (error, data) => {
+    if (error) throw error;
+    products = JSON.parse(data);
+    console.log(`Server setup: ${products.length} products loaded`);
+  });
+
+  // Load in Users data
+  fs.readFile("initial-data/users.json", "utf8", (error, data) => {
+    if (error) throw error;
+    users = JSON.parse(data);
+    console.log(`Server setup: ${users.length} users loaded`);
+  });
+
+});
+
+// Define Routes
+myRouter.get("/", (request, response) => {
+  response.end("This is the root endpoint, nothing to see here.");
+});
