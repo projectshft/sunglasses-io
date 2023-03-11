8c36e108414cce74f27c1ef15f639ec46d156ff7
josephl37 <josephliow2000@gmail.com>
2023-03-11 16:22:05 -0600

    added GET products given brand id endpoint

--- app/server.js
+++ app/server.js
@@ -7,6 +7,7 @@ var bodyParser   = require('body-parser');
 var uid = require('rand-token').uid;
 
 const Brands = require('../initial-data/brands.json');
+const Products = require('../initial-data/products.json');
 
 const PORT = 3001;
 
@@ -19,9 +20,22 @@ let server = http.createServer(function (request, response) {
 }).listen(PORT);
 
 // TODO: GET all brands
-myRouter.get('/brands', function(request, response) {
+myRouter.get('/api/brands', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
   return response.end(JSON.stringify(Brands));
+})
+
+// TODO: GET all products given brand id
+myRouter.get('/api/brands/:id/products', function(request, response) {
+  const { id } = request.params;
+  const products = Products.find(product => product.categoryId == id);
+  if (!products) {
+    response.writeHead(404, "That brand id does not exist");
+    return response.end();
+  }
+  response.writeHead(200, {"Content-Type": "application/json"});
+  const relatedProducts = Products.filter(product => product.categoryId == id);
+  return response.end(JSON.stringify(relatedProducts));
 })
 
 module.exports = server;
--- test/test-server.js
+++ test/test-server.js
@@ -10,10 +10,25 @@ describe("/GET brands", () => {
   it("it should GET all the brands", (done) => {
     chai
       .request(server)
-      .get("/brands")
+      .get(`/api/brands`)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.an("array");
+        done();
+      })
+  })
+})
+
+// TODO: Test GET brands/:id/products endpoint
+describe("/GET products in brand", () => {
+  it("it should GET all the products given brand id", (done) => {
+    let id = 1;
+    chai
+      .request(server)
+      .get(`/api/brands/${1}/products`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an("array")
         done();
       })
   })
