dc64d5c97fd2d521ba7ce65754611a5815f0de7c
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-12 07:15:45 -0400

    made test for login and 1 brand

---
+++ test/brands.js
@@ -0,0 +1,24 @@
+let brands = "";
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Brands", () => {
+  describe("/GET brands", () => {
+    it("it should GET all of the brands", done => {
+      chai.request(server)
+      .get("/brands")
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.an("array");
+        res.body.length.should.be.eql(0);
+        done();
+      })
+    })
+  })
+})
---
+++ test/login.js
@@ -0,0 +1,42 @@
+let Login = "";
+
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../server");
+
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe("Login", () => {
+  describe("/POST login data", () => {
+    it("post all required login data", done => {
+      let login = {
+        username: "test",
+        password: "test",
+        salt: "test",
+        md5: "test",
+        sha1: "test",
+        sha256: "test"
+      }
+      chai.request(server)
+      .post("/login")
+      .send(login.username, login.password)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should
+        res.should.have.status(201)
+        res.body.should.be.an("object");
+        res.body.should.have(username)
+        res.body.should.have(password)
+        res.body.should.have(salt)
+        res.body.should.have(md5)
+        res.body.should.have(sha1)
+        res.body.should.have(sha256)
+        err.should.have.status(400)
+        err.should.have.status(401)
+        done();
+      })
+    })
+  })
+})
