ac323f18d745364a844808b034d2e0964f396017
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2023-03-13 10:54:19 -0400

    set up server a little bit more

--- app/server.js
+++ app/server.js
@@ -1,22 +1,36 @@
 const http = require('http');
 const finalHandler = require('finalhandler');
-// const queryString = require('querystring');
+const queryString = require('querystring');
 const Router = require('router');
 const bodyParser   = require('body-parser');
-const Brands = require('./models/brands')
-const Products = require('./models/products')
+const fs = require('fs');
+
+const Brands = require('./models/brands');
+const Products = require('./models/products');
 //Need other Models-------
 
 // const uid = require('rand-token').uid;
+
+//state variables
+let brands = [];
+let users = [];
+let products = [];
 
 // Setup router
 var myRouter = Router();
 myRouter.use(bodyParser.json());
 
-// This function is a bit simpler...
 let server = http.createServer(function (request, response) {
   	myRouter(request, response, finalHandler(request, response))
-}).listen(8090);
+}).listen(8090, () => {
+  //data from files
+  //Brands
+  brands = JSON.parse(fs.readFileSync("./initial-data/brands.json", "utf-8"));
+//products
+  products = JSON.parse(fs.readFileSync("./initial-data/products.json", "utf-8"));
+//all users
+  users = JSON.parse(fs.readFileSync("./initial-data/users.json", "utf-8"));
+});
 
 myRouter.get('/brand', function(request, response) {
   response.writeHead(200, {"Content-Type": "application/json"});
