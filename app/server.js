c98061a7c146ad78d2734d54f71b071543421e15
FenrirIsFree <nickscape96@gmail.com>
2023-03-26 07:46:51 -0400

    Added Tests

--- app/server.js
+++ app/server.js
@@ -1,13 +1,25 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser   = require('body-parser');
-var uid = require('rand-token').uid;
+const http = require('http');
+const fs = require('fs');
+const finalHandler = require('finalhandler');
+const queryString = require('querystring');
+const Router = require('router');
+const bodyParser = require('body-parser');
+const uid = require('rand-token').uid;
+
+const sunData = require('../initial-data/products.json')
 
 const PORT = 3001;
+const myRouter = Router();
+myRouter.use(bodyParser.json());
 
-http.createServer(function (request, response) {
-
+let server = http.createServer(function (request, response) {
+  myRouter(request, response, finalHandler(request, response))
 }).listen(PORT);
+
+myRouter.get('/sunglasses', function(request, response) {
+  // Return all sunglasses in the DB
+  response.writeHead(200, { "Content-Type": "application/json" });
+  return response.end(JSON.stringify(sunData));
+});
+
+module.exports = server;
--- test/sunglasses.js
+++ test/sunglasses.js
@@ -1,21 +1,62 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app/server.js');
+const server = require('../app/server');
 
-const expect = chai.expect();
+// const expect = chai.expect();
 const should = chai.should();
 
 chai.use(chaiHttp);
 
 describe('Sunglasses', () => {
   describe('/GET sunglasses', () => {
-    it('it should GET all the sunglasses', done => {
+    it('it should GET all the sunglasses', (done) => {
       chai
         .request(server)
         .get('/sunglasses')
         .end((err, res) => {
-          
-        })
-    })
-  })
-})
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+          res.body.length.should.be.eql(11);
+          done();
+        });
+    });
+  });
+});
+
+describe('User', () => {
+  describe('/POST user info')
+    it('should return the user info as an object and a token when POST correct login info', (done) => {
+      const validLogin = {
+        username: 'yellowleopard753',
+        password: 'jonjon'
+      };
+
+      chai
+        .request(server)
+        .post('/me')
+        .send(validLogin)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('object');
+          res.body.should.have.property('cart');
+          res.body.should.have.property('accessToken');
+          done();
+        });
+    });
+
+    it('should return an error when provided with th wrong login credentials', (done) => {
+      const invalidCredentials = {
+        username: 'yellowleopard753',
+        password: 'nope'
+      };
+
+      chai
+        .request(server)
+        .post('/me')
+        .send(invalidCredentials)
+        .end((err, res) => {
+          res.should.have.status(401);
+          done();
+        });
+    });
+});
