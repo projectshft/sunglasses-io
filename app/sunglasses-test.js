ccb61f1aa4855af5a701f56be2d37d17e4b0ebba
Connor Cook <97connorcook@gmail.com>
2023-05-17 11:30:45 -0700

    chai installed -A

---
+++ app/sunglasses-test.js
@@ -0,0 +1,12 @@
+const expect = require('chai');
+
+const chai = require("chai");
+
+const chaiHttp = require("chai-http");
+
+const server = require("../server");
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
