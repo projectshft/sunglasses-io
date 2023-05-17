05de741fb859bf73a197b2ef4df183807e1080f9
Connor Cook <97connorcook@gmail.com>
2023-05-17 12:37:42 -0700

    /get brands test added -A

--- app/sunglasses-test.js
+++ app/sunglasses-test.js
@@ -10,3 +10,41 @@ const should = chai.should();
 
 chai.use(chaiHttp);
 
+describe('Sunglasses', () => {
+  describe('/GET Brands', () => {
+    it('Should return an array of brands', (done) => {
+      //arrange
+      //act
+      chai
+        .request(server)
+        .get('/brands')
+      //assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.length.should.be.eq(5);
+          done();
+        });
+    });
+  });
+  describe('/GET Brand Products', () => {
+    it('Should get the products of a given brand', (done) => {
+      //arrange
+      let categoryId = 1
+      //act
+      chai
+        .request(server)
+        .get(`/brand/${id}/products`)
+      //assert
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.forEach((product) => {
+            expect(product).to.be.an("object");
+            expect(product).to.include.all.keys('id', 'categoryId', 'name', 'description', 'price', 'imageUrls')
+          });
+          done();
+        })
+    });
+  });
+})
