ce56f7a2464f40fbf2fbff58be97541db98b5762
Becky <tug76312@temple.edu>
2023-05-18 21:18:24 -0400

    updated test.js

--- app/server.js
+++ app/server.js
@@ -3,8 +3,14 @@ var fs = require('fs');
 var finalHandler = require('finalhandler');
 var queryString = require('querystring');
 var Router = require('router');
-var bodyParser   = require('body-parser');
+var bodyParser = require('body-parser');
 var uid = require('rand-token').uid;
+
+const baseUrl = 'https://api.sunglasses.com/v1';
+
+//set up router 
+var myRouter = Router(); 
+myRouter.use(bodyParser.json());
 
 const PORT = 3001;
 
--- app/sunglasses-test.js
+++ app/sunglasses-test.js
@@ -1,36 +1,89 @@
 let { expect } = require("chai");
 let ShoppingCart = require("./sunglasses");
 
-describe("The shopping cart", () => {
-  describe("Subtotal should be", () => {
-    it("0 if no items are added", () => {
-      //arrage
-      let shoppingCart = new ShoppingCart();
-      //act
-      let subtotal = shoppingCart.subtotal;
-      //assert
-      expect(subtotal).to.equal(0);
-    });
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("./server");
+//?
+let should = chai.should();
+//?
+chai.use(chaiHttp);
 
-    it("the sum of all items price * quantity for all items", () => {
-      let shopping = new ShoppingCart([
-        {
-          id: 1,
-          quantity: 5,
-          price: 5,
-        },
-        {
+const baseUrl = "https://api.sunglasses.com/v1";
+
+describe("Sunglasses API ", () => {
+  describe("GET /sunglasses", () => {
+    it("should return an array of sunglasses", (done) => {
+      chai
+        .request(baseUrl)
+        .get("/sunglasses")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res).to.be.an("array");
+          expect(res).to.be.json;
+          done();
+        });
+    });
+  });
+
+  describe("GET /me", () => {
+    it("should return a user profile", (done) => {
+      chai
+        .request(baseUrl)
+        .get("/me")
+        .end((err, res) => {
+          expect(err).to.be.null;
+          expect(res).to.have.status(200);
+          expect(res).to.be.an("array");
+          expect();
+        });
+    });
+  });
+
+  describe("the shopping cart", () => {
+    describe("the items in the shopping cart", () => {
+      it("return the items added to the cart", () => {
+        let shoppingCart = new ShoppingCart([
+          {
+            id: 1,
+            quantity: 4,
+            price: 50,
+          },
+        ]);
+
+        let item = {
           id: 2,
-          quantity: 4,
-          price: 5,
-        },
-        {
-          id: 3,
-          quantity: 1,
-          price: 50,
-        },
-      ]);
-      expect(subtotal).to.equal(95);
+          quantity: 2,
+          price: 30,
+        };
+
+        let addedItem = shoppingCart.add(item); 
+        expect(addedItem).to.equal(item); 
+      });
+    });
+    describe("total should be", () => {
+      it("0 if no items are passes in ", () => {
+        let shoppingCart = new ShoppingCart();
+        let subtotal = shoppingCart.subtotal;
+        expect(subtotal).to.equal(0);
+      });
+
+      it("the sum of price * quantity for all items", () => {
+        let shoppingCart = new ShoppingCart([
+          {
+            id: 1,
+            quantity: 5,
+            price: 5,
+          },
+          {
+            id: 2,
+            quantity: 4,
+            price: 5,
+          },
+        ]);
+        expect(shoppingCart.subtotal).to.equal(45);
+      });
     });
   });
 });
