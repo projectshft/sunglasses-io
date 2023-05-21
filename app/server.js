92e94f6eedc4a123270d78ea0d656ead95650b22
Becky <tug76312@temple.edu>
2023-05-21 16:14:01 -0400

    new data.json file

--- app/server.js
+++ app/server.js
@@ -1,19 +1,53 @@
-var http = require('http');
-var fs = require('fs');
-var finalHandler = require('finalhandler');
-var queryString = require('querystring');
-var Router = require('router');
-var bodyParser = require('body-parser');
-var uid = require('rand-token').uid;
+var http = require("http");
+var fs = require("fs");
+var finalHandler = require("finalhandler");
+var queryString = require("querystring");
+var Router = require("router");
+var bodyParser = require("body-parser");
+var uid = require("rand-token").uid;
+const yaml = require("js-yaml");
 
-const baseUrl = 'https://api.sunglasses.com/v1';
+const express = require("express");
+const app = express();
+const swaggerUi = require("swagger-ui-express");
 
-//set up router 
-var myRouter = Router(); 
+const PORT = 3002;
+const baseUrl = "https://api.sunglasses.com/v1";
+
+//varibles
+let users = [];
+let glasses = [];
+let accessToken = [];
+let failLoginAttempts = {};
+
+//set up router
+var myRouter = Router();
 myRouter.use(bodyParser.json());
 
-const PORT = 3001;
+//create server
+http
+  .createServer(function (request, response) {
+    myRouter(request, response, finalHandler(request, response));
+  })
+  .listen(PORT, () => {
+    // const store = JSON.parse(fs.readFileSync('swagger.yaml','us-ascii'))
+    const swaggerFile = fs.readFileSync("app/swagger.yaml");
+    const swaggerDocument = yaml.load(swaggerFile);
+    console.log(`listening on ${PORT}`);
+  });
 
-http.createServer(function (request, response) {
+myRouter.get("/", function (request, response) {
+  response.end("Welcome to the sunglasses store!");
+});
 
-}).listen(PORT);
+//public route - endpoint for 'search for my glasses' - GET /api/brands
+app.get("/sunglasses", (req, res) => {
+  const ked = "";
+});
+
+// individual product and its info - GET /api/brands/:id/products
+//creat login? - POST /api/login
+// shoping cart - show cart - GET /api/me/cart
+// shoping cart - add items - POST /api/me/cart
+// shopping cart - delete items - DELETE /api/me/cart/:productId
+// shopping cart - change quanity - POST /api/me/cart/:productId
--- app/sunglasses-test.js
+++ app/sunglasses-test.js
@@ -26,64 +26,65 @@ describe("Sunglasses API ", () => {
         });
     });
   });
-
-  describe("GET /me", () => {
-    it("should return a user profile", (done) => {
-      chai
-        .request(baseUrl)
-        .get("/me")
-        .end((err, res) => {
-          expect(err).to.be.null;
-          expect(res).to.have.status(200);
-          expect(res).to.be.an("array");
-          expect();
-        });
-    });
-  });
-
-  describe("the shopping cart", () => {
-    describe("the items in the shopping cart", () => {
-      it("return the items added to the cart", () => {
-        let shoppingCart = new ShoppingCart([
-          {
-            id: 1,
-            quantity: 4,
-            price: 50,
-          },
-        ]);
-
-        let item = {
-          id: 2,
-          quantity: 2,
-          price: 30,
-        };
-
-        let addedItem = shoppingCart.add(item); 
-        expect(addedItem).to.equal(item); 
-      });
-    });
-    describe("total should be", () => {
-      it("0 if no items are passes in ", () => {
-        let shoppingCart = new ShoppingCart();
-        let subtotal = shoppingCart.subtotal;
-        expect(subtotal).to.equal(0);
-      });
-
-      it("the sum of price * quantity for all items", () => {
-        let shoppingCart = new ShoppingCart([
-          {
-            id: 1,
-            quantity: 5,
-            price: 5,
-          },
-          {
-            id: 2,
-            quantity: 4,
-            price: 5,
-          },
-        ]);
-        expect(shoppingCart.subtotal).to.equal(45);
-      });
-    });
-  });
 });
+
+//   describe("GET /me", () => {
+//     it("should return a user profile", (done) => {
+//       chai
+//         .request(baseUrl)
+//         .get("/me")
+//         .end((err, res) => {
+//           expect(err).to.be.null;
+//           expect(res).to.have.status(200);
+//           expect(res).to.be.an("array");
+//           expect();
+//         });
+//     });
+//   });
+
+//   describe("the shopping cart", () => {
+//     describe("the items in the shopping cart", () => {
+//       it("return the items added to the cart", () => {
+//         let shoppingCart = new ShoppingCart([
+//           {
+//             id: 1,
+//             quantity: 4,
+//             price: 50,
+//           },
+//         ]);
+
+//         let item = {
+//           id: 2,
+//           quantity: 2,
+//           price: 30,
+//         };
+
+//         let addedItem = shoppingCart.add(item); 
+//         expect(addedItem).to.equal(item); 
+//       });
+//     });
+//     describe("total should be", () => {
+//       it("0 if no items are passes in ", () => {
+//         let shoppingCart = new ShoppingCart();
+//         let subtotal = shoppingCart.subtotal;
+//         expect(subtotal).to.equal(0);
+//       });
+
+//       it("the sum of price * quantity for all items", () => {
+//         let shoppingCart = new ShoppingCart([
+//           {
+//             id: 1,
+//             quantity: 5,
+//             price: 5,
+//           },
+//           {
+//             id: 2,
+//             quantity: 4,
+//             price: 5,
+//           },
+//         ]);
+//         expect(shoppingCart.subtotal).to.equal(45);
+//       });
+//     });
+//   });
+// });
