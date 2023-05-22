a4cae142381431d2506420185a8fdbc5fa34b101
Becky <tug76312@temple.edu>
2023-05-21 20:33:04 -0400

    working /sunglasses route

--- app/server.js
+++ app/server.js
@@ -4,17 +4,14 @@ var finalHandler = require("finalhandler");
 var queryString = require("querystring");
 var Router = require("router");
 var bodyParser = require("body-parser");
-var uid = require("rand-token").uid;
-const yaml = require("js-yaml");
-
-const express = require("express");
-const app = express();
-const swaggerUi = require("swagger-ui-express");
 
 const PORT = 3002;
-const baseUrl = "https://api.sunglasses.com/v1";
+
+// Read the JSON file
+const jsonData = fs.readFileSync("data.json", "utf-8");
 
 //varibles
+const sunglassesData = JSON.parse(jsonData).sunglasses;
 let users = [];
 let glasses = [];
 let accessToken = [];
@@ -30,9 +27,6 @@ http
     myRouter(request, response, finalHandler(request, response));
   })
   .listen(PORT, () => {
-    // const store = JSON.parse(fs.readFileSync('swagger.yaml','us-ascii'))
-    const swaggerFile = fs.readFileSync("app/swagger.yaml");
-    const swaggerDocument = yaml.load(swaggerFile);
     console.log(`listening on ${PORT}`);
   });
 
@@ -41,8 +35,15 @@ myRouter.get("/", function (request, response) {
 });
 
 //public route - endpoint for 'search for my glasses' - GET /api/brands
-app.get("/sunglasses", (req, res) => {
-  const ked = "";
+myRouter.get("/sunglasses", function (request, response) {
+  const searchGlasses = sunglassesData.map((sunglasses) => {
+    return {
+      name: sunglasses.name,
+      brand: sunglasses.brand,
+    };
+  });
+  response.setHeader("Content-Type", "application/json");
+  response.end(JSON.stringify(searchGlasses));
 });
 
 // individual product and its info - GET /api/brands/:id/products
