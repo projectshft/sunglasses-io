fdc3e950518c3440acfde4ff6145b4f936e8ca44
Becky <tug76312@temple.edu>
2023-05-21 23:05:11 -0400

    working GET /api/brands/:id/products

--- app/server.js
+++ app/server.js
@@ -4,6 +4,9 @@ var finalHandler = require("finalhandler");
 var queryString = require("querystring");
 var Router = require("router");
 var bodyParser = require("body-parser");
+const express = require('express'); 
+const app = express(); 
+
 
 const PORT = 3002;
 
@@ -19,23 +22,14 @@ let failLoginAttempts = {};
 
 //set up router
 var myRouter = Router();
-myRouter.use(bodyParser.json());
+app.use(bodyParser.json());
 
-//create server
-http
-  .createServer(function (request, response) {
-    myRouter(request, response, finalHandler(request, response));
-  })
-  .listen(PORT, () => {
-    console.log(`listening on ${PORT}`);
-  });
-
-myRouter.get("/", function (request, response) {
+app.get("/", function (request, response) {
   response.end("Welcome to the sunglasses store!");
 });
 
 //public route - endpoint for 'search for my glasses' - GET /api/brands
-myRouter.get("/sunglasses", function (request, response) {
+app.get("/sunglasses", function (request, response) {
   const searchGlasses = sunglassesData.map((sunglasses) => {
     return {
       name: sunglasses.name,
@@ -47,8 +41,29 @@ myRouter.get("/sunglasses", function (request, response) {
 });
 
 // individual product and its info - GET /api/brands/:id/products
+app.get("/sunglasses/:itemId", function (request, response) {
+    //look up the item, if it doesnt exist, return 404
+    const thisItemId = request.params.itemId; 
+    const item = sunglassesData.find(g => g.itemId === thisItemId); 
+    if (!item) {
+        response.status(404).send('The sunglasses with this id is not found')
+    } else {
+        response.setHeader("Content-Type", "application/json");
+        response.end(JSON.stringify(item)); 
+    }
+  });
 //creat login? - POST /api/login
 // shoping cart - show cart - GET /api/me/cart
 // shoping cart - add items - POST /api/me/cart
 // shopping cart - delete items - DELETE /api/me/cart/:productId
 // shopping cart - change quanity - POST /api/me/cart/:productId
+
+//create server
+// http
+//   .createServer(function (request, response) {
+//     myRouter(request, response, finalHandler(request, response));
+//   })
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+  });
+
