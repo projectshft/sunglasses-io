42d230dc6f8a822c68122320807eda49b080332f
Jordan Cox <jordancox747@outlook.com>
2023-11-18 07:11:30 -0700

    getUserCart refactor

--- app/controllers/user.ts
+++ app/controllers/user.ts
@@ -7,11 +7,14 @@ import {
 } from "../../types/type-definitions";
 import { GetValidAccessToken, UpdateAccessToken } from "../login-methods";
 import { ServerResponse } from "http";
+import { GetCartContents } from "../cart-methods";
 
 const getValidToken: GetValidAccessToken =
   require("../login-methods.ts").getValidToken;
 const updateAccessToken: UpdateAccessToken =
   require("../login-methods.ts").updateAccessToken;
+const getCartContents: GetCartContents =
+  require("../cart-methods.ts").getCartContents;
 
 /**
  * Retrieves current user profile data
@@ -81,10 +84,157 @@ const getUser = (
   );
 };
 
+/**
+ * Logs user into website with username and password passed in the request body
+ * @param request Client request to server
+ * @param response Server response to client
+ * @returns void or ServerResponse
+ */
+const postUserLogin = (
+  request: Request,
+  response: Response,
+  users: User[]
+): void | ServerResponse => {
+  // Check users exist
+  if (users.length <= 0) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Users not found",
+      })
+    );
+  }
+
+  const { username, password } = request.body;
+
+  if (!username || !password) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Unauthorized",
+      })
+    );
+  }
+
+  /**
+   * User connected to username
+   */
+  const matchedUser = users.find((user) => user.login.username == username);
+
+  if (!matchedUser || matchedUser.login.password != password) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Unauthorized",
+      })
+    );
+  }
+
+  /**
+   * accessToken object containing username, time when last updated, and 16-character uid token
+   */
+  const accessToken = updateAccessToken(username);
+
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(
+    JSON.stringify({
+      responseCode: response.statusCode,
+      responseMessage: accessToken,
+    })
+  );
+};
+
+/**
+ * Retrieves current contents of user's cart
+ * @param request Client request to server
+ * @param response Server response to client
+ * @returns void or ServerResponse
+ */
+const getUserCart = (
+  request: Request,
+  response: Response,
+  users: User[]
+): void | ServerResponse => {
+  // Check users exist
+  if (users.length <= 0) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Users not found",
+      })
+    );
+  }
+
+  const accessToken = getValidToken(request);
+
+  if (!accessToken) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Unauthorized",
+      })
+    );
+  }
+
+  /**
+   * User connected to accessToken
+   */
+  const matchedUser = users.find(
+    (user) => user.login.username == accessToken.username
+  );
+
+  if (!matchedUser) {
+    response.writeHead(401, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Unauthorized",
+      })
+    );
+  }
+
+  /**
+   * Array of all products in cart, single product in cart if productId is included in query, or null if no product is found matching productId
+   */
+  const cart = getCartContents(matchedUser.cart, request);
+
+  if (!cart) {
+    response.writeHead(404, { "Content-Type": "application/json" });
+    return response.end(
+      JSON.stringify({
+        responseCode: response.statusCode,
+        responseMessage: "Product not found",
+      })
+    );
+  }
+
+  // Update accessToken lastUpdated time
+  updateAccessToken(accessToken.username);
+
+  response.writeHead(200, {
+    "Content-Type": "application/json",
+  });
+  response.end(
+    JSON.stringify({
+      responseCode: response.statusCode,
+      responseMessage: cart,
+    })
+  );
+};
+
 export interface UserController {
   getUser: typeof getUser;
+  postUserLogin: typeof postUserLogin;
+  getUserCart: typeof getUserCart;
 }
 
 module.exports = {
   getUser,
+  postUserLogin,
+  getUserCart
 };
