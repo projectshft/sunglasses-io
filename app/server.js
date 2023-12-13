0fe6cc96815b2db24f22df3b37133f857652471d
Jono Layman <J.layman910@gmail.comgit config --global user.name Jon>
2023-12-12 20:33:53 -0500

    added testing

--- app/server.js
+++ app/server.js
@@ -15,7 +15,8 @@ myRouter.use(bodyParser.json());
 // State holding variables 
 let brands = [];
 let users = [];
-let products = []
+let products = [];
+let accessTokens = [];
 
 // Set up server
 const server = http.createServer(function (request, response) {
@@ -23,28 +24,82 @@ const server = http.createServer(function (request, response) {
     response.writeHead(200, CORS_HEADERS);
     return response.end();
   }
-  const requestToken = request.headers['x-authentication'];
-  if (!validateToken(requestToken)) {
-    response.writeHead(401, "Unauthorized ")
-    return response.end();
-  }
+  //   const requestToken = request.headers['x-authentication'];
+  //   if (!validateToken(requestToken)) {
+  //     response.writeHead(401, "Unauthorized ")
+  //     return response.end();
+  // }
   myRouter(request, response, finalHandler(request, response));
 }).listen(PORT, (error) => {
   if (error) {
     throw error
   }
-  fs.readFile('initial-data/brands.json', 'utf8', function (error, data) {
+  fs.readFile('../initial-data/brands.json', 'utf8', function (error, data) {
     if (error) throw error;
     brands = JSON.parse(data)
+    console.log(`Server setup: ${brands.length} brands loaded`)
   })
-  fs.readFile('initial-data/products.json', 'utf8', function (error, data) {
+  fs.readFile('../initial-data/products.json', 'utf8', function (error, data) {
     if (error) throw error;
     products = JSON.parse(data)
+    console.log(`Server setup: ${products.length} products loaded`)
   })
-  fs.readFile('initial-data/users.json', 'utf8', function (error, data) {
+  fs.readFile('../initial-data/users.json', 'utf8', function (error, data) {
     if (error) throw error;
     users = JSON.parse(data)
+    console.log(`Server setup: ${users.length} users loaded`)
   })
 })
 
+myRouter.get('/api/brands', function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  return response.end(JSON.stringify(brands))
+});
+
+myRouter.get('/api/brands/{Id}/products', function (request, response) {
+  const brandId = request.params.id;
+  function getBrandById(id) {
+    return brands.find(brand => brand.id === id)
+  }
+  const brand = getBrandById(brandId)
+  if (brand) {
+    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify(brand))
+  } else {
+    response.writeHead(404, {'Content-Type' : 'application/json'})
+    response.end(JSON.stringify({error: "Brand not found"}))
+  }
+
+});
+
+// myRouter.get('/api/products', function (request, response) {
+//   response.writeHead(200, { 'Content-Type': 'application/json' })
+//   response.end(JSON.stringify(products))
+// });
+
+// myRouter.post('/api/login', function (request, response) {
+//   if (request.body.username && request.body.password) {
+//     let user = users.find((user) => {
+//       return user.login.username == request.body.username && username.login.password === request.body.password
+//     })
+//     if (user) {
+//       response.writeHead(200, { 'Content-Type': 'application/json' });
+//       let currentAccessToken = accessTokens.find((tokenObj) => {
+//         return tokenObj.username === user.login.username
+//       })
+//       if (currentAccessToken) {
+//         currentAccessToken.lastUpdated = new Date();
+//         return response.end(JSON.stringify(currentAccessToken.token));
+//       } else {
+//         let newAccessToken = {
+//           username: user.login.username,
+//           lastUpdated: new Date(),
+//           token: uid(16)
+//         }
+//         accessTokens.push(newAccessToken);
+//         return response.end(JSON.stringify(newAccessToken.token))
+//       }
+//     }
+//   }
+// })
 module.exports = server;
