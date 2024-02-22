14e08f1d7281f575ae19ba4435e89c2babc38df2
Scott <spalocsik@gmail.com>
2024-02-22 16:04:58 -0500

    added cart endpoints and tests, still need to add delete functionality

--- app/server.js
+++ app/server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
+const queryString = require('querystring');
+const url = require('url');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger-updated.yaml');
 const app = express();
-const uid = require('rand-token').uid;
-const newAccessToken = uid(16);
+const fs = require('fs');
 
 app.use(bodyParser.json());
 
-// Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
-
+const products = JSON.parse(fs.readFileSync('api/products.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('api/users.json', 'utf-8'));
+const brands = JSON.parse(fs.readFileSync('api/brands.json', 'utf-8'));
 
 // Error handling
 app.use((err, req, res, next) => {
@@ -27,35 +26,153 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Define get /api/brands endpoint
 app.get('/api/brands', (req, res) => {
-  const brandsData = [];
-  res.status(200).json(brandsData);
-});
+    res.status(200).json(brands);
+  });
+
 
 // Define get /api/brands/:brandId/products endpoint
 app.get('/api/brands/:brandId/products', (req, res) => {
   const brandId = req.params.brandId;
-  const productsData = []; 
-  res.status(200).json(productsData);
-});
 
-// Define get /api/products endpoint
+  // Filter products based on brandId
+  const productsFromBrandId = products.filter(product => product.brandId === brandId);
+
+  res.status(200).json(productsFromBrandId);
+  });
+
+
+// Define get /api/products endpoint 
 app.get('/api/products', (req, res) => {
-  const productsData = [];
-  res.status(200).json(productsData);
-});
+    res.status(200).json(products);
+  });
+
 
 // Define post /api/login endpoint
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
+  const secretKey = 'secretkey';
+  const options = { expiresIn: '1h' };
 
+  // Finds the user
   const user = users.find(user => user.login.username === username && user.login.password === password);
 
   if (user) {
-    res.status(200).json({ message: 'Login succesful'});
+    // Generate and send a token if login is successful
+    const token = jwt.sign({ username: user.login.username }, secretKey, options);
+    console.log('Generated Token:', token);
+    res.status(200).json({ message: 'Login successful', token: token, user: user });
   } else {
-    res.status(401).json({ error: 'Invalid credentials'});
+    res.status(401).json({ error: 'Invalid credentials' });
   }
 });
+
+
+// Define api/me/cart get endpoint
+app.get('/api/me/cart', (req, res) => {
+  const token = req.header('Authorization').replace('Bearer ', ''); // Extract token from the Authorization header
+
+  // Verify the token to get user information
+  jwt.verify(token, 'secretkey', (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+
+    const username = decoded.username;
+
+    // Find the user based on the decoded username
+    const user = users.find(user => user.login.username === username);
+
+      if (user) {
+        // Return the user/cart data
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ error: 'User not found' });
+      }
+    });
+  });
+
+app.post('/api/me/cart', (req, res) => {
+  const authorizationHeader = req.header('Authorization');
+  
+  if (!authorizationHeader) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+  
+  const token = authorizationHeader.replace('Bearer ', '');
+  
+  // Verify the token to get user information
+  jwt.verify(token, 'secretkey', (err, decoded) => {
+    if (err) {
+      res.status(401).json({ error: 'Unauthorized' });
+      return;
+    }
+  
+    const username = decoded.username;
+  
+    // Find the user based on the decoded username
+    const user = users.find(user => user.login.username === username);
+  
+    if (user) {
+      // Return the current state of the user's cart without modifying it
+      res.status(200).json(user.cart);
+    } 
+  });
+});
+
+app.post('/api/me/cart/:productId', (req, res) => {
+  const authorizationHeader = req.header('Authorization');
+
+  if (!authorizationHeader) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const token = authorizationHeader.replace('Bearer ', '');
+
+  // Verify the token to get user information
+  jwt.verify(token, 'secretkey', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const username = decoded.username;
+
+    // Find the user based on the decoded username
+    const user = users.find(user => user.login.username === username);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    const productId = req.params.productId;
+    const productToAdd = products.find(product => product.id === productId);
+
+    if (!productToAdd) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Check if the product is already in the user's cart
+    const existingCartItem = user.cart.find(item => item.product.id === productId);
+
+    if (existingCartItem) {
+      // If the product is already in the cart, increment the quantity
+      existingCartItem.quantity++;
+    } else {
+      // If the product is not in the cart, add it with a quantity of 1
+      user.cart.push({
+        product: productToAdd,
+        quantity: 1,
+      });
+    }
+
+    // Return the updated cart
+    res.status(200).json(user.cart);
+  });
+});
+
+  
+
 
 
 
--- test/server.test.js
+++ test/server.test.js
@@ -1,117 +1,274 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app/server'); // Adjust the path as needed
-const products = require('../api/products');
 
 const should = chai.should();
+const { expect } = require('chai');
 chai.use(chaiHttp);
 
-// TODO: Write tests for the server
+// Tests for brands
 describe('Brands', () => {
-  describe('/api/brands', () => {
-    it('should retrieve all brands in the store', (done) => {
+  describe('GET /api/brands', () => {
+    it('should get all brands in the store', (done) => {
       chai.request(server)
         .get('/api/brands')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          if (res.body.length === 0) {
-            console.log("There are no brands in the database.")
-          } else {
-            res.body.forEach((brand) => {
-              brand.should.have.property('id');
-              brand.should.have.property('name');
-              brand.id.should.be.a('string');
-              brand.name.should.be.a('string');
-            });
-          }
           done();
         });
     });
   });
 
-  describe('/api/brands/{brandId}/products', () => {
-    it('should retrieve all products specific to brand ID', (done) => {
+  describe('GET /api/brands/{brandId}/products', () => {
+    it('should get all products specific to brand ID', (done) => {
   
-      const brandId = "1";
+      const brandId = "3";
 
       chai.request(server)
         .get(`/api/brands/${brandId}/products`)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          if (res.body.length === 0) {
-            console.log("There are no products associated with this brand.")
-          } else {
-            res.body.forEach((product) => {
-              product.should.have.property('id');
-              product.should.have.property('categoryId');
-              product.should.have.property('name');
-              product.should.have.property('description');
-              product.should.have.property('price');
-              product.should.have.property('imageUrls');
-              product.id.should.be.a('string');
-              product.categoryId.should.be.a('string');
-              product.name.should.be.a('string');
-              product.description.should.be.a('string');
-              product.price.should.be.a('number');
-              product.imageUrls.should.be.an('array');
-            });
-          }
-          done();
+
+        // Check if all returned products have the correct brandId
+        res.body.forEach(product => {
+          product.should.have.property('brandId').equal(brandId);
+        });
+        
+        done();
         });
     });
   });
 
+// Tests for products
 describe('Products', () => {
-  describe('/api/products', () => {
-    it('should retrieve all products in the store', (done) => {
+  describe('GET /api/products', () => {
+    it('should get all products in the store', (done) => {
       chai.request(server)
         .get('/api/products')
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.an("array");
-          if (res.body.length === 0) {
-            console.log("There are no products in the database.")
-          } else {
-            res.body.forEach((product) => {
-              product.should.have.property('id');
-              product.should.have.property('categoryId');
-              product.should.have.property('name');
-              product.should.have.property('description');
-              product.should.have.property('price');
-              product.should.have.property('imageUrls');
-              product.id.should.be.a('string');
-              product.categoryId.should.be.a('string');
-              product.name.should.be.a('string');
-              product.description.should.be.a('string');
-              product.price.should.be.a('number');
-              product.imageUrls.should.be.an('array');
-            });
-          }
           done();
         });
     });
   });
 
+// Tests for user
 describe('User', () => {
-  describe('/api/login', () => {
+  describe('POST /api/login', () => {
     it('should log the user into the system with valid credentials', (done) => {
       const userLoginInfo = {
         username: 'yellowleopard753',
-        password: 'jonjon'
+        password: 'jonjon',
       };
-    
+
       chai.request(server)
         .post('/api/login')
-        .set('content-type', 'application/json')
         .send(userLoginInfo)
         .end((err, res) => {
+          // successful login
           res.should.have.status(200);
+          res.body.should.have.property('token');
+
           done();
         });
     });
+
+    it('should return 401 with invalid credentials', (done) => {
+      const invalidUserLoginInfo = {
+        username: 'invalidUsername',
+        password: 'invalidPassword',
+      };
+
+      chai.request(server)
+        .post('/api/login')
+        .send(invalidUserLoginInfo)
+        .end((err, res) => {
+          // unsuccessful login
+          res.should.have.status(401);
+
+          done();
+        });
     });
   });
+  describe('GET /api/me/cart', () => {
+    it('should get the contents of the users cart', (done) => {
+    // Test user
+      const user = {
+        login: {
+          username: 'yellowleopard753',
+          password: 'jonjon',
+        },
+        cart: [
+          {
+            product: {
+              id: '1',
+              name: 'Superglasses',
+              price: 150
+            },
+            quantity: 2,
+          },
+        ],
+      };
+      // Perform a login to get a valid token
+      chai.request(server)
+        .post('/api/login')
+        .set('content-type', 'application/json')
+        .send({
+          username: 'yellowleopard753',
+          password: 'jonjon',
+        })
+        .end((loginErr, loginRes) => {
+          // Check if login was successful
+          loginRes.should.have.status(200);
+          loginRes.body.should.have.property('token');
+  
+          const token = loginRes.body.token;
+  
+          // Perform a GET request to /api/me/cart with the obtained token
+          chai.request(server)
+            .get('/api/me/cart')
+            .set('Authorization', `Bearer ${token}`)
+            .send({ username: user.login.username })
+            .end((err, res) => {
+              if (res.status === 401) {
+                console.log(res.body);
+              }
+  
+              res.should.have.status(200);
+              res.body.should.have.property('login');
+              res.body.login.should.have.property('username').equal('yellowleopard753');
+              res.body.should.have.property('cart');
+  
+              done();
+          });
+        });
+      });
+    });
+  describe('POST /api/me/cart', () => {
+    it('should post the users current cart', (done) => {
+      const testUser = {
+        login: { username: 'testuser' },
+        cart: []
+      };
+    
+      const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InllbGxvd2xlb3BhcmQ3NTMiLCJpYXQiOjE3MDg2MzQzNTUsImV4cCI6MTcwODYzNzk1NX0.8QFVD3EFQSgaxjRcn-0NfyMa7fGQYoDgeGyecZL9j54';
+    
+      chai.request(server)
+        .post('/api/me/cart')
+        .set('Authorization', `Bearer ${token}`)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('array');
+    
+          done();
+        });
+    });
+    
+    it('should return 401 for unauthorized access', (done) => {
+      chai.request(server)
+        .post('/api/me/cart')
+        .end((err, res) => {
+          expect(res).to.have.status(401);
+    
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/me/cart/:productId', () => {
+    it('should add a product to the users cart', (done) => {
+      // Test user
+      const user = {
+        login: {
+          username: 'yellowleopard753',
+          password: 'jonjon'
+        },
+        cart: []
+      };
+
+      // Product to be added to cart
+      const productToAdd = {
+        id: '1',
+        name: 'Superglasses',
+        price: 150
+      };
+
+      // Login to get token
+      chai.request(server)
+        .post('/api/login')
+        .set('content-type', 'application/json')
+        .send({
+          username: 'yellowleopard753',
+          password: 'jonjon'
+        })
+        .end((loginErr, loginRes) => {
+          // check if login was successful
+          loginRes.should.have.status(200);
+          loginRes.body.should.have.property('token');
+
+          const token = loginRes.body.token;
+
+      // Perform a POST request to /api/me/cart/:productId with the obtained token
+      chai.request(server)
+        .post(`/api/me/cart/1`) // hardcoded ID as 1 for testing
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an('array');
+    
+          // Check if the product has been added to the cart
+          const addedProduct = res.body.find(item => item.product.id === productToAdd.id);
+          expect(addedProduct).to.exist;
+          expect(addedProduct.quantity).to.equal(1);
+    
+          done();
+        });
+    });
+  });
+
+    it('should return 401 for unauthorized access', (done) => {
+      chai.request(server)
+        .post('/api/me/cart/1') // assuming productId is '1'
+        .end((err, res) => {
+          expect(res).to.have.status(401);
+
+          done();
+        });
+    });
+
+    it('should return 404 for non-existing product', (done) => {
+      // Perform a login to get a valid token
+      chai.request(server)
+        .post('/api/login')
+        .set('content-type', 'application/json')
+        .send({
+          username: 'yellowleopard753',
+          password: 'jonjon',
+        })
+        .end((loginErr, loginRes) => {
+          // Check if login was successful
+          loginRes.should.have.status(200);
+          loginRes.body.should.have.property('token');
+
+          const token = loginRes.body.token;
+
+          // Perform a POST request to /api/me/cart/:productId with the obtained token for a non-existing product
+          chai.request(server)
+            .post('/api/me/cart/nonExistingProductId')
+            .set('Authorization', `Bearer ${token}`)
+            .send()
+            .end((err, res) => {
+              expect(res).to.have.status(404);
+
+              done();
+            });
+          });
+});
+});
+});
 });
 });
