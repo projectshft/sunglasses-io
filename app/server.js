63f627c520005dc8a460ef5b805072c4ef5a27af
AdamC7313 <AdamC7313@gmail.com>
2024-02-23 21:50:00 -0500

    first commit

--- app/server.js
+++ app/server.js
@@ -22,10 +22,86 @@ app.use((err, req, res, next) => {
 // Swagger
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+//Middleware
+
+const authenticateJWT = (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	
+	const sha256 = users.find((user) => user.login.username === req.body.username).login.sha256
+
+	if(authHeader) {
+		const token = authHeader.split(' ')[1];
+
+		jwt.verify(token, sha256, (err, user) => {
+			if(err) {
+				return res.sendStatus(403);
+			}
+
+			req.user = user;
+			next();
+		});
+	}
+};
+
 // Starting the server
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
+});
+
+
+app.get('/brands', (req, res) => {
+	return res.json(brands);
+});
+
+app.get('/brands/:id/products', (req, res) => {
+
+	if(!brands.some(brand => brand.id === req.params.id)) {
+		return res.status(404).send("Brand not found")
+	}
+	const brandProducts = products.filter((product) => product.categoryId == req.params.id)
+	res.json(brandProducts)
+})
+
+app.get('/products', (req, res) => {
+	return res.json(products)
+})
+
+
+app.post('/login', (req, res) => {
+
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).send("Username and password are required")
+	}
+
+	let user = users.find((user) => user.login.username === req.body.username)
+
+	if(!user) {
+		return res.status(401).send("User not found")
+	}
+
+	if(user.login.password !== req.body.password) {
+		return res.status(401).send("Invalid password")
+	}
+
+	if(user.login.password === req.body.password) {
+		const token	= jwt.sign(user, user.login.sha256)
+		res.json({ accessToken: token })
+	}
+	
+});
+
+
+app.get('/me/cart', authenticateJWT, (req, res) => {
+	let user = users.find((user) => user.login.username === req.body.username)
+	res.json(user.cart);
+});
+
+app.post('/me/cart', authenticateJWT, (req, res) => {
+	let user = users.find((user) => user.login.username === req.body.username)
+	user.cart.push(req.body)
+	res.json(user.cart);
 });
 
 module.exports = app;
--- test/server.test.js
+++ test/server.test.js
@@ -7,8 +7,121 @@ chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 
-describe('Brands', () => {});
+const brandThreeEx = [
+    {
+    "id": "6",
+    "categoryId": "3",
+    "name": "glas",
+    "description": "Pretty awful glasses",
+    "price":10,
+    "imageUrls":["https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg"]
+  },
+  {
+    "id": "7",
+    "categoryId": "3",
+    "name": "QDogs Glasses",
+    "description": "They bark",
+    "price":1500,
+    "imageUrls":["https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg","https://image.shutterstock.com/z/stock-photo-yellow-sunglasses-white-backgound-600820286.jpg"]
+  }
+]
 
-describe('Login', () => {});
+describe('Brands', () => {
+  describe("/GET brand", () => {
+    it('should GET all the brands', done => {
+      chai
+        .request(server)
+        .get("/brands")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          done(err);
+        })
+    })
+  })
+  describe("/GET products for brand", () => {
+    it('should not GET the products for a brand id that does not exist', done => {
+      chai
+        .request(server)
+        .get("/brands/6/products")
+        .end((err, res) => {
+          res.should.have.status(404);
+          done(err)
+        })
+    })
+    
+    it('should GET the products for a paticular brand ID', done => {
+      chai
+        .request(server)
+        .get("/brands/3/products")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.an("array");
+          res.body.should.eql(brandThreeEx)
+          done(err)
+        })
+    })
+  })
+  describe("/GET products", () => {
+    it('should get a list of all products', done => {
+      chai
+        .request(server)
+        .get('/products')
+        .end((err, res) => {
+          res.should.have.status(200)
+          res.body.should.be.an("array");
+          done(err)
+        })
+    })
+  })
+});
+
+describe('/POST Login', () => {
+  it('should not POST login if username is blank', done => {
+    const passwordOnly = {username: '', password: 'helloWorld'}
+    chai
+      .request(server)
+      .post('/login')
+      .send(passwordOnly)
+      .end((err, res) => {
+        res.should.have.status(400)
+        done(err)
+      })
+  })
+  it('should not POST login if password is blank', done => {
+    const usernameOnly = {username: 'hello', password: ''}
+    chai
+      .request(server)
+      .post('/login')
+      .send(usernameOnly)
+      .end((err, res) => {
+        res.should.have.status(400)
+        done(err)
+      })
+  })
+  it('should not POST login if username and password are blank', done => {
+    const blank = {username: '', password: ''}
+    chai
+      .request(server)
+      .post('/login')
+      .send(blank)
+      .end((err, res) => {
+        res.should.have.status(400)
+        done(err)
+      })
+  })
+  it('should POST login if username and password are not blank', done => {
+    const login = {username: 'yellowleopard753', password: 'jonjon'}
+    chai
+      .request(server)
+      .post('/login')
+      .send(login)
+      .end((err, res) => {
+        res.should.have.status(200)
+        res.body.should.be.an('object')
+        res.body.should.have.property('md5')
+        done(err)
+      })
+})});
 
 describe('Cart', () => {});
