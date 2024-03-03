effee004e80a8953686f5aed95da28c49748b20d
Ryan Hardin <rhardin378@gmail.com>
2024-03-03 08:16:02 -0600

    working get route for '/brands' and tests.  working get route for '/brands/:brandId/products' and tests

--- app/server.js
+++ app/server.js
@@ -1,4 +1,5 @@
-const express = require("express");
+// const express = require("express");
+// const app = express();
 let http = require("http");
 let fs = require("fs");
 const bodyParser = require("body-parser");
@@ -6,48 +7,68 @@ const finalHandler = require("finalhandler");
 const Router = require("router");
 // const jwt = require("jsonwebtoken");
 const uid = require("rand-token").uid;
-const swaggerUi = require("swagger-ui-express");
+// const swaggerUi = require("swagger-ui-express");
 
-const YAML = require("yamljs");
-const swaggerDocument = YAML.load("swagger.yaml");
-const app = express();
+// const YAML = require("yamljs");
+// const swaggerDocument = YAML.load("./swagger.yaml");
+
 const myRouter = Router();
 myRouter.use(bodyParser.json());
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 2121;
 
 // Importing the data from JSON files
-const users = require("../initial-data/users.json");
+let users = [];
+// let brands = require("../initial-data/brands.json");
 const brands = require("../initial-data/brands.json");
 const products = require("../initial-data/products.json");
-
-// // Error handling
-// myRouter.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send("Something broke!");
-// });
+let signedInUser = {};
 
 // UNABLE TO SERVE SWAGGER FILES DUE TO USING ROUTER AND HTTP INSTEAD OF EXPRESS FOR SERVER
 // Swagger
-// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// myRouter.use("/api-docs", () => {
+//   swaggerUi.setup(swaggerDocument);
+// });
 
 // Starting the server
-http
+let server = http
   .createServer((request, response) => {
     myRouter(request, response, finalHandler(request, response));
   })
   .listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    // console.log(brands);
+    // console.log(products);
   });
 
 myRouter.get("/brands", (request, response) => {
-  response.end("brands");
+  if (brands.length === 0) {
+    response.writeHead(404);
+    response.end("no brands found");
+  } else {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(brands));
+  }
 });
 
-myRouter.get("/brands/:id/products", (request, response) => {
-  response.end("products from a brand");
+myRouter.get("/brands/:brandId/products", (request, response) => {
+  // get all products from a particular brand
+  // filter only products with brand id of :id
+  let brandProducts = products.filter((product) => {
+    return product.categoryId == request.params.brandId;
+  });
+  console.log(brandProducts);
+  if (brandProducts.length < 1) {
+    response.writeHead(404, "Brand with that id does not exist");
+    response.end("Not a valid brand");
+  } else {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(brandProducts));
+  }
 });
 
 myRouter.get("/products", (request, response) => {
+  // parse the query from url  name = glasses
+  // filter products by name to see if products includes the name used in query
   response.end("all products");
 });
 //actually a post
@@ -72,4 +93,9 @@ myRouter.get("/me/cart/:productId", (request, response) => {
   response.end("change quantity");
 });
 
-module.exports = app;
+// Error handling
+myRouter.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+module.exports = server;
--- test/server.test.js
+++ test/server.test.js
@@ -1,14 +1,47 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../app/server'); // Adjust the path as needed
-
-const should = chai.should();
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let server = require("../app/server"); // Adjust the path as needed
+let should = chai.should();
 chai.use(chaiHttp);
 
 // TODO: Write tests for the server
 
-describe('Brands', () => {});
+describe("/GET Brands", () => {
+  it("it should Get all the brands", (done) => {
+    return chai
+      .request(server)
+      .get("/brands")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an("array");
+        done();
+      });
+  });
+});
 
-describe('Login', () => {});
+describe("/GET /brands/brandId/products", () => {
+  it("it should Get all the products from a brand", (done) => {
+    return chai
+      .request(server)
+      .get("/brands/2/products")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.length.should.be.eql(2);
+        res.body.should.be.an("array");
+        done();
+      });
+  });
+  it("It should not Get any products if the brand does not exist", (done) => {
+    return chai
+      .request(server)
+      .get("/brands/a/products")
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
 
-describe('Cart', () => {});
+// describe("Login", () => {});
+
+// describe("Cart", () => {});
