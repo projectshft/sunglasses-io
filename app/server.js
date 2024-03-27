b6d36309e89fc88f7032549d561bc30c4f562156
Wesley Frankel <wesleyfrankel@gmail.com>
2024-03-26 18:35:28 -0700

    Initial commit

--- app/server.js
+++ app/server.js
@@ -1,31 +1,60 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml'); // Replace './swagger.yaml' with the path to your Swagger file
+const express = require("express");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("js-yaml");
+const swaggerDocument = YAML.load("./swagger.yaml"); // Replace './swagger.yaml' with the path to your Swagger file
 const app = express();
 
 app.use(bodyParser.json());
 
 // Importing the data from JSON files
-const users = require('../initial-data/users.json');
-const brands = require('../initial-data/brands.json');
-const products = require('../initial-data/products.json');
+const users = require("../initial-data/users.json");
+const brands = require("../initial-data/brands.json");
+const products = require("../initial-data/products.json");
+let tokens = ["nZQ3lxORlCvXbLcE", "nZQ3lxORlCvXbL6E"];
 
-// Error handling
+// GET /api/brands
+app.get("/api/brands", (req, res) => {
+  res.status(200).json(brands);
+});
+
+// POST /api/login
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Both fields need to be filled" });
+  }
+
+  const user = users.find(
+    (user) =>
+      user.login.username === username && user.login.password === password
+  );
+
+  if (user) {
+    let token = uid.sync(16);
+    user.login.token = token;
+    tokens.push(token);
+    res.status(200).json({ message: "Login successful" });
+  } else {
+    res.status(401).json({ message: "Invalid username or password" });
+  }
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Starting the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
